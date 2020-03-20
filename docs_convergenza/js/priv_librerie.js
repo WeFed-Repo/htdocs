c65395c973f3363bc53ac4db0db54e4a12147915
davidecioetto <davide.cioetto@alten.it>
2020-03-20 15:55:40 +0100

    Primo upload convergenza

---
+++ docs_convergenza/js/priv_librerie.js
@@ -0,0 +1,10 @@
+/* jshint strict:false */
+/* globals $, menuData, navIndex */
+/* exported menuLev3, librerie, ajaxBaseUrl */
+
+// Link statici forniti dal backend
+var librerie = true;
+var ajaxBaseUrl = '/librerie/include/commons/ajax';
+
+// Variabili globali librerie
+var libBasepath = "/connect.php?page=strutt_priv.php&tpl=";
