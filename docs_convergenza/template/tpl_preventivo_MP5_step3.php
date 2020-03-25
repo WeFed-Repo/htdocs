71c50ce28fc34fb2e1eadc5e1256bf263351face
sstrepponi <62430689+sstrepponi@users.noreply.github.com>
2020-03-25 10:09:52 +0100

    Assicurazioni step 3 e 4 preventivazione
    Assicurazioni step 3 e 4 preventivazione

---
+++ docs_convergenza/template/tpl_preventivo_MP5_step3.php
@@ -0,0 +1,279 @@
+<!-- TITOLO DI PAGINA -->
+<section>
+    <div class="titolo">
+            <h1>
+                <div class="row">
+
+                    
+                    <div class="col-sm-8">
+                        <span>Richiedi il preventivo<br>Riepilogo</span></span>
+                    </div>
+                    <div class="col-sm-4">	
+                        <div class="pager pull-right">
+                            <div class="circle current_page">1</div>
+                            <div class="circle current_page">2</div>
+                            <div class="circle current_page">3</div>
+                            <div class="circle">4</div>
+                        </div>
+                    </div>
+                    
+                </div>
+            </h1>
+    </div>
+</section>
+<!-- TITOLO DI PAGINA -->
+
+
+<section>
+	<div class="border-output">
+		<div class="row first-row">
+			<div class="col-sm-12 col-xs-12 boxDefault">
+				<h3 class="titleSection titleForm">Dati preventivo</h3>
+				<div class="formWrapper">
+					<div class="row">
+						<div class="col-xs-4">
+							<label class="control-label">Contraente/assicurato</label>
+						</div>
+						<div class="col-xs-8">
+							<span class="resume">Nome_Cognome</span>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-xs-4">
+							<label class="control-label">Polizza</label>
+						</div>
+						<div class="col-xs-8">
+							<span class="resume">Nome_prodotto</span>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-xs-4">
+							<label class="control-label">Data preventivo</label>
+						</div>
+						<div class="col-xs-8">
+							<span class="resume">data_odierna</span>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-xs-4">
+							<label class="control-label">Decorrenza polizza</label>
+						</div>
+						<div class="col-xs-8">
+							<span class="resume overflowEllipsis">a polizza decorre dal giorno dell'eventuale acquisto</span>
+						</div>
+					</div>
+                    <div class="row">
+						<div class="col-xs-4">
+							<label class="control-label">Tacito rinnovo</label>
+						</div>
+						<div class="col-xs-8">
+							<span class="resume">S&igrave;&nbsp; </span> <a class="text-underline" href="Javascript:;" data-toggle="tooltip" data-title="La polizza si rinnova automaticamente. Potrà essere disdetta in agenzia o con raccomandata entro 30 giorni dalla scadenza."> dettagli</a>
+						</div>
+					</div>
+					
+				</div>
+			</div>
+			
+		</div>
+	</div>
+
+    <div class="border-output">
+		<div class="row first-row">
+			<div class="col-sm-12 col-xs-12 boxDefault">
+				<h3 class="titleSection titleForm">Beni assicurati</h3>
+				<div class="formWrapper">
+					<div class="row">
+						<div class="col-xs-4">
+							<label class="control-label">Tipo di bene</label>
+						</div>
+						<div class="col-xs-8">
+							<span class="resume">Tipo_Bene_1</span>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-xs-4">
+							<label class="control-label">Valore immobile</label>
+						</div>
+						<div class="col-xs-8">
+                        <span class="resume">xxx.xxx,xx &euro;&nbsp; </span> <a class="text-underline" href="Javascript:;" data-toggle="tooltip" data-title="Corrisponde al valore di ricostruzione dell'immobile calcolato sulla base dei mq e della categoria catastale"> dettagli</a>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-xs-4">
+							<label class="control-label">Indirizzo</label>
+						</div>
+						<div class="col-xs-8">
+							<span class="resume">Via Roma 5, Milano 20122 (MI)</span>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-xs-4">
+							<label class="control-label">Garanzie</label>
+						</div>
+						<div class="col-xs-8">
+							<span class="resume overflowEllipsis">Garanzia_1, Garanzia_2, Garanzia_3, </span>
+						</div>
+					</div>
+                </div>
+			</div>
+			
+		</div>
+	</div>
+</section>
+
+<section>
+<div class="clearfix selezioneCC selezioneCCNoBor">
+    <div class="border-output">
+		<div class="row first-row">
+			<div class="col-sm-12 col-xs-12 boxDefault">
+				<h3 class="titleSection titleForm">Premio</h3>
+				<div class="formWrapper">
+					<div class="row">
+						<div class="col-xs-4">
+							<label class="control-label">Premio totale</label>
+						</div>
+						<div class="col-xs-8">
+							<span class="resume">000.000,00 &euro;</span>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-xs-4">
+							<label class="control-label">Frazionamento</label>
+						</div>
+						<div class="col-xs-8">
+							<span class="resume">Annuale</span>
+						</div>
+                    </div>
+                    <div class="row">
+						<div class="col-xs-12">
+                            <a class="link-text btn-show" href="javascript:;"><i class="icon icon-f-row_expand"></i>Mostra dettaglio premio</a>
+                            <a style="display:none" class="link-text btn-hide" href="javascript:;"><i class="icon icon-f-row_contract"></i>Nascondi dettaglio premio</a>
+						</div>
+                    </div>
+                    <div class="details" style="display:none">
+                        <div class="row">
+                            <div class="col-xs-12">
+                                <span class="resume">Dimora abituale</span>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-xs-4">
+                                <label class="control-label">Danni al fabbricato</label>
+                            </div>
+                            <div class="col-xs-8">
+                                <span class="resume">000,00 &euro;</span>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-xs-4">
+                                <label class="control-label">Danni al contenuto + RC e Assistenza</label>
+                            </div>
+                            <div class="col-xs-8">
+                                <span class="resume">000,00 &euro;</span>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-xs-4">
+                                <label class="control-label">Furto, rapina e scippo</label>
+                            </div>
+                            <div class="col-xs-8">
+                                <span class="resume">000,00 &euro;</span>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-xs-12">
+                                <span class="resume">Box</span>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-xs-4">
+                                <label class="control-label">Danni al fabbricato</label>
+                            </div>
+                            <div class="col-xs-8">
+                                <span class="resume">000,00 €</span>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-xs-12">
+                                <span class="resume">Soggetto fisico</span>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-xs-4">
+                                <label class="control-label">Tutela legale</label>
+                            </div>
+                            <div class="col-xs-8">
+                                <span class="resume">000,00 €</span>
+                            </div>
+                        </div>
+                    </div>
+                   
+				</div>
+			</div>
+			
+		</div>
+	</div>
+</div>
+</section>
+
+
+
+<section>
+    <h3 class="titleSection titleForm">Informativa precontrattuale</h3>
+    <p>Prendi visione delle informative precontrattuali</p>
+    <div class="form-group">
+        <div class="row">
+            <div class="form-field-input col-xs-12 col-sm-12">
+                <label class="informativa"><input disabled type="checkbox" name="" id="" class="input-toggle-hidden">Dichiaro di aver preso visione dell' <a href="#" target="_blank" class="text-underline">allegato 3</a>, <a href="#" target="_blank" class="text-underline">l'allegato 4</a>, il <a href="#" target="_blank" class="text-underline">DIP</a>, il <a href="#" target="_blank" class="text-underline">DIP aggiuntivo</a>, delle <a href="#" target="_blank" class="text-underline">condizioni di assicurazione</a>, e di averne compreso e salvato il contenuto.</label>
+            </div>
+        </div> 
+           
+    </div>
+</section>
+<!-- BOTTONE USATO IN STEP DISPOSITIVE-->
+<section>
+  <div class="form-group btnWrapper clearfix">
+    <div class="btn-align-right">
+  		 	<div>
+				<a type="button" href="#" class="btn btn-primary" title="salva il preventivo">salva preventivo</a>
+  				<a type="button" href="#" class="btn btn-primary" title="acquista ora">acquista ora</a>
+  			</div>
+      </div>
+      <div class="btn-align-back">
+          <div>
+            <a type="button" href="javascript:vai('back')" class="btn btn-default">indietro</a>
+          </div>
+      </div>
+  </div>
+</section>
+
+
+<script>
+    //GESTIONE SLIDE CONTENUTI NASCOSTI
+    $(".btn-show").on("click",function() {
+        $(this).closest(".row").next(".details").eq(0).slideDown();
+        $(this).hide();
+        $(this).next(".btn-hide").show();
+    })
+    $(".btn-hide").on("click",function() {
+        $(this).closest(".row").next(".details").eq(0).slideUp();
+        $(this).hide();
+        $(this).prev(".btn-show").show();
+    })
+    //GESTIONE ABILITAZIONE CHECKBOX
+    var counter = 0
+
+    $(".informativa").find("a").on("click", function() {
+        if(!$(this).hasClass("visited"))
+        {
+            $(this).addClass("visited");
+            counter += 1
+            
+        }
+       if (counter=== $(".informativa").find("a").length) 
+       {
+            $(".informativa").find("input[type='checkbox']").attr("disabled",false)
+       }
+       
+    });
+</script>
