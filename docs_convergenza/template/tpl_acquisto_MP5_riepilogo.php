cf3b2f06c5f744d44c154554f0bcb303901d574f
sstrepponi <62430689+sstrepponi@users.noreply.github.com>
2020-03-25 17:04:53 +0100

    acquisto polizza MP5
    aggiunto template riepilogo acquisto polizza e modifica riepilogo preventivo

--- docs_convergenza/template/tpl_acquisto_MP5_riepilogo.php
+++ docs_convergenza/template/tpl_acquisto_MP5_riepilogo.php
@@ -3,17 +3,14 @@
     <div class="titolo">
             <h1>
                 <div class="row">
-
-                    
                     <div class="col-sm-8">
-                        <span>Richiedi il preventivo<br>Riepilogo</span></span>
+                        <span>Acquisto ed emissione polizza<br>RIEPILOGO</span></span>
                     </div>
                     <div class="col-sm-4">	
                         <div class="pager pull-right">
                             <div class="circle current_page">1</div>
-                            <div class="circle current_page">2</div>
-                            <div class="circle current_page">3</div>
-                            <div class="circle">4</div>
+                            <div class="circle">2</div>
+                            <div class="circle">3</div>
                         </div>
                     </div>
                     
@@ -21,21 +18,28 @@
             </h1>
     </div>
 </section>
-<!-- TITOLO DI PAGINA -->
-
 
 <section>
-	<div class="border-output">
+  <p>Di seguito sono riportati i dati della polizza che stai per acquistare.</p>
+  <div class="border-output">
 		<div class="row first-row">
 			<div class="col-sm-12 col-xs-12 boxDefault">
-				<h3 class="titleSection titleForm">Dati preventivo</h3>
+				<h3 class="titleSection titleForm">Dati polizza</h3>
 				<div class="formWrapper">
+					<div class="row">
+						<div class="col-xs-4">
+							<label class="control-label">Preventivo N.</label>
+						</div>
+						<div class="col-xs-8">
+							<span class="resume">0000000</span>
+						</div>
+					</div>
 					<div class="row">
 						<div class="col-xs-4">
 							<label class="control-label">Contraente/assicurato</label>
 						</div>
 						<div class="col-xs-8">
-							<span class="resume">Nome_Cognome</span>
+							<span class="resume">nome_cognome</span>
 						</div>
 					</div>
 					<div class="row">
@@ -43,23 +47,31 @@
 							<label class="control-label">Polizza</label>
 						</div>
 						<div class="col-xs-8">
-							<span class="resume">Nome_prodotto</span>
+							<span class="resume">nome_cognome</span>
 						</div>
 					</div>
 					<div class="row">
 						<div class="col-xs-4">
-							<label class="control-label">Data preventivo</label>
+							<label class="control-label">Data emissione</label>
 						</div>
 						<div class="col-xs-8">
 							<span class="resume">data_odierna</span>
 						</div>
 					</div>
-					<div class="row">
+                    <div class="row">
 						<div class="col-xs-4">
-							<label class="control-label">Decorrenza polizza</label>
+							<label class="control-label">Data decorrenza</label>
 						</div>
 						<div class="col-xs-8">
-							<span class="resume overflowEllipsis">a polizza decorre dal giorno dell'eventuale acquisto</span>
+							<span class="resume">data_odierna</span>
+						</div>
+                    </div>
+                    <div class="row">
+						<div class="col-xs-4">
+							<label class="control-label">Data scadenza</label>
+						</div>
+						<div class="col-xs-8">
+							<span class="resume">data_odierna + 1 anno</span>
 						</div>
 					</div>
                     <div class="row">
@@ -70,18 +82,16 @@
 							<span class="resume">S&igrave;&nbsp; </span> <a class="text-underline" href="Javascript:;" data-toggle="tooltip" data-title="La polizza si rinnova automaticamente. Potrà essere disdetta in agenzia o con raccomandata entro 30 giorni dalla scadenza."> dettagli</a>
 						</div>
 					</div>
-					
 				</div>
 			</div>
 			
 		</div>
-	</div>
-
+    </div>
     <div class="border-output">
 		<div class="row first-row">
 			<div class="col-sm-12 col-xs-12 boxDefault">
 				<h3 class="titleSection titleForm">Beni assicurati</h3>
-				<div class="formWrapper">
+				<div class="formWrapper marginBottom25">
 					<div class="row">
 						<div class="col-xs-4">
 							<label class="control-label">Tipo di bene</label>
@@ -95,15 +105,15 @@
 							<label class="control-label">Valore immobile</label>
 						</div>
 						<div class="col-xs-8">
-                        <span class="resume">xxx.xxx,xx &euro;&nbsp; </span> <a class="text-underline" href="Javascript:;" data-toggle="tooltip" data-title="Corrisponde al valore di ricostruzione dell'immobile calcolato sulla base dei mq e della categoria catastale"> dettagli</a>
+                            <span class="resume">xxx.xxx,xx &euro;&nbsp; </span> <a class="text-underline" href="Javascript:;" data-toggle="tooltip" data-title="Corrisponde al valore di ricostruzione dell'immobile calcolato sulla base dei mq e della categoria catastale."> dettagli</a>
 						</div>
-					</div>
-					<div class="row">
+                    </div>
+                    <div class="row">
 						<div class="col-xs-4">
 							<label class="control-label">Indirizzo</label>
 						</div>
 						<div class="col-xs-8">
-							<span class="resume">Via Roma 5, Milano 20122 (MI)</span>
+                            <span class="resume">Via Roma 5, Milano 20122 (MI)</span>
 						</div>
 					</div>
 					<div class="row">
@@ -111,16 +121,48 @@
 							<label class="control-label">Garanzie</label>
 						</div>
 						<div class="col-xs-8">
-							<span class="resume overflowEllipsis">Garanzia_1, Garanzia_2, Garanzia_3, </span>
+                            <span class="resume">Garanzia_1, Garanzia_2, Garanzia_3</span>
+						</div>
+					</div>
+                </div>
+                <div class="formWrapper">
+					<div class="row">
+						<div class="col-xs-4">
+							<label class="control-label">Tipo di bene</label>
+						</div>
+						<div class="col-xs-8">
+							<span class="resume">Tipo_Bene_2</span>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-xs-4">
+							<label class="control-label">Valore immobile</label>
+						</div>
+						<div class="col-xs-8">
+                            <span class="resume">xxx.xxx,xx &euro;&nbsp; </span> <a class="text-underline" href="Javascript:;" data-toggle="tooltip" data-title="Corrisponde al valore di ricostruzione dell'immobile calcolato sulla base dei mq e della categoria catastale."> dettagli</a>
+						</div>
+                    </div>
+                    <div class="row">
+						<div class="col-xs-4">
+							<label class="control-label">Indirizzo</label>
+						</div>
+						<div class="col-xs-8">
+                            <span class="resume">Via Roma 5, Milano 20122 (MI)</span>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-xs-4">
+							<label class="control-label">Garanzie</label>
+						</div>
+						<div class="col-xs-8">
+                            <span class="resume">Garanzia_1, Garanzia_2, Garanzia_3</span>
 						</div>
 					</div>
                 </div>
 			</div>
-			
 		</div>
 	</div>
 </section>
-
 <section>
 <div class="clearfix selezioneCC selezioneCCNoBor">
     <div class="border-output">
@@ -217,34 +259,81 @@
 	</div>
 </div>
 </section>
-
-
-
 <section>
-    <h3 class="titleSection titleForm">Informativa precontrattuale</h3>
-    <p>Prendi visione delle informative precontrattuali</p>
-    <div class="form-group">
-        <div class="row">
-            <div class="form-field-input col-xs-12 col-sm-12">
-                <label class="informativa"><input disabled type="checkbox" name="" id="" class="input-toggle-hidden">Dichiaro di aver preso visione dell' <a href="#" target="_blank" class="text-underline">allegato 3</a>, <a href="#" target="_blank" class="text-underline">l'allegato 4</a>, il <a href="#" target="_blank" class="text-underline">DIP</a>, il <a href="#" target="_blank" class="text-underline">DIP aggiuntivo</a>, delle <a href="#" target="_blank" class="text-underline">condizioni di assicurazione</a>, e di averne compreso e salvato il contenuto.</label>
+    <h3 class="titleSection titleForm">Seleziona il conto di addebito</h3>
+    <div class="row">
+    <div class="col-xs-12 col-sm-4 form-field-input">
+        <label class="control-label">Conto corrente  di addebito</label>
+        <script type="text/javascript">
+            /* Esempio inizializzazione */
+            $(function(){
+                // Inizializza la special-select tipo "default" (esempio di callback)
+                $("#spselDetail").spSel();
+            });
+
+        </script>
+        <div class="spsel nosel" id="spselDetail"  placeholder="Seleziona...">
+            <input type="hidden" name="spselDetailInput">
+            <div class="spsel-options">
+                <div class="spsel-option" data-value="0">
+                    <a class="spsel-option-el">Elemento 0 lorem ipsum dolor sit amet<span class="only-detail"><br>Lorem <strong>ipsum</strong> dolor<br><span>XXXX</span></a>
+                </div>
+                <div class="spsel-option" data-value="1">
+                    <a class="spsel-option-el">Elemento 0 lorem ipsum dolor sit amet<span class="only-detail"><br>Lorem <strong>ipsum</strong> dolor<br><span>XXXX</span></a>
+                </div>
             </div>
-        </div> 
-           
-    </div>
+        </div>
+    
+        </div>
+        </div>
+        <div class="form-group">
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 form-field-input">
+                    <h3 class="titleSection titleForm">Come desideri ricevere la documentazione contrattuale?</h3>
+                </div>
+            </div>
+             <div class="row">
+               <div class="form-field-input col-xs-12">
+                  <label>
+                    <input type="radio" name="formato" value="D"  id="" checked onclick="$('#digitale').show();$('#cartaceo').hide()">
+                    Formato digitale
+                  </label>
+                  <p class="marginLeftExtraLarge">Sarà disponibile, una volta completato l'acquisto, nella sezione "Polizze attive”.</p>
+               </div>
+              <div class="form-field-input col-xs-6">
+                 <label>
+                    <input type="radio" name="formato" value="C"  id="" onclick="$('#digitale').hide();$('#cartaceo').show()">
+                    Formato cartaceo
+                  </label>
+                  <p class="marginLeftExtraLarge">ATTENZIONE: se scegli di ricevere la documentazione in formato cartaceo, non potrai acquistare la polizza online ma solo in filiale.</p>
+               </div>
+            </div>
+        </div>
 </section>
-<!-- BOTTONE USATO IN STEP DISPOSITIVE-->
 <section>
-  <div class="form-group btnWrapper clearfix">
-    <div class="btn-align-right">
-  		 	<div>
-				<a type="button" href="#" class="btn btn-primary" title="salva il preventivo">salva preventivo</a>
-  				<a type="button" href="#" class="btn btn-primary" title="acquista ora">acquista ora</a>
-  			</div>
-      </div>
-      <div class="btn-align-back">
-          <div>
-            <a type="button" href="javascript:vai('back')" class="btn btn-default">indietro</a>
+    <div class="row" id="digitale">
+        <div class="col-xs-12 col-sm-4">
+            <p><strong>Autorizza il pagamento</strong></p>
+            <p>Per autorizzare clicca su Acquista polizza e utilizza la tua <strong>app di sicurezza.</strong></p>
+        </div>
+        <div class="col-xs-12 col-sm-8 paddingTopLarge">
+            <div class="btn-align-right">
+                <div>
+                    <a type="button" href="#" class="btn btn-primary" title="acquista polizza">acquista polizza</a>
+                </div>
           </div>
-      </div>
-  </div>
+        </div>
+    </div>
+    <div class="row" id="cartaceo" style="display:none">
+        <div class="col-xs-12 col-sm-4">
+            <p>Contatta il gestore della tua filiale di riferimento</p>
+        </div>
+        <div class="col-xs-12 col-sm-8 paddingTopLarge">
+            <div class="btn-align-right">
+                <div>
+                    <a type="button" href="#" class="btn btn-primary" title="prendi appuntamento">prendi appuntamento</a>
+                </div>
+          </div>
+        </div>
+    </div>
 </section>
