ada14fd4026145774a0937f502d3d88441784364
sstrepponi <62430689+sstrepponi@users.noreply.github.com>
2020-03-26 11:04:01 +0100

    template acquisto MP5
    aggiunti template acquisto MP5

---
+++ docs_convergenza/template/tpl_acquisto_MP5_pagamento_ok.php
@@ -0,0 +1,47 @@
+<!-- TITOLO DI PAGINA -->
+<section>
+    <div class="titolo">
+            <h1>
+                <div class="row">
+                    <div class="col-sm-8">
+                        <span>Acquisto ed emissione polizza<br>FIRMA DELLA DOCUMENTAZIONE CONTRATTUALE</span></span>
+                    </div>
+                    <div class="col-sm-4">	
+                        <div class="pager pull-right">
+                            <div class="circle current_page">1</div>
+                            <div class="circle current_page">2</div>
+                            <div class="circle">3</div>
+                        </div>
+                    </div>
+                    
+                </div>
+            </h1>
+    </div>
+</section>
+
+<section>
+    <h3 class="titleSection titleForm">Il pagamento della polizza è andato a buon fine.</h3>
+    <p>Per finalizzare l'emissione della polizza prosegui con la presa visione e la sottoscrizione della documentazione contrattuale</p>
+    <p><a target="_blank" href="#" onclick="$('[data-toggle =tooltip]').tooltip('disable')"><i class="icon icon-file_pdf_fill fLeft" title="
+    Nome_documento"></i> <span class="fLeft btnLink paddingLeftSmall">
+    Nome_documento</span></a>
+</section>
+<section>
+    <h3 class="titleSection titleForm">SOTTOSCRIZIONE CONDIZIONI GENERALI</h3>
+    <p>In approvazione  del contenuto e delle condizioni riportate nella documentazione contrattuale, appongo la mia firma.</p>
+    <div class="row">
+        <div class="col-xs-12 col-sm-4">
+           <p>(Inserisci la Password usa e getta/OTP generata dal tuo dispositivo di sicurezza).</p>
+        </div>
+        <div class="col-xs-12 col-sm-4 form-field-input">
+            <input type="text" id="pwdInput" value="" name="" autocomplete="off" class="form-control"  data-toggle="tooltip" data-title="aprire il documento pdf per procedere alla firma">
+        </div>
+        <div class="col-xs-12 col-sm-4">
+            <div class="btn-align-right">
+                <div>
+                    <a type="button" href="#" class="btn btn-primary" title="acquista polizza">firma</a>
+                </div>
+          </div>
+        </div>
+    </div>
+</section>
---
+++ docs_convergenza/template/tpl_acquisto_esito.php
@@ -0,0 +1,56 @@
+<!-- TITOLO DI PAGINA -->
+<section>
+    <div class="titolo">
+            <h1>
+                <div class="row">
+
+                    
+                    <div class="col-sm-8">
+                        <span>ACQUISTO ED EMISSIONE POLIZZA<br>Esito</span></span>
+                    </div>
+                    <div class="col-sm-4">	
+                        <div class="pager pull-right">
+                            <div class="circle current_page">1</div>
+                            <div class="circle current_page">2</div>
+                            <div class="circle current_page">3</div>
+                         </div>
+                    </div>
+                    
+                </div>
+            </h1>
+    </div>
+</section>
+<!-- TITOLO DI PAGINA -->
+
+<section>
+	<div class="row">
+		<div class="col-sm-12">
+			<div class="messagePanel success hidden-print bgBoxBonif bgBoxSuccess messageFirma">
+				<div class="row">
+					<div class="col-xs-12 linHeighVert"><i class="icon iconBig icon-alert_ok"></i><h3>La polizza n. 10000220713 è emessa da BPM Assicurazioni (TBD)</h3>
+						<div class="row">
+							<div class="col-xs-12">
+								<p></p>
+						</div>
+
+					</div>					
+				</div>
+				
+			</div>
+		</div>
+	</div>
+</section>
+<section>
+	<div class="row boxAction">
+		<div class="col-xs-12"><h3>COSA PUOI FARE ORA</h3></div>
+	</div>
+	<div class="row boxAction">
+		<div class="col-sm-7 hidden-xs"><p>Stampa il contratto</p></div>
+		<div class="col-sm-5 col-xs-12 alignRight"><a href="#" class="btn btn-link"><span>Apri Pdf</span><i class="icon icon-arrow_right" title="icon-freccia_dx"></i></a></div>
+	</div>
+	<div class="row boxAction">
+		<div class="col-sm-7 hidden-xs"><p>Consulta le tue polizze</p></div>
+		<div class="col-sm-5 col-xs-12 alignRight"><a href="#" class="btn btn-link"><span>Vai a polizze</span><i class="icon icon-arrow_right" title="icon-freccia_dx"></i></a></div>
+	</div>
+</section>
+
