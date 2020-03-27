57decadad3d9af70c043c789a1f6dc27f97b8c43
fabdom <62433800+fabdom@users.noreply.github.com>
2020-03-27 15:18:15 +0100

    Web advisory
    esempio di accordion che contiene iframe del pdf e checkbox sulla destra

--- docs_convergenza/include/oggetti/Accordion/Verona/Accordion_001/oggetto.php
+++ docs_convergenza/include/oggetti/Accordion/Verona/Accordion_001/oggetto.php
@@ -1,22 +1,51 @@
 <div id="accordion" class="nosort ui-accordion ui-widget ui-helper-reset" role="tablist">
-		<h3 class="ui-accordion-header ui-state-default ui-accordion-icons ui-corner-all" role="tab" id="ui-id-2" aria-controls="ui-id-3" aria-selected="true" aria-expanded="true" tabindex="0">
-			<i class="icon icon-3x icon-accordion_down"></i>
-			<i class="icon icon-3x icon-accordion_up"></i>
-			<a href="#">La comodità del servizio</a>
-		</h3>
-		<div class="ui-accordion-content ui-helper-reset ui-widget-content ui-corner-bottom ui-accordion-content-active" id="ui-id-3" aria-labelledby="ui-id-2" role="tabpanel" aria-hidden="false" style="display: block;">
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque imperdiet ex id erat pretium, et <strong>accumsan eros pharetra.</strong> In vitae dolor condimentum, fringilla nunc vitae, molestie sem. Nulla maximus fermentum mauris congue pellentesque. Mauris turpis sapien, vestibulum a egestas ut, luctus sed justo. Aenean felis nisl, molestie id sapien ac, lacinia pulvinar est. Cras convallis erat non purus sollicitudin vehicula. Ut sed lacinia augue. Curabitur eleifend faucibus enim, nec volutpat erat sagittis ut. <strong>Sed suscipit, ligula efficitur mattis</strong> suscipit, mauris leo euismod est, ullamcorper placerat sem lacus ac sem. Ut ante arcu, placerat eu ante id, suscipit tristique sapien.</p>
-		</div>
-
-		<h3 class="ui-accordion-header ui-state-default ui-accordion-icons ui-corner-all" role="tab" id="ui-id-4" aria-controls="ui-id-5" aria-selected="false" aria-expanded="false" tabindex="-1"><span class="ui-accordion-header-icon ui-icon ui-icon-triangle-1-e"></span>
-			<i class="icon icon-3x icon-accordion_down"></i>
-			<i class="icon icon-3x icon-accordion_up"></i>
-			<a href="#">I documenti che puoi ricevere</a>
-		</h3>
-		<div class="ui-accordion-content ui-helper-reset ui-widget-content ui-corner-bottom" id="ui-id-5" aria-labelledby="ui-id-4" role="tabpanel" aria-hidden="true" style="display: none;">
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque imperdiet ex id erat pretium, et <strong>accumsan eros pharetra.</strong> In vitae dolor condimentum, fringilla nunc vitae, molestie sem. Nulla maximus fermentum mauris congue pellentesque. Mauris turpis sapien, vestibulum a egestas ut, luctus sed justo. Aenean felis nisl, molestie id sapien ac, lacinia pulvinar est. Cras convallis erat non purus sollicitudin vehicula. Ut sed lacinia augue. Curabitur eleifend faucibus enim, nec volutpat erat sagittis ut. <strong>Sed suscipit, ligula efficitur mattis</strong> suscipit, mauris leo euismod est, ullamcorper placerat sem lacus ac sem. Ut ante arcu, placerat eu ante id, suscipit tristique sapien.</p>
-		</div>
+	<h3 class="ui-accordion-header ui-state-default ui-accordion-icons ui-corner-all" role="tab" id="ui-id-2" aria-controls="ui-id-3" aria-selected="true" aria-expanded="true" tabindex="0">
+		<i class="icon icon-3x icon-accordion_down"></i>
+		<i class="icon icon-3x icon-accordion_up"></i>
+		<a href="#">La comodità del servizio</a>
+	</h3>
+	<div class="ui-accordion-content ui-helper-reset ui-widget-content ui-corner-bottom ui-accordion-content-active" id="ui-id-3" aria-labelledby="ui-id-2" role="tabpanel" aria-hidden="false" style="display: block;">
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque imperdiet ex id erat pretium, et <strong>accumsan eros pharetra.</strong> In vitae dolor condimentum, fringilla nunc vitae, molestie sem. Nulla maximus fermentum mauris congue pellentesque. Mauris turpis sapien, vestibulum a egestas ut, luctus sed justo. Aenean felis nisl, molestie id sapien ac, lacinia pulvinar est. Cras convallis erat non purus sollicitudin vehicula. Ut sed lacinia augue. Curabitur eleifend faucibus enim, nec volutpat erat sagittis ut. <strong>Sed suscipit, ligula efficitur mattis</strong> suscipit, mauris leo euismod est, ullamcorper placerat sem lacus ac sem. Ut ante arcu, placerat eu ante id, suscipit tristique sapien.</p>
 	</div>
+
+	<h3 class="ui-accordion-header ui-state-default ui-accordion-icons ui-corner-all" role="tab" id="ui-id-4" aria-controls="ui-id-5" aria-selected="false" aria-expanded="false" tabindex="-1"><span class="ui-accordion-header-icon ui-icon ui-icon-triangle-1-e"></span>
+		<i class="icon icon-3x icon-accordion_down"></i>
+		<i class="icon icon-3x icon-accordion_up"></i>
+		<a href="#">I documenti che puoi ricevere</a>
+	</h3>
+	<div class="ui-accordion-content ui-helper-reset ui-widget-content ui-corner-bottom" id="ui-id-5" aria-labelledby="ui-id-4" role="tabpanel" aria-hidden="true" style="display: none;">
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque imperdiet ex id erat pretium, et <strong>accumsan eros pharetra.</strong> In vitae dolor condimentum, fringilla nunc vitae, molestie sem. Nulla maximus fermentum mauris congue pellentesque. Mauris turpis sapien, vestibulum a egestas ut, luctus sed justo. Aenean felis nisl, molestie id sapien ac, lacinia pulvinar est. Cras convallis erat non purus sollicitudin vehicula. Ut sed lacinia augue. Curabitur eleifend faucibus enim, nec volutpat erat sagittis ut. <strong>Sed suscipit, ligula efficitur mattis</strong> suscipit, mauris leo euismod est, ullamcorper placerat sem lacus ac sem. Ut ante arcu, placerat eu ante id, suscipit tristique sapien.</p>
+	</div>
+	
+	
+	
+	<h3 class="ui-accordion-header ui-state-default ui-accordion-icons ui-corner-all" role="tab" id="ui-id-4" aria-controls="ui-id-5" aria-selected="false" aria-expanded="false" tabindex="-1"><span class="ui-accordion-header-icon ui-icon ui-icon-triangle-1-e"></span>
+		<i class="icon icon-3x icon-accordion_down"></i>
+		<i class="icon icon-3x icon-accordion_up"></i>
+		<a href="#">Lorem ipsum dolor</a>
+	</h3>
+	<div class="ui-accordion-content ui-helper-reset ui-widget-content ui-corner-bottom" id="ui-id-5" aria-labelledby="ui-id-4" role="tabpanel" aria-hidden="true" style="display: none;">
+		<div class="row">
+			<div class="col-sm-9 col-xs-12">
+				<div class="divForm" id="propostaconsulenza">
+					<div id="documentoPDF" style="margin-bottom:10px;">
+						<iframe scrolling="yes" allowfullscreen="" webkitallowfullscreen="" id="embedId" title="Proposta Commerciale" src="/HT/JS/pdfjs/web/viewer.html?file=%2FWEBHT%2Finvestimenti%2Fconsulenza%2FpropostaDownloadPdf%3Ftype%3Dinline%26tipoDocumento%3DPC" style="height: 400px;width: 100%;"></iframe>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-3 col-xs-12">
+				<input type="checkbox" name="" id="" class="flLeft">
+				<label class="labelConsenso">Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet</label>
+		         
+			</div>
+		</div>
+		
+		
+		<!--p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque imperdiet ex id erat pretium, et <strong>accumsan eros pharetra.</strong> In vitae dolor condimentum, fringilla nunc vitae, molestie sem. Nulla maximus fermentum mauris congue pellentesque. Mauris turpis sapien, vestibulum a egestas ut, luctus sed justo. Aenean felis nisl, molestie id sapien ac, lacinia pulvinar est. Cras convallis erat non purus sollicitudin vehicula. Ut sed lacinia augue. Curabitur eleifend faucibus enim, nec volutpat erat sagittis ut. <strong>Sed suscipit, ligula efficitur mattis</strong> suscipit, mauris leo euismod est, ullamcorper placerat sem lacus ac sem. Ut ante arcu, placerat eu ante id, suscipit tristique sapien.</p-->
+	</div>
+	
+	
+</div>
 	<script type="text/javascript">
 	$(function() {
 		$("#accordion").accordion({
