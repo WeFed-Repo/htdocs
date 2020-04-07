77fc5069f84605650e29e065615b790cb275d597
martapetrella <martollum@gmail.com>
2020-04-07 18:02:01 +0200

    seconda tab ordini e movimenti

--- docs_convergenza/template/tpl_inv_ordini_e_movimenti.php
+++ docs_convergenza/template/tpl_inv_ordini_e_movimenti.php
@@ -78,6 +78,38 @@ $selectMercato = array(
 $tipo_op_1 = array('Acquisto', 'Vendita');
 $tipo_op_2 = array('', 'intraday', 'overnight');
 
+$selectPeriodoFondi = array(
+  'Ultimi 7 giorni',
+  'Ultimi 30 giorni',
+  'Ultimi 3 mesi',
+  'Ultimi 6 mesi',
+);
+
+$selectStatoOrdineFondi = array(
+  'Tutti gli stati',
+  'Eseguito',
+  'Accettato',
+  'In esecuzione',
+);
+
+$selectTipoOperazioneFondi = array(
+  'Tutte le operazioni',
+  'Sottoscrizione',
+  'Rimborso',
+  'Switch',
+  'Rata PAC',
+  'Rimborso programmato',
+  'Reinvest. cedola',
+  'Accredito cedola',
+  'Carico quote',
+  'Scarico quote',
+  'Carico quote fusione',
+  'Scarico quote fusione',
+  'Trasferimenti in ingresso',
+  'Trasferimenti in uscita',
+  'Altro',
+);
+
 ?>
 
 <h2>Ordini e movimenti</h2>
@@ -108,23 +140,6 @@ $tipo_op_2 = array('', 'intraday', 'overnight');
         <div class="row">
           <div class="col-xs-12 col-md-6">
               <div class="form-group">
-                <script type="text/javascript">
-                $(function() {
-                    $("#periodo").mask("99/99/9999");
-                    $("#periodo").datepicker({
-                        minDate: 1,
-                        showOtherMonths: true,
-                        showOn: "button",
-                        prevText: "<i class=\"icon icon-arrow_left\" title=\"icon-arrow_left\"></i>",
-                        nextText: "<i class=\"icon icon-arrow_right\" title=\"icon-arrow_right\"></i>",
-                        buttonImage: "/img/ret/pixel_trasp.gif",
-                        buttonImageOnly: true,
-                        beforeShow: renderPickDateMobile,
-                        onClose: function() { $('#datePickerWrapper').modal('hide'); }
-                    });
-                    appendDatePickerIcon('periodo');
-                });
-                </script>
                 <label class="control-label" for="periodo">Periodo</label>
                 <div class="input-group">
                   <input type="text" id="periodo"
@@ -193,36 +208,16 @@ $tipo_op_2 = array('', 'intraday', 'overnight');
         <!-- FUNZIONI PER FILTRI -->
         <?php include("./parts/table_th_filter.php"); ?>
         <!-- FINE FUNZIONI PER FILTRI -->
-        <script>
-            // Esempio di creazione della tabella di bootstrap
-            $(function(){
-                var tablePortafoglio = $('#tablePortafoglio');
-                tablePortafoglio.bootstrapTable({
-                        onPostBody: function(){
 
-                            // Funzioni da ripetere ad ogni refresh
-                            $(".table-btn-more").not("inited").click(function(){
-                                    $("#layerConfronta").modal("show");
-                                });
-
-                            attivaIconaOperativa("#tablePortafoglio");
-
-                        }
-                });
-
-                // Inizializza i filtri addizionali
-                initThFilter();
-            });
-        </script>
-        <table cellspacing="0" cellpadding="0" border="0"  id="tablePortafoglio" class="sortableTable has-fixed-cols" data-fixed-cols="2">
+        <table cellspacing="0" cellpadding="0" border="0"  id="tableOrdiniMovimenti" class="sortableTable has-fixed-cols" data-fixed-cols="2">
             <thead>
                 <tr>
                     <th class="center"><a class="btn-icon" data-toggle="modal" data-target="#layerLegenda"><i class="icon icon-2x icon-info_fill"></i></a></th>
                     <th class="left filter" data-sortable="true" id="filterTitolo">Titolo / ISIN</th>
                     <th class="left">Tipo operazione</th>
-                    <th class="right">Q.t&agrave; immessa / eseguita</th>
-                    <th class="right">Prezzo immesso / eseguito</th>
-                    <th class="right">Data / Ora</th>
+                    <th class="right">Q.t&agrave; immessa/ eseguita</th>
+                    <th class="right">Prezzo immesso/ eseguito</th>
+                    <th class="right">Data/ Ora</th>
                     <th class="left">Stato ordine</th>
                     <th class="left">Mercato</th>
                     <th class="right">N. ordine sintetico</th>
@@ -251,6 +246,19 @@ $tipo_op_2 = array('', 'intraday', 'overnight');
             </tbody>
         </table>
 
+        <!-- PULSANTIERA SOTTO TABELLA -->
+        <div class="btnWrapper marginTopMedium">
+            <div class="btn-align-left">
+                <a class="btn-link-icon" href="#"><i class="icon icon-excel_filled"></i><span>Scarica in Excel</span></a>
+                <a class="btn-link-icon" href="#"><i class="icon icon-file_pdf_fill"></i><span>Scarica in PDF</span></a>
+            </div>
+            <!--div class="btn-align-right">
+                <a class="btn-link-icon" data-toggle="modal" data-target="#layerCambio"><i class="icon icon_cambi"></i><span>Tasso di cambio</span></a>
+            </div-->
+            <br class="clear">
+        </div>
+        <!-- FINE PULSANTIERA SOTTO TABELLA -->
+
 
       </div>
     </div>
@@ -263,7 +271,112 @@ $tipo_op_2 = array('', 'intraday', 'overnight');
         </h4>
       </div>
       <div id="collapse1c" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading2b" style="height: auto;">
-        2
+
+            <div class="row">
+              <div class="col-xs-12 col-md-6">
+                <div class="form-group">
+                  <label class="control-label">Periodo</label>
+                  <select class="form-control" id="selectPeriodFondi">
+                    <?php auto_input_select( $selectPeriodoFondi ) ?>
+                  </select>
+                </div>
+              </div>
+              <?php if($site == "webank") { ?>
+              <div class="col-xs-12 col-md-6">
+                <div class="form-group">
+                  <label class="control-label">Stato ordine</label>
+                  <select class="form-control" id="selectStatoOrdineFondi">
+                    <?php auto_input_select( $selectStatoOrdineFondi ) ?>
+                  </select>
+                </div>
+              </div>
+              <?php } ?>
+              <div class="col-xs-12 col-md-6">
+                <div class="form-group">
+                  <label class="control-label">Tipi di operazione</label>
+                  <select class="form-control" id="selectTipoOperazioneFondi">
+                    <?php auto_input_select( $selectTipoOperazioneFondi ) ?>
+                  </select>
+                </div>
+              </div>
+              <div class="col-xs-12 col-md-6 no-label">
+      					<div class="btn-align-right">
+      						<a type="button" class="btn btn-primary" href="#">Cerca</a>
+      					</div>
+              </div>
+              <div class="col-xs-12">
+                <p class="note flLeft noFloatMobile">
+                    <span class="flLeft">Ultimo Aggiornamento: <?= date('d/m/Y') ?> ore <?= date('h:i:s') ?></span>
+                    <a href="#" class="no-underline btn-icon flLeft padding-l-m" id="refreshBtn">
+                      <i class="icon icon-2x icon-update"></i>
+                    </a>
+                </p>
+            </div>
+
+            </div>
+
+
+            <!-- TABELLA -->
+            <!-- FUNZIONI PER FILTRI -->
+            <?php include("./parts/table_th_filter.php"); ?>
+            <!-- FINE FUNZIONI PER FILTRI -->
+            <table cellspacing="0" cellpadding="0" border="0"  id="tableOrdiniMovimenti" class="sortableTable has-fixed-cols" data-fixed-cols="2">
+                <thead>
+                  <tr>
+                    <th class="center"><a class="btn-icon" data-toggle="modal" data-target="#layerLegenda"><i class="icon icon-2x icon-info_fill"></i></a></th>
+                    <th class="left filter" data-sortable="true" id="filterTitolo">Fondo / ISIN</th>
+                    <th class="left">Stato ordine</th>
+                    <th class="right">Quote/ azioni</th>
+                    <th class="left">Tipo operazione</th>
+                    <th class="right">Controv. in Euro</th>
+                    <th class="right">Ultimo prezzo</th>
+                    <th class="right">Data Valuta</th>
+                    <th class="right">Data Inserim.</th>
+                  </tr>
+                </thead>
+                <tbody>
+                <?php for($x=0;$x<=10;$x++) { ?>
+                  <tr>
+                      <td class="center">
+                        <a class="btn-icon btn-icon-operativa" data-isin="<?php print (999990 + $x )?>">
+                          <i class="icon icon-2x icon-ico_azioni02A"></i>
+                        </a>
+                      </td>
+                      <td class="left"><a href="#">Ipsum dolor sit amet</a> <?php print rand(1000,1000000)/100;?> </td>
+                      <td class="left"><?php
+                        $a1 = $selectStatoOrdineFondi;
+                        array_shift($a1);
+                        $a1[] = 'Annullato';
+                        echo $a1[array_rand($a1)];
+                      ?></td>
+                      <td class="left"><?php print rand(1000,1000000)/100;?></td>
+                      <td class="left"><?php
+                        $a2 = $selectTipoOperazioneFondi;
+                        array_shift($a2);
+                        echo $a2[array_rand($a2)];
+                      ?></td>
+                      <td class="right"><?php print rand(1000,1000000)/100;?></td>
+                      <td class="right"><?php print rand(1000,1000000)/100;?></td>
+                      <td class="right">20/01/2020</td>
+                      <td class="right">20/01/2020</td>
+                  </tr>
+                <?php } ?>
+                </tbody>
+            </table>
+
+            <!-- PULSANTIERA SOTTO TABELLA -->
+            <div class="btnWrapper marginTopMedium">
+                <div class="btn-align-left">
+                    <a class="btn-link-icon" href="#"><i class="icon icon-excel_filled"></i><span>Scarica in Excel</span></a>
+                    <a class="btn-link-icon" href="#"><i class="icon icon-file_pdf_fill"></i><span>Scarica in PDF</span></a>
+                </div>
+                <!--div class="btn-align-right">
+                    <a class="btn-link-icon" data-toggle="modal" data-target="#layerCambio"><i class="icon icon_cambi"></i><span>Tasso di cambio</span></a>
+                </div-->
+                <br class="clear">
+            </div>
+            <!-- FINE PULSANTIERA SOTTO TABELLA -->
+
       </div>
     </div>
     <div class="panel panel-default">
@@ -281,12 +394,34 @@ $tipo_op_2 = array('', 'intraday', 'overnight');
     </div>
   </div>
 </div>
-<script>
-//inizializzazione dei tab-accordion
-//TRASFORMA IL TAB DI CONTENUTO IN ACCORDION SU MOBILE
-//possono coesistere più gruppo di tab/accordion in pagina
-//parametri: id contenitore e incice tab ch edeve essere aperto al load
-$(function(){
-  resizeTab('secondTab',0);
-});
+
+<!-- INIZIALIZZAZIONI -->
+<script type="text/javascript">
+  $(function(){
+    resizeTab('secondTab',0);
+    $("#periodo").mask("99/99/9999");
+    $("#periodo").datepicker({
+        minDate: 1,
+        showOtherMonths: true,
+        showOn: "button",
+        prevText: "<i class=\"icon icon-arrow_left\" title=\"icon-arrow_left\"></i>",
+        nextText: "<i class=\"icon icon-arrow_right\" title=\"icon-arrow_right\"></i>",
+        buttonImage: "/img/ret/pixel_trasp.gif",
+        buttonImageOnly: true,
+        beforeShow: renderPickDateMobile,
+        onClose: function() { $('#datePickerWrapper').modal('hide'); }
+    });
+    appendDatePickerIcon('periodo');
+    var tableOrdiniMovimenti = $('#tableOrdiniMovimenti');
+    tableOrdiniMovimenti.bootstrapTable({
+      onPostBody: function(){
+        // Funzioni da ripetere ad ogni refresh
+        $(".table-btn-more").not("inited").click(function(){
+          $("#layerConfronta").modal("show");
+        });
+
+      }
+    });
+    initThFilter();
+  });
 </script>
