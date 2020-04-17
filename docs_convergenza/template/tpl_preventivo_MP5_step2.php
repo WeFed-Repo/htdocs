e48ca6702bdee116a836fd0889fd6f2bafd65ca2
sstrepponi <62430689+sstrepponi@users.noreply.github.com>
2020-04-17 17:28:55 +0200

    assicurazioni covea
    assicurazioni covea preventivatore

--- docs_convergenza/template/tpl_preventivo_MP5_step2.php
+++ docs_convergenza/template/tpl_preventivo_MP5_step2.php
@@ -3,8 +3,6 @@
     <div class="titolo">
             <h1>
                 <div class="row">
-
-                    
                     <div class="col-sm-8">
                         <span>Richiedi il preventivo<br><span>Consensi privacy</span></span>
                     </div>
@@ -85,21 +83,15 @@
     <form method="post" action="" id="aggiungiAssicurazioni">
         <h3 class="titleSection titleForm">Beni da assicurare</h3>
         <p>Puoi indicare fino ad un massimo di due fabbricati (a scelta tra abitazione abituale, saltuaria e box) e una persona (soggetto fisico).</p>        
-        <div class="formWrapper">
+        <div class="formWrapper garanzia">
             <div class="form-group">
                 <div class="row">
-                    <div class="form-field-input col-xs-12 col-sm-46">
+                    <div class="form-field-input col-xs-12 col-sm-12">
                         <label class="control-label">Bene/soggetto da assicurare</label>
                         <div class="form-field">
                             <div class="row">
-                                <div class="form-field-input col-xs-6 col-sm-6">
-                                <select name="" class="form-control bene-assicurato" id="bene-assicurato_0">
-                                    <option id="" selected="selected" value="">Seleziona</option>             
-                                    <option id="" value="DA">Dimora abituale</option>
-                                    <option id="" value="DS">Dimora saltuaria</option>
-                                    <option id="" value="B">Box</option>
-                                    <option id="" value="SF">Soggetto fisico</option>
-                                </select>
+                                <div class="form-field-input col-xs-12 col-sm-6">
+                                    <select name="" class="form-control bene-assicurato" id="bene-assicurato_0"></select>
                                 </div>
                             </div>
                         </div>
@@ -107,8 +99,8 @@
                 </div>
             </div>
             <div class="add-wrapper"></div>
-         </div>
-         <div class="formWrapper hidden">
+        </div>
+        <div class="formWrapper garanzia hidden">
             <div class="form-group">
                <div class="row">
                     <div class="col-xs-12 col-sm-6">
@@ -130,90 +122,74 @@
                 </div>
             </div>
             <div class="add-wrapper"></div>
-         </div>
-         <div class="formWrapper hidden">
-            <div class="form-group">
-               <div class="row">
-                    <div class="col-xs-12 col-sm-6">
-                        <a class="link-text btn-aggiungi" href="javascript:;"><i class="icon icon-f-row_expand"></i> Aggiungi bene</a>
-                        <a class="link-text btn-elimina hidden" href="javascript:;"><i class="icon icon-f-row_contract"></i> Elimina bene</a>
-                    </div>
-                </div>
-                <div class="row next-select hidden" >
-                    <div class="form-field-input col-xs-12 col-sm-46">
-                        <label class="control-label">Bene/soggetto da assicurare</label>
-                        <div class="form-field">
-                            <div class="row">
-                                <div class="form-field-input col-xs-6 col-sm-6">
-                                    <select name="" class="form-control bene-assicurato" id="bene-assicurato_2"></select>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="add-wrapper"></div>
-         </div>
+        </div>
     </form>
 </section>
 <script>
   	//chiamata per caricare html aggiunto
-    var  selectAggiungiAss= $('.bene-assicurato'),
+    var   selectAggiungiAss= $('.bene-assicurato'), //select di selezione
           btnAggiungi = $('.btn-aggiungi'),
           btnElimina = $('.btn-elimina'),
-          arrayOption = [{value:"DA",text:"Dimora abituale"}, {value:"DS",text:"Dimora saltuaria"}, {value:"B",text:"box"},{ value:"SF", text:"Soggetto fisico"}],
-          arrayOptionSelected = [],
-          nextArrayOptions = [],
-          optionList = []
-          
-    $.each(selectAggiungiAss, function(index, value ) {
-        $(this).on("change", function() {
-            var  selectAggiungiAss = $(this),
-            urlToCall;
-            arrayOptionSelected = [];
-            nextArrayOptions[index] = [];
-            optionList[index] = [];
-            arrayOptionSelected.push({value: $(this).val(), text: $(this).find("option:selected").text() });
-            nextArrayOptions[index] = arrayOption.filter(function(value,index) {
-              return value =! arrayOptionSelected[index] 
-            });
-            optionList[index] = nextArrayOptions[index].map(el => {return "<option value=" + el["value"] +">" + el["text"] +"</option>"})
-            $("#bene-assicurato_" + (index+1)).html('<option id="" selected="selected" value="">Seleziona</option>' + optionList[index]);
-       
-        switch (selectAggiungiAss.val()) {
-    	 	case 'DA':
-            case 'DS':
-            urlToCall = "/include/aggiungiDimora.html";
-    	 	break;
-
-    	 	case 'B':
-                urlToCall = "/include/aggiungiBox.html";
-    	 	break;
-
-    	 	case 'SF':
-            urlToCall = "/include/aggiungiPersona.html";
-    	 	break;
-            default:
-            break;
-    	 	
-    	}
+          arrayOption = [  // voci che possono essere aggiunte
+              {value:"DA",text:"Dimora abituale",  type: "F" }, 
+              {value:"DS",text:"Dimora saltuaria", type: "F" }, 
+              {value:"B",text:"box", type: "F"},
+              {value:"SF", text:"Soggetto fisico", type: "P"}],
+          arrayOptionSelect = []
         
-        $.ajax({
-	          type: "POST",
+    
+    var setSelect = function(idSelect) {
+        var wrapper = $("#" + idSelect),
+            optionHtml = "<option selected=\"selected\" value=\"\">Seleziona</option>"
+        arrayOption.map (el => {
+            optionHtml +=  "<option value=" + el["value"]  + " " + "data-type=" + el["type"]  +">" + el["text"] +"</option>"
+        })
+        wrapper.append(optionHtml);
+    }
+    //al load popolo la prima select
+    setSelect("bene-assicurato_0");
+    //alla selezione di una categoria aggiungo o tolgo dall'array di partenza
+    $.each(selectAggiungiAss, function(index, value ) {
+        
+        $(this).on("change", function() {
+            //popolo l'array per l'eventuale select successiva
+            valSelected = $(this).find("option:selected").val();
+            arrayOptionSelect[index] = arrayOption.filter((element) => element.value !==  valSelected);
+            console.log(arrayOptionSelect[0]);
+            
+            //Chiamata ad html relativo con switch di contenuto
+                switch (valSelected) {
+                    case 'DA':
+                    case 'DS':
+                    urlToCall = "/include/aggiungiDimora.html";
+                    break;
+ 
+                    case 'B':
+                    urlToCall = "/include/aggiungiBox.html";
+                    break;
+
+                    case 'SF':
+                    urlToCall = "/include/aggiungiPersona.html";
+                     break;
+                    default:
+                    break;
+                }
+            $.ajax({
+	          type: "GET",
 	              url: urlToCall,
 	              dataType: 'html',
 	              success: function(result){
-	                var addWrapper = selectAggiungiAss.closest('.formWrapper').find('.add-wrapper'),
-                        nextFormWrapper =  selectAggiungiAss.closest('.formWrapper').next('.formWrapper').eq(0);
-                    addWrapper.html("")
-    	            addWrapper.html(result);
-                    nextFormWrapper.removeClass("hidden");
-                }
+	                //nextFormWrapper =  selectAggiungiAss.closest('.formWrapper').next('.formWrapper').eq(0);
+                    $('.add-wrapper').eq(index).html(result);
+                    $('.formWrapper.hidden').eq(index).removeClass("hidden").addClass("visible");
+    	        }
+             })
         })
-       })
-        })
-      
-      
+        
+    
+    })
+    
+      /*
       btnAggiungi.on('click', function(){
         
         //TOGLIE NELLA SELECT LA CATEGORIA GIA' SELEZIONATA
@@ -227,6 +203,6 @@
         $(this).closest('.formWrapper').find(".next-select").addClass("hidden");;
         $(this).addClass("hidden");
         
-     })
+      })*/
 
 </script>
