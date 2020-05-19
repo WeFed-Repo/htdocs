519d25485e68055ed059334333a3544fc3ed97af
davidecioetto <davide.cioetto@alten.it>
2020-05-19 16:15:20 +0200

    Update varie rimaste indietro

--- docs_aletti/src/components/pages/Testiframe/index.js
+++ docs_aletti/src/components/pages/Testiframe/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import {ambiente} from "functions/genericVars";
 import "./style.scss";
+import {ambiente} from "functions/genericVars";
 
 /*
 
@@ -11,19 +11,21 @@ URL IFRAME MIFID: "/promotori/dashboard/goToMifidQuest"
 export default class extends Component {
 
     componentDidMount() {
-
-       window.setMifid = function(obj){
-           console.log(obj)
-       }
-
+        window.setMifid = function(obj){
+                alert("Oggetto ricevuto: verra' mostrato in console").
+                console.log("-------- OGGETTO RICEVUTO --------");
+                console.log(obj);
+                console.log("----- FINE OGGETTO RICEVUTO ------");
+        }
+     
     }
 
-    // Associa un src differente a seconda di dove si invoca l'iframe
-    render()  {
+    render(){
+        // Associa un src differente a seconda di dove si invoca l'iframe
         let iframeSrc = (ambiente.name==="locale")? "/_iframe_testing.htm" : "/promotori/dashboard/goToMifidQuest";
+
         return(
             <iframe className="iframe-resp" name="iframe_mifid" src={iframeSrc}></iframe>
         )
     }
-
 }
