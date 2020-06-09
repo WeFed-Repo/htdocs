3a2c6925546f3e1f5038dc3d3fa9d1d61738437e
martapetrella <martollum@gmail.com>
2020-06-09 15:54:41 +0200

    aggiutna links ALETTI

--- docs_convergenza/template/F24index.php
+++ docs_convergenza/template/F24index.php
@@ -13,13 +13,13 @@
 	<!-- / JS -->
 	<style>
 		body #main.librerie h2 {border-bottom: solid 4px #ccc;margin-bottom:20px;}
-		#main.librerie .elenco-oggetti li .links {width:100px!important}
+		/*#main.librerie .elenco-oggetti li .links {width:150px!important}*/
 		#main.librerie ul li a {width:45px!important}
 	</style>
 </head>
 
 <body>
-	<header class="librerie"> 
+	<header class="librerie">
 		<div class="wrapper">
 			<h1>Librerie convergenza Nuove APP</h1>
 		</div>
@@ -38,103 +38,116 @@
 						<div class="links">
 							<a href="/template/F24_esitoF24Vuoto.php?bank=WB" target="_blank" class="mi">WE</a>
 							<a href="/template/F24_esitoF24Vuoto.php?bank=HT" target="_blank" class="vr">YOU</a>
+							<a href="/template/F24_esitoF24Vuoto.php?bank=AL" target="_blank" class="al">AL</a>
 						</div>
 					</li>
-					
+
 					<li>
 						<span>02-F24 Inseriti - Mostra ultimi (non vuoto)</span>
 						<div class="links">
 							<a href="/template/F24_esitoF24.php?bank=WB" target="_blank" class="mi">WE</a>
 							<a href="/template/F24_esitoF24.php?bank=HT" target="_blank" class="vr">YOU</a>
+							<a href="/template/F24_esitoF24.php?bank=AL" target="_blank" class="al">AL</a>
 						</div>
 					</li>
-					
+
 					<li>
 						<span>03-Nuovo modulo - Step 1</span>
 						<div class="links">
 							<a href="/template/F24_nuovo.php?bank=WB" target="_blank" class="mi">WE</a>
 							<a href="/template/F24_nuovo.php?bank=HT" target="_blank" class="vr">YOU</a>
+							<a href="/template/F24_nuovo.php?bank=AL" target="_blank" class="al">AL</a>
 						</div>
 					</li>
-					
+
 					<li>
 						<span>04-Nuovo modulo - Step 1 (Errore in modale)</span>
 						<div class="links">
 							<a href="/template/F24_esito.php?bank=WB" target="_blank" class="mi">WE</a>
 							<a href="/template/F24_esito.php?bank=HT" target="_blank" class="vr">YOU</a>
+							<a href="/template/F24_esito.php?bank=AL" target="_blank" class="al">AL</a>
 						</div>
 					</li>
-										
+
 					<li>
 						<span>05-Nuovo modulo - Step 1 (rubrica)</span>
 						<div class="links">
 							<a href="/template/F24_rubrica.php?bank=WB" target="_blank" class="mi">WE</a>
 							<a href="/template/F24_rubrica.php?bank=HT" target="_blank" class="vr">YOU</a>
+							<a href="/template/F24_rubrica.php?bank=AL" target="_blank" class="al">AL</a>
 						</div>
 					</li>
-					
+
 					<li>
 						<span>06-Nuovo modulo - Step 1 (rubrica - modifica)</span>
 						<div class="links">
 							<a href="/template/F24_modifica.php?bank=WB" target="_blank" class="mi">WE</a>
 							<a href="/template/F24_modifica.php?bank=HT" target="_blank" class="vr">YOU</a>
+							<a href="/template/F24_modifica.php?bank=AL" target="_blank" class="al">AL</a>
 						</div>
 					</li>
-					
-					
+
+
 					<li>
 						<span>07-Nuovo modulo - Step 2</span>
 						<div class="links">
 							<a href="/template/F24_step2.php?bank=WB" target="_blank" class="mi">WE</a>
 							<a href="/template/F24_step2.php?bank=HT" target="_blank" class="vr">YOU</a>
+							<a href="/template/F24_step2.php?bank=AL" target="_blank" class="al">AL</a>
 						</div>
 					</li>
-										
+
 					<li>
 						<span>08-Nuovo modulo - Step 2 (modale di Ricerca codici identificativi)</span>
 						<div class="links">
 							<a href="/template/F24_lente1.php?bank=WB" target="_blank" class="mi">WE</a>
 							<a href="/template/F24_lente1.php?bank=HT" target="_blank" class="vr">YOU</a>
+							<a href="/template/F24_lente1.php?bank=AL" target="_blank" class="al">AL</a>
 						</div>
 					</li>
-					
+
 					<li>
 						<span>09-Nuovo modulo - Step 2 (modale di Ricerca codici ufficio sezione Erario)</span>
 						<div class="links">
 							<a href="/template/F24_lente2.php?bank=WB" target="_blank" class="mi">WE</a>
 							<a href="/template/F24_lente2.php?bank=HT" target="_blank" class="vr">YOU</a>
+							<a href="/template/F24_lente2.php?bank=AL" target="_blank" class="al">AL</a>
 						</div>
 					</li>
-										
+
 					<li>
 						<span>10-Nuovo modulo (step 3)</span>
 						<div class="links">
 							<a href="/template/F24_step3.php?bank=WB" target="_blank" class="mi">WE</a>
 							<a href="/template/F24_step3.php?bank=HT" target="_blank" class="vr">YOU</a>
+							<a href="/template/F24_step3.php?bank=AL" target="_blank" class="al">AL</a>
 						</div>
 					</li>
-										
+
 					<li>
 						<span>11-Nuovo modulo - Step 3 (modale di Ricerca codici tributo F24 Semplificato)</span>
 						<div class="links">
 							<a href="/template/F24_lente3.php?bank=WB" target="_blank" class="mi">WE</a>
 							<a href="/template/F24_lente3.php?bank=HT" target="_blank" class="vr">YOU</a>
+							<a href="/template/F24_lente3.php?bank=AL" target="_blank" class="al">AL</a>
 						</div>
 					</li>
-					
+
 					<li>
 						<span>12-Nuovo modulo - errore</span>
 						<div class="links">
 							<a href="/template/F24_errore.php?bank=WB" target="_blank" class="mi">WE</a>
 							<a href="/template/F24_errore.php?bank=HT" target="_blank" class="vr">YOU</a>
+							<a href="/template/F24_errore.php?bank=AL" target="_blank" class="al">AL</a>
 						</div>
 					</li>
-					
+
 					<li>
 						<span>13-Nuovo modulo - errore2</span>
 						<div class="links">
 							<a href="/template/F24_errore2.php?bank=WB" target="_blank" class="mi">WE</a>
 							<a href="/template/F24_errore2.php?bank=HT" target="_blank" class="vr">YOU</a>
+							<a href="/template/F24_errore2.php?bank=AL" target="_blank" class="al">AL</a>
 						</div>
 					</li>
 				</ul>
