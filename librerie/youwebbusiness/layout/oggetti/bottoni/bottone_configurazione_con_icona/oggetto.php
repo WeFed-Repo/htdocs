4653b6a9a22600f2e4b7f033f6205f53cac7be41
martapetrella <martollum@gmail.com>
2020-06-25 14:27:34 +0200

    fix icona accordion

--- librerie/youwebbusiness/layout/oggetti/bottoni/bottone_configurazione_con_icona/oggetto.php
+++
@@ -1,4 +0,0 @@
-<button class="btn btn-primary btn-config" onclick="window.location.href='strutt_priv.php?tpl=tpl_home_configurator.php'">
-    <img class="icon icon-autorizza float-left" src="./fe/img/icon/icon-5.svg">
-    Configura homepage
-</button>
---
+++ librerie/youwebbusiness/layout/oggetti/bottoni/plus-minus/oggetto.php
@@ -0,0 +1,7 @@
+<!-- buttons -->
+
+<div class="ico-btn plus-minus"></div>
+
+<div class="ico-btn plus-minus is-active"></div>
+
+<p><br /><br />NOTE: usare la classe ".is-active" per cambiare stato al bottone</p>
