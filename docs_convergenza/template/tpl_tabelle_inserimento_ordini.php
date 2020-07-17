ecae01bdbd9facbe509f00dd57e670fc856013b2
sstrepponi <62430689+sstrepponi@users.noreply.github.com>
2020-07-17 08:10:00 +0200

    tpl tabelle inserimento ordini
    tpl tabelle inserimento ordini

---
+++ docs_convergenza/template/tpl_tabelle_inserimento_ordini.php
@@ -0,0 +1,768 @@
+<!-- ######## This is a comment ######## -->
+<section>
+	<form class="formGenerico borderFormRounded output mww-dom-component webank-component-checkbox" role="form" name="saveForm">
+		<div class="form-group">
+			<div class="row">
+				<div class="col-sm-6">
+					<label class="control-label-output">
+						Titolo
+					</label>
+					<span class="output">
+						MGT - ETF GLOBAL TITANS 50
+					</span>
+				</div>
+				<div class="col-sm-6">
+					<label class="control-label-output">
+						Mercato
+					</label>
+					<span class="output">
+						ETFPlus
+					</span>
+				</div>
+			</div>
+			 <br class="clear" />
+			<div class="row">
+				<div class="col-sm-6">
+					<label class="control-label-output">
+						Tipo di ordine
+					</label>
+					<span class="output">
+						Acquisto
+					</span>
+				</div>
+				<div class="col-sm-6">
+					<label class="control-label-output">
+						Quantit&agrave;
+					</label>
+					<span class="output">
+						10
+					</span>
+				</div>
+			</div>
+			<br class="clear" />
+			<div class="row">
+				<div class="col-sm-6">
+					<label class="control-label-output">
+						Prezzo immesso
+					</label>
+					<span class="output">
+						43,105 EUR
+					</span>
+				</div>
+				<div class="col-sm-6">
+					<label class="control-label-output">
+						Controvalore teorico
+					</label>
+					<span class="output">
+						n.d. &euro;
+					</span>
+				</div>
+			</div>
+			<br class="clear" hidden="" />
+			<div class="row" hidden="">
+				<div class="col-sm-6">
+					<label class="control-label-output">
+						Prezzo Tel quel stimato
+					</label>
+				</div>
+				<div class="col-sm-6">
+					<label class="control-label-output">
+						Controvalore prenotato stimato
+					</label>
+				</div>
+			</div>
+			<br class="clear" />
+			<div class="row" hidden="">
+				<div class="col-sm-6" hidden="">
+					<label class="control-label-output">
+						Margine %
+					</label>
+				</div>
+				<div class="col-sm-6" hidden="">
+					<label class="control-label-output">
+						Variazione margine
+					</label>
+				</div>
+				<div class="col-sm-6" hidden="">
+					<label class="control-label-output">
+						Ctv margine teorico
+					</label>
+				</div>
+			</div>
+			<br class="clear" hidden="" />
+			<div class="row" hidden="">
+				<div class="col-sm-6">
+					<label class="control-label-output">
+						Condizione
+					</label>
+				</div>
+				<div class="col-sm-6">
+					<label class="control-label-output">
+						Prezzo soglia
+					</label>
+				</div>
+			</div>
+			<br class="clear" hidden="" />
+			<div class="row">
+				<div class="col-sm-6">
+					<label class="control-label-output">
+						Fase di mercato
+					</label>
+					<span class="output">
+						Continua
+					</span>
+				</div>
+				<div class="col-sm-6">
+					<label class="control-label-output">
+						Validit&agrave; (fino al)
+					</label>
+					<span class="output">
+						16/07/2020
+					</span>
+				</div>
+			</div>
+			<br class="clear" />
+			<div class="row" hidden="">
+				<div class="col-sm-6">
+					<label class="control-label-output">
+						Scadenza
+					</label>
+				</div>
+			</div>
+			<br class="clear" hidden="" />
+			<div class="row" hidden="">
+				<div class="col-sm-6" hidden="">
+					<label class="control-label-output">
+						Stop loss
+					</label>
+				</div>
+				<div class="col-sm-6" hidden="">
+					<label class="control-label-output">
+						Take profit
+					</label>
+				</div>
+			</div>
+			<br class="clear" hidden="" />
+			<div class="row" hidden="">
+				<div class="col-sm-6">
+					<label class="control-label-output">
+						Soglia inferiore
+						<br />
+						(Ultimo prezzo &lt;=)
+					</label>
+				</div>
+				<div class="col-sm-6">
+					<label class="control-label-output">
+						Soglia superiore
+						<br />
+						(Ultimo prezzo &gt;=)
+					</label>
+				</div>
+			</div>
+			<br class="clear" hidden="" />
+			<div class="row" hidden="">
+				<div class="col-sm-6">
+					<label class="control-label-output">
+						Scadenza
+					</label>
+				</div>
+				<div class="col-sm-6">
+					<label class="control-label-output">
+						Fase
+					</label>
+				</div>
+			</div>
+			<br class="clear" hidden="" />
+			<div class="row" hidden="">
+				<div class="col-sm-6">
+					<label class="control-label-output">
+						Canale di notifica
+					</label>
+				</div>
+				<div class="col-sm-6">
+					&nbsp;
+				</div>
+			</div>
+		</div>
+
+		<section>
+			<p><strong>Strumento finanziario: ETF GLOBAL TITANS 50 Controvalore dell'operazione: n.d. &euro;</strong></p>
+		
+			<!--ROW --->
+			<div class="row">
+				<div class="col-sm-12">
+						<strong>
+							Legenda
+						</strong>
+						<br />
+						I costi e oneri di seguito riportati rappresentano le commissioni massime applicabili e non tengono conto di eventuali sconti concordati
+						<br />
+						<strong>
+							Costi per Prodotto:
+						</strong>
+						<br />
+						tutti i costi e gli oneri associati alla realizzazione e gestione degli strumenti/prodotti finanziari
+						<br />
+						<strong>
+							Costi per Servizio:
+						</strong>
+						<br />
+						tutti i costi e gli oneri connessi applicati dalla Banca relativi ai servizi di investimento e/o servizi accessori prestati al cliente
+						<br />
+						<strong>
+							Incentivi:
+						</strong>
+						<br />
+						retrocessione che la Banca percepisce dalle Societ&agrave; prodotto sotto forma di quota parte dei costi sostenuti dal cliente o come prestazioni non monetarie
+				</div>
+			</div>	
+			<div class="row">
+				<div class="col-sm-6">
+					<div class="headerContainerNoBootS">
+						<div class="tableContainerNoBootS">
+								<table class="riepilogo">
+								<thead>
+									<tr>
+										<th class="left intestazione">
+											Costi e oneri su base annua
+										</th>
+										<th class="right">
+											Valore &euro;
+										</th>
+										<th class="right">
+											Valore %
+										</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr class="header">
+										<td class="left">
+											Costi per servizio
+										</td>
+										<td class="right">
+											10,00 &euro;
+										</td>
+										<td class="right">
+											2,32 &euro;
+										</td>
+									</tr>
+									<tr class="pari">
+										<td class="left">
+											Spese una tantum - INGRESSO
+										</td>
+										<td class="right">
+											0,00 &euro;
+										</td>
+										<td class="right">
+											0,00 &euro;
+										</td>
+									</tr>
+									<tr class="dispari">
+										<td class="left">
+											Spese correnti
+										</td>
+										<td class="right">
+											0,00 &euro;
+										</td>
+										<td class="right">
+											0,00 &euro;
+										</td>
+									</tr>
+									<tr class="pari">
+										<td class="left">
+											Spese per operazioni
+										</td>
+										<td class="right">
+											10,00 &euro;
+										</td>
+										<td class="right">
+											2,32 &euro;
+										</td>
+									</tr>
+									<tr class="dispari">
+										<td class="left">
+											Costi accessori
+										</td>
+										<td class="right">
+											0,00 &euro;
+										</td>
+										<td class="right">
+											0,00 &euro;
+										</td>
+									</tr>
+									<tr class="pari">
+										<td class="left">
+											Spese per servizi accessori
+										</td>
+										<td class="right">
+											0,00 &euro;
+										</td>
+										<td class="right">
+											0,00 &euro;
+										</td>
+									</tr>
+									<tr class="header">
+										<td class="left">
+											Costi per prodotto
+										</td>
+										<td class="right">
+											1,81 &euro;
+										</td>
+										<td class="right">
+											0,42 &euro;
+										</td>
+									</tr>
+									<tr class="pari">
+										<td class="left">
+											Spese una tantum - INGRESSO
+										</td>
+										<td class="right">
+											0,00 &euro;
+										</td>
+										<td class="right">
+											0,00 &euro;
+										</td>
+									</tr>
+									<tr class="dispari">
+										<td class="left">
+											Spese correnti
+										</td>
+										<td class="right">
+											1,72 &euro;
+										</td>
+										<td class="right">
+											0,40 &euro;
+										</td>
+									</tr>
+									<tr class="pari">
+										<td class="left">
+											Spese per operazioni
+										</td>
+										<td class="right">
+											0,09 &euro;
+										</td>
+										<td class="right">
+											0,02 &euro;
+										</td>
+									</tr>
+									<tr class="dispari">
+										<td class="left">
+											Costi accessori
+										</td>
+										<td class="right">
+											0,00 &euro;
+										</td>
+										<td class="right">
+											0,00 &euro;
+										</td>
+									</tr>
+								</tbody>
+								<tfoot>
+									<tr class="totali">
+										<td class="left">
+											TOTALE
+										</td>
+										<td class="right">
+											11,81 &euro;
+										</td>
+										<td class="right">
+											2,74 &euro;
+										</td>
+									</tr>
+								</tfoot>
+							</table>
+						</div>
+					</div>
+				</div>
+			<div class="col-sm-6">
+					<div class="headerContainerNoBootS">
+						<div class="tableContainerNoBootS">
+						<section>
+						<table class="riepilogo">
+						<thead>
+							<tr>
+								<th class="left intestazione">
+									Costi e oneri su base annua
+								</th>
+								<th class="right">
+									Valore &euro;
+								</th>
+								<th class="right">
+									Valore %
+								</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr class="header">
+								<td class="left">
+									Costi per servizio
+								</td>
+								<td class="right">
+									11,81 &euro;
+								</td>
+								<td class="right">
+									2,74 &euro;
+								</td>
+							</tr>
+							<tr class="pari">
+								<td class="left">
+									Spese una tantum - Uscita*
+								</td>
+								<td class="right">
+									0,00 &euro;
+								</td>
+								<td class="right">
+									0,00 &euro;
+								</td>
+							</tr>
+							<tr class="dispari">
+								<td class="left">
+									Spese per operazioni
+								</td>
+								<td class="right">
+									10,00 &euro;
+								</td>
+								<td class="right">
+									2,32 &euro;
+								</td>
+							</tr>
+							<tr class="pari">
+								<td class="left">
+									Costi per prodotto
+								</td>
+								<td class="right">
+									0,00 &euro;
+								</td>
+								<td class="right">
+									0,00 &euro;
+								</td>
+							</tr>
+							<tr class="dispari">
+								<td class="left">
+									Spese una tantum - Uscita*
+								</td>
+								<td class="right">
+									0,00 &euro;
+								</td>
+								<td class="right">
+									0,00 &euro;
+								</td>
+							</tr>
+						</tbody>
+					</table>
+					* Le commissioni di uscita esposte sono le massime che potranno essere applicate in caso di uscita dall'investimento durante il primo anno.
+					
+					</section>
+					<br>
+					<section>
+						<table class="riepilogo">
+							<thead>
+								<tr>
+									<th class="left intestazione">
+										Incentivi monetari su base annua
+									</th>
+									<th class="right">
+										Valore &euro;
+									</th>
+									<th class="right">
+										Valore %
+									</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr class="pari">
+									<td class="left">
+										Una Tantum
+									</td>
+									<td class="right">
+										0,00 &euro;
+									</td>
+									<td class="right">
+										0,00 &euro;
+									</td>
+								</tr>
+								<tr class="dispari">
+									<td class="left">
+										Ricorrenti
+									</td>
+									<td class="right">
+										0,00 &euro;
+									</td>
+									<td class="right">
+										0,00 &euro;
+									</td>
+								</tr>
+							</tbody>
+							<tfoot>
+								<tr class="totali">
+									<td class="left">
+										TOTALE
+									</td>
+									<td class="right">
+										0,00 &euro;
+									</td>
+									<td class="right">
+										0,00 &euro;
+									</td>
+								</tr>
+							</tfoot>
+						</table>
+					</section>
+						</div>
+					</div>
+			</div>
+			</div>
+		</section>
+		<section>
+			<div class="row">
+				<div class="col-sm-6">
+						<div class="">
+							<p>
+								<strong>
+									Impatto dei costi sul rendimento
+								</strong>
+							</p>
+							La tabella rappresenta un'illustrazione dell'effetto dei costi monetari sul rendimento degli
+							<br />
+							strumenti/prodotti, in ipotesi di rendimento zero
+							<br />
+							<br />
+							Pertanto il Controvalore(Ctv) netto &egrave; ottenuto come differenza algebrica tra Controvalore
+							<br />
+							lordo e i costi monetari applicabili
+							<br />
+							<br />
+						</div>
+						<div class="headerContainerNoBootS">
+							<div class="tableContainerNoBootS">
+							
+						
+							</div>
+						</div>
+				</div>
+				<div class="col-sm-6">
+					<div class="headerContainerNoBootS">
+						<div class="tableContainerNoBootS">
+							<table class="riepilogo">
+								<tbody>
+									<tr class="pari">
+										<td class="left">
+											Controvalore teorico operazione
+										</td>
+										<td class="right">
+											431,05 &euro;
+										</td>
+										<td class="right">
+											n.d. &euro;
+										</td>
+									</tr>
+									<tr class="dispari">
+										<td class="left">
+											Commissione di uscita*
+										</td>
+										<td class="right">
+											0,00 &euro;
+										</td>
+										<td class="right">
+											0,00 &euro;
+										</td>
+									</tr>
+									<tr class="pari">
+										<td class="left">
+											Commissione di ingresso
+										</td>
+										<td class="right">
+											0,00 &euro;
+										</td>
+										<td class="right">
+											0,00 &euro;
+										</td>
+									</tr>
+									<tr class="dispari">
+										<td class="left">
+											Altre commissioni
+										</td>
+										<td class="right">
+											-11,81 &euro;
+										</td>
+										<td class="right">
+											-2,74 &euro;
+										</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+					</div>
+					* Le commissioni di uscita esposte sono le massime che potranno essere applicate in caso di uscita dall'investimento durante il primo anno.
+					<div class="headerContainerNoBootS" hidden="">
+						<div class="tableContainerNoBootS">
+							<table class="riepilogo">
+								<thead>
+									<tr>
+										<th class="left intestazione">
+											Costi e oneri su base annua
+										</th>
+										<th class="right">
+											Valore &euro;
+										</th>
+										<th class="right">
+											Valore %
+										</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr class="header">
+										<td class="left">
+											Costi per servizio
+										</td>
+										<td class="right">
+											- &euro;
+										</td>
+										<td class="right">
+											- %
+										</td>
+									</tr>
+									<tr class="pari">
+										<td class="left">
+											Spese una tantum - Uscita*
+										</td>
+										<td class="right">
+											- &euro;
+										</td>
+										<td class="right">
+											- %
+										</td>
+									</tr>
+									<tr class="dispari">
+										<td class="left">
+											Spese per operazioni
+										</td>
+										<td class="right">
+											- &euro;
+										</td>
+										<td class="right">
+											- %
+										</td>
+									</tr>
+									<tr class="pari">
+										<td class="left">
+											Costi per prodotto
+										</td>
+										<td class="right">
+											- &euro;
+										</td>
+										<td class="right">
+											- %
+										</td>
+									</tr>
+									<tr class="dispari">
+										<td class="left">
+											Spese una tantum - Uscita*
+										</td>
+										<td class="right">
+											- &euro;
+										</td>
+										<td class="right">
+											- %
+										</td>
+									</tr>
+								</tbody>
+							</table>
+							* Le commissioni di uscita esposte sono le massime che potranno essere applicate in caso di uscita dall'investimento durante il primo anno.
+							<br />
+							<table class="riepilogo">
+								<thead>
+									<tr>
+										<th class="left intestazione">
+											Incentivi monetari su base annua
+										</th>
+										<th class="right">
+											Valore &euro;
+										</th>
+										<th class="right">
+											Valore %
+										</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr class="pari">
+										<td class="left">
+											Una Tantum
+										</td>
+										<td class="right">
+											- &euro;
+										</td>
+										<td class="right">
+											- %
+										</td>
+									</tr>
+									<tr class="dispari">
+										<td class="left">
+											Ricorrenti
+										</td>
+										<td class="right">
+											- &euro;
+										</td>
+										<td class="right">
+											- %
+										</td>
+									</tr>
+								</tbody>
+								<tfoot>
+									<tr class="totali">
+										<td class="left">
+											TOTALE
+										</td>
+										<td class="right">
+											- &euro;
+										</td>
+										<td class="right">
+											- %
+										</td>
+									</tr>
+								</tfoot>
+							</table>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-12">
+				<div class="checkbox inline">
+				<label class="textWrapper">
+					<input type="checkbox" />
+					<span class="text">
+						Dichiaro di aver preso visione dei costi, oneri e incentivi massimi connessi all'operazione.
+					</span>
+				</label>
+			</div>	
+		</section>
+		<br class="clear" />
+		<div class="mww-dom-component webank-component-box">
+			<section class="boxesitoWrap">
+				<div class="boxesito attenzione">
+					<div class="text">
+						<h3>
+							AVVERTENZE SULL'OPERAZIONE CHE STAI PER EFFETTUARE
+						</h3>
+						<ul class="lista">
+							<li>
+								<a>
+									Stai per negoziare uno strumento PRIIPs, clicca qui per prendere visione del KID
+								</a>
+							</li>
+						</ul>
+						<div class="checkbox inline">
+							<label class="textWrapper">
+								<input disabled="disabled" type="checkbox" />
+								<span class="text">
+									Dichiaro di aver preso visione del documento contenente le informazioni chiave (KID) e di averne acquisito copia su supporto duraturo.
+								</span>
+							</label>
+						</div>
+					</div>
+				</div>
+			</section>
+		</div>
+	</form>
+</section>
