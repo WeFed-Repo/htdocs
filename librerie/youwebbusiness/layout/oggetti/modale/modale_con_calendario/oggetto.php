b1ceced6bb338792a5654cc3674a6d88e41d0d06
martapetrella <martollum@gmail.com>
2020-07-20 12:18:56 +0200

    link underline + calendario in modale

---
+++ librerie/youwebbusiness/layout/oggetti/modale/modale_con_calendario/oggetto.php
@@ -0,0 +1,7 @@
+<p>Esempio di modale con calendario</p>
+
+<button type="button" class="btn btn-primary" data-modal-open="cal">
+  Launch demo modal
+</button>
+
+<?php virtual('/youwebbusiness/layout/oggetti/modale/modale_con_calendario/modale.php');?>
