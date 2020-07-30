aa2cbba6f43b7edee3019647bd5d326e25ae1b3c
martapetrella <martollum@gmail.com>
2020-07-30 12:24:06 +0200

    amends

--- librerie/youwebbusiness/layout/oggetti/box/_sblocca_distinta/oggetto.php
+++ librerie/youwebbusiness/layout/oggetti/box/_sblocca_distinta/oggetto.php
@@ -1,23 +1,23 @@
-<div class="bordered pb-3">
+<div class="bordered">
 	<div class="row">
 		<div class="col-12">
-      <div class="d-flex">
-        <div>
-          <img class="big-icon" src="./fe/img/icon/disponi-bonifici.svg">
-        </div>
-        <div class="px-4">
+      <div class="d-flex flex-wrap flex-sm-nowrap">
+				<div class="pb-3 w-100 w-sm-auto text-center">
+        	<img class="big-icon" src="./fe/img/icon/disponi-bonifici.svg">
+				</div>
+				<div class="px-4 pb-3">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua.
           Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
           nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
           reprehenderit in voluptate velit esse cillum dolore nulla pariatur.
         </div>
-        <div>
-          <button class="btn btn-white fl-left">
-            <i class="icon icon-aggiorna" title="icon-aggiorna"></i>
-            Button icon
-          </button>
-        </div>
+				<div class="pb-3 w-100 w-sm-auto text-center">
+        	<button class="btn btn-white fl-none">
+	          <i class="icon icon-aggiorna" title="icon-aggiorna"></i>
+	          Button icon
+	        </button>
+				</div>
       </div>
 		</div>
 	</div>
