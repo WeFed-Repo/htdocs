405f72aef4ca5f81611f7e48ffddc2dda0bf6fb0
fabdom <62433800+fabdom@users.noreply.github.com>
2020-09-28 09:50:03 +0200

    Mobile webank
    Modifica pagina per nuova app

---
+++ docs_convergenza/template/tpl_responsive_Webank_mobile.php
@@ -0,0 +1,186 @@
+<div class="titolo">
+	<h1>
+		<div class="row">
+			<div class="col-sm-8">
+				<!-- TITOLO -->
+							<span>Webank Mobile</span>
+				<!-- /TITOLO -->
+		
+				<!-- HELP -->
+				
+				<!-- /HELP -->
+
+				<!-- DOCUMENTI OBBLIGATORI MOBILE -->		
+				<i class="icon icon-documento hidden-sm hidden-md hidden-lg" title="Documenti" id="ico-documenti" style="display:none"></i>
+				<!-- /DOCUMENTI OBBLIGATORI MOBILE -->
+			</div>
+
+			
+		</div>
+	</h1>
+</div>
+	<section class="">
+
+	<section>
+<p>Con l'app di Webank, realizzata per i nostri clienti ascoltando i loro feedback, scoprirai un nuovo modo di interagire con la tua banca.  	Tanti bisogni? In un'unica app trovi tutto il necessario per semplificare la tua quotidianit&agrave;:</p>
+</section> <section>
+<div style="display: block;" id="tab1" class="tab-content">
+<div class="vt-obj">
+<div class="row">
+<div class="col-sm-5">
+<div class="vt-img"><img data-content="accessoveloce" src="/WB/fe/img/new_mobile_webank_0.png" class="img-responsive" /> <img style="display: none;" data-content="voce" src="/WB/fe/img/new_mobile_webank_1.png" class="img-responsive" /> <img style="display: none;" data-content="onetap" src="/WB/fe/img/new_mobile_webank_2.png" class="img-responsive" /> <img style="display: none;" data-content="carte" src="/WB/fe/img/new_mobile_webank_3.png" class="img-responsive" /></div>
+</div>
+<hr class="visible-xs" />
+<!-- CONSOLE ICONE MOBILE -->
+<div class="col-xs-12 visible-xs">
+<div class="vt-console"><a data-content="accessoveloce" data-toggle="switch-content" class="btn-icon selected" href="#1"> <i class="icon icon-mob icon-acc_veloce_fill act"></i><i class="icon icon-mob icon-acc-veloce_outline dis"></i> </a> <a data-content="voce" data-toggle="switch-content" class="btn-icon" href="#1"> <i class="icon icon-mob icon-voce_fill act"></i><i class="icon icon-mob icon-voice_outline dis"></i> </a> <a data-content="onetap" data-toggle="switch-content" class="btn-icon" href="#1"> <i class="icon icon-mob icon-tap_filled act"></i><i class="icon icon-mob icon-tap_outline dis"></i> </a> <a data-content="carte" data-toggle="switch-content" class="btn-icon" href="#1"> <i class="icon icon-mob icon-pcarta_fill act"></i><i class="icon icon-mob icon-pcarta_outline dis"></i> </a></div>
+</div>
+<!-- FINE CONSOLE MOBILE --> <!-- BLOCCO DEI TESTI -->
+<div class="col-sm-7 col-xs-12">
+<div class="vt-contents">
+<div class="testoIcona"><a data-content="accessoveloce" data-toggle="switch-content" class="btn-icon selected" href="#1"> <i class="icon icon-mob icon-acc_veloce_fill act"></i><i class="icon icon-mob icon-acc-veloce_outline dis"></i> </a>
+<div class="leftTesto">
+<h4>ACCESSO VELOCE, BASTI TU!</h4>
+<p>Tramite il <strong>riconoscimento</strong> della tua <strong>impronta digitale</strong> o del <strong>viso</strong> accedi facilmente al tuo conto. In pi&ugrave; puoi utilizzarla anche come Token App.</p>
+</div>
+</div>
+<div class="testoIcona"><a data-content="voce" data-toggle="switch-content" class="btn-icon" href="#1"> <i class="icon icon-mob icon-voce_fill act"></i><i class="icon icon-mob icon-voice_outline dis"></i> </a>
+<div class="leftTesto">
+<h4>COMANDI VOCALI</h4>
+<p>Parla con il tuo smartphone per consultare i movimenti del conto, delle carte, effettuare i pagamenti e cercare la Filiale pi&ugrave; vicina.</p>
+</div>
+</div>
+<div class="testoIcona"><a data-content="onetap" data-toggle="switch-content" class="btn-icon" href="#1"> <i class="icon icon-mob icon-tap_filled act"></i><i class="icon icon-mob icon-tap_outline dis"></i> </a>
+<div class="leftTesto">
+<h4>FUNZIONI VELOCI</h4>
+<p>Una volta impostate le trovi nell'<i>area di benvenuto</i>&nbsp;e ti permettono, in pochi tap e senza inserire la password, di effettuare le operazioni pi&ugrave; utilizzate.</p>
+</div>
+</div>
+<div class="testoIcona"><a data-content="carte" data-toggle="switch-content" class="btn-icon" href="#1"> <i class="icon icon-mob icon-pcarta_fill act"></i><i class="icon icon-mob icon-pcarta_outline dis"></i> </a>
+<div class="leftTesto">
+<h4>GESTISCI LE TUE CARTE</h4>
+<p>Carte di&nbsp;<strong>debito,&nbsp;credito</strong>&nbsp;e&nbsp;<strong>prepagate</strong>: fai tap su quella che desideri, visualizza i&nbsp;<strong>dettagli</strong>&nbsp;e&nbsp;<strong>personalizza le impostazioni</strong>.</p>
+</div>
+</div>
+</div>
+</div>
+<!-- FINE BLOCCO DEI TESTI --></div>
+</div>
+<script type="text/javascript"></script>
+<script type="text/javascript">// <![CDATA[
+ 
+/* Controllo switch immagine */ 
+$(function(){ 
+	$("a[data-toggle=switch-content]").click(function(){ 
+	var bt = $(this); 
+	var contshow= bt.attr("data-content"); 
+	/* Reset di tutte le immagini visibili */ 
+	$("img[data-content]").hide(); 
+	/* Reset di tutti i link */ 
+	$("a[data-content]").removeClass("selected"); 
+	$("img[data-content="+contshow+"]").show(); 
+	$("a[data-content="+contshow+"]").addClass("selected"); 
+	}); 
+});
+// ]]></script>
+</div>
+</section>
+<section>
+<div class="row boxInfoStore2">
+	<div class="col-sm-8">
+		<h3>DISPONIBILE PER TUTTI I DEVICE</h3>
+		<p>Se hai gi&agrave; l'app Webank, imposta l'aggiornamento automatico: appena disponibile sarai tra i primi ad averla.</p>
+		<p><strong>Non l'hai ancora scaricata?</strong> Clicca sul pulsante dello Store del tuo dispositivo (solo da smartphone).</p>
+		<p>Di seguito i requisiti minimi: iPhone: iOS versione 12<br /> Android: versione 6<br /> iPad: iOS versione 12<br /> Tablet Android: versione 6</p>
+		<div class="row boxInfoStore2">
+			<div class="col-sm-4">
+				<a target="_blank" title="Iphone" href="https://itunes.apple.com/it/app/webank/id306283651?mt=8"><img alt="Iphone" src="/WB/fe/img/btn_apple_store_download_ita.png" /></a>
+			</div>
+			<div class="col-sm-4">
+				<a target="_blank" title="Android" href="https://play.google.com/store/apps/details?id=com.opentecheng.android.webank&amp;hl=it"><img alt="Android" src="/WB/fe/img/btn_android_store_download_ita.png" /></a>
+			</div>
+			<div class="col-sm-4">
+				<a target="_blank" title="Huawei" href="https://play.google.com/store/apps/details?id=com.opentecheng.android.webank&amp;hl=it"><img alt="Android" src="/WB/fe/img/btn_huawei_store_download_ita.png" /></a>
+			</div>
+		</div>
+	</div>
+	<div class="col-sm-4">
+		<div class="box-generico-border">
+			<div class="box-generico-text"><img alt="Device" src="/WB/fe/img/new_device_webank.png" class="img-responsive" /></div>
+		</div>
+	</div>
+</div>
+</section>
+
+<section> <!--
+<p>Hai gi&agrave; installato l'App sul tuo device? <a href="/WEBWB/serviziPerTe/gestioneDispositiviApp.do" mce_href="/WEBWB/serviziPerTe/gestioneDispositiviApp.do">Vai alla pagina di gestione</a></p>
+</section> --></section>
+
+	</section>
+
+
+
+
+
+
+
+
+
+
+
+<script type="text/javascript">// <![CDATA[
+	$(document).ready(function(){
+		$( ".open-modal" ).click(function() {
+			console.log('aaaa' + $(this).data('id') + 'bbbb');
+			$('#nameVideo').val($(this).data('id'));
+		});
+    });
+// ]]></script>
+<!-- Overlayer Guarda il video-->
+<div class="modal fade" id="videoContent" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+	<div class="modal-dialog" role="document" id="xxxx">
+		<div class="modal-content">
+			<div class="modal-header" id="xxxx2">
+				<a class="close btn-icon" data-dismiss="modal" aria-label="Close">
+					<i class="icon icon-alert_error_fill icon-2x"></i>
+				</a>	
+			</div>
+			<div class="modal-body">
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-xs-12">
+							<!-- VIDEO -->
+							<script src="/WB/fe/js/priv_videoplayer.js" type="text/javascript"></script>		
+				
+							<style>
+								#videowrapper {width:100%;float:left;display:block; margin-bottom: 30px;}
+							</style>		
+
+							<input type="hidden" id="nameVideo"/>
+							<div id="videowrapper"></div>
+							<script type="text/javascript">
+								$(document).ready(function(){
+									$( ".open-modal" ).click(function() {
+										console.log('aaaa' + $(this).data('id') + 'bbbb');
+										$('#nameVideo').val($(this).data('id'));
+
+										console.log('aaa' + $('#nameVideo').val() + 'bbbb');
+										getVideo({
+											wrapper: "#videowrapper",
+											videopath : "https://media.webank.it/vod/" + $(this).data('id'),
+											autoplay: false,
+											initialVolume: 0.8
+										});
+									});
+    							});
+								
+							</script>
+							<!-- END VIDEO -->
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- End Overlayer Guarda il video-->
