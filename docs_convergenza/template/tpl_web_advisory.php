7c81b4815922b75015876a9aa3e55d01435bf0ac
sstrepponi <62430689+sstrepponi@users.noreply.github.com>
2020-09-28 08:11:48 +0200

    web aadvisory aletti
    web aadvisory aletti

--- docs_convergenza/template/tpl_web_advisory.php
+++ docs_convergenza/template/tpl_web_advisory.php
@@ -38,10 +38,10 @@
 							<div class="row">
 								<div class="col-xs-12">
 									<div class="row">
-										<div class="col-xs-10">
+										<div class="col-xs-9">
 											Numero proposte di consulenza che devi ancora <strong>visualizzare</strong> o che stai valutando
 										</div>
-										<div class="col-xs-2 alignRight">
+										<div class="col-xs-3 alignRight">
 											<strong class="result-number">1</strong>
 										</div>
 									</div>
@@ -67,8 +67,8 @@
 							<div class="row">
 								<div class="col-xs-12">
 									<div class="row">
-										<div class="col-xs-10">Numero proposte di consulenza <strong>che hai confermato</strong></div>
-										<div class="col-xs-2 alignRight"><strong class="result-number">2</strong></div>
+										<div class="col-xs-9">Numero proposte di consulenza <strong>che hai confermato</strong></div>
+										<div class="col-xs-3 alignRight"><strong class="result-number">2</strong></div>
 									</div>	
 								</div>
 							</div>
