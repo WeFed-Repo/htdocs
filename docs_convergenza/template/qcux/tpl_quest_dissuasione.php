b03cea64a7491ba5972bd4fc76418d441f31ca28
davidecioetto <davide.cioetto@alten.it>
2020-10-01 16:51:18 +0200

    Pagina di dissuasione questionario

---
+++ docs_convergenza/template/qcux/tpl_quest_dissuasione.php
@@ -0,0 +1,8 @@
+<!-- DIV QUESTIONARIO -->
+<div class="quest feedback">
+    <div class="fb-message">
+        <div class="img-broken"></div>
+        <span class="payoff"><strong>Siamo spiacenti</strong>.<br>A questo link non ci sono questionari attivi.</span>
+    </div>
+</div>
+<!-- / DIV QUESTIONARIO -->
