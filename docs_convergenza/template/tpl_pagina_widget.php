e6f250aeb0554f98dfd6bbab32a08ba4e5c2f558
davidecioetto <davide.cioetto@alten.it>
2020-10-12 17:44:08 +0200

    Layout pagina Widget

---
+++ docs_convergenza/template/tpl_pagina_widget.php
@@ -0,0 +1,79 @@
+<!-- Titolo -->
+<h2>Tutte le funzionalit&agrave; della tua App</h2>
+<!-- / Titolo -->
+<style>
+/* BOX HERO */
+.box-hero {width:100%;display:block; position:relative; background-color: #EBEDEE;min-height:460px;}
+.box-hero .box-bg-dark {background-color:#122e5c;padding:20px 20px 20px 320px;}
+.box-hero .box-bg-dark .hero-block {position:absolute;width:300px;left:20px;top:20;}
+.box-hero .box-bg-dark .hero {height: 360px; margin: 0px auto 25px; display:block;}
+.box-hero .box-bg-dark .hero-promo-text {color:#fff}
+.box-hero .box-bg-light {padding:20px 20px 20px 320px;display:block;clear:both;}
+
+/* Bottoni app */
+.btn-app-console .btn-app {margin-left:15px}
+.btn-app-console .btn-app:first-child {margin-left:0}
+.btn-app {width:135px;height:40px;display:inline-block;background-size:contain;background: transparent none no-repeat center center;}
+.btn-appstore {background-image: url("/common/fe/img/btn_appstore.png");}
+.btn-googleplay {background-image: url("/common/fe/img/btn_googleplay.png");}
+</style>
+<!-- Box Hero -->
+<section>
+    <div class="box-hero">
+        <div class="box-bg-dark">
+            <div class="hero-block">
+                <img src="/common/fe/img/ph_phone.png" class="hero">
+                <!-- Pulsanti app -->
+                <div class="btn-app-console">
+                    <a href="#" class="btn-app btn-appstore"></a>
+                    <a href="#" class="btn-app btn-googleplay"></a>
+                </div>
+                <!-- / pulsanti app -->
+            </div>
+            <div class="hero-promo-text hidden-xs">
+                FACILE<br>
+
+                    La installi con un tap<br>
+
+                    SICURA<br>
+
+                    La usi come Dispositivo di Sicurezza<br>
+
+                    COMPLETA<br>
+
+                    Gestisci le operazioni bancarie e gli investimenti
+                
+            </div>
+        </div>
+        <div class="box-bg-light">    
+            <p>
+                <strong>SEMPRE CON TE</strong><br>
+                Se hai gi&agrave; l'app, aggiornala ora per avere tutte le novit&agrave;.<br>
+                Non l'hai ancora scaricata?<br>
+                Clicca sul pulsante dello Store del tuo dispositivo (solo da smartphone)!
+            </p>
+        </div>
+    </div>            
+</section>
+<!-- / Box Hero -->
+
+<!-- Box descrittivo immagine e testo -->
+<div class="row">
+    <div class="col-sm-6">
+        <h3>Lorem ipsum dolor sit amet</h3>
+        <p><strong>Dolor</strong> lorem dolor ipsum sit amet consectetur adipiscing elit summa cum laude. </p>
+        <ul>
+            <li>Lorem dolor sit amet consectetur adipiscing elit summa</li>
+            <li>Lorem dolor sit amet consectetur adipiscing elit summa</li>
+            <li>Lorem dolor sit amet consectetur adipiscing elit summa</li>
+            <li>Lorem dolor sit amet consectetur adipiscing elit summa</li>
+        </ul>
+        <a href="#"><i class="icon icon-play"></i> Nome_video_tutorial</a>
+    </div>
+    <div class="col-sm-6">
+        <div class="light-bg-box">
+            <img src="/common/fe/img/ph_phone.png">
+        </div>
+    </div>
+</div>
+<!-- /Box descrittivo immagine e testo -->
