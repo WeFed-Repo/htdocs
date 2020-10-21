8cd4873321f26d6105182aae39815942e2cf2ee4
fabdom <62433800+fabdom@users.noreply.github.com>
2020-10-21 12:37:49 +0200

    digital identity
    modifiche dovute a M.P.

---
+++ docs_convergenza/template/tpl_priv_digital_identity.php
@@ -0,0 +1,50 @@
+<style>
+.row-gray{background-color: #F2F2F2}
+.box-padding{padding-top:10px;padding-bottom:10px}
+</style>
+<section class="hidden-xs">
+	<!--section>
+		<div><span>CLUSTER: 3</span></div>
+		<div><span><span>idcnt: 2216<br><br></span></span></div>
+	</section-->
+	<section>
+		<div class="disp-tab">
+			<div class="row disp-row row-gray">
+				<div class="col-xs-12 col-sm-10 box-padding"><!--
+	<h3>PERCHE  RICONTRATTUALIZZARE?</h3>
+	-->
+					<p>L'attivazione dell'<strong>Identit&agrave; Digitale</strong> consente di <strong>semplificare la gestione dei rapporti bancari</strong>, di ridurre i tempi,&nbsp;<strong>ricevere i documenti in formato elettronico</strong> e evitare sprechi di carta<br><br> <strong>Continuerai ad accedere</strong> e operare da tutti i canali, <strong>utilizzando le credenziali gi&agrave; in tuo possesso</strong>.</p>
+				</div>
+				<div class="col-xs-12 col-sm-2 alignCenter box-padding"><img src="/common/fe/img/immagineIdentitaDigitale.png" alt="Identit&agrave; digitale" class="img-resp"></div>
+			</div>
+		</div>
+	</section>
+	<section>
+		<div class="disp-tab">
+			<div class="row disp-row">
+				<div class="col-xs-12 col-sm-10 box-padding">
+					<h3>COSA CAMBIA</h3>
+					<p><strong>Il canone mensile previsto per il tuo Internet Banking non cambia.</strong> <strong>Per azzerarlo</strong>, se non hai gi&agrave; la gratuit&agrave;, <strong>ti basta effettuare 2 disposizioni online nell'arco del mese</strong>.</p>
+					<p><strong>Tutti i rapporti</strong> (conti, carte, deposito titoli) <strong>collegati alla tua posizione saranno abilitati ai servizi a distanza</strong> (internet banking e phone banking). Potrai modificare questa impostazione al percorso <i>Conti &gt; Pannello rapporti</i>. <br><br> <strong>Gli Alert che hai impostato</strong> per tenere sotto controllo la tua operativit&agrave; <strong>saranno disattivati</strong>. Potrai riattivarli dopo la sottoscrizione del Contratto d'Identit&agrave; Digitale cliccando sull'apposita icona presente in alto a destra. Rimangono <strong>attivi e gratuiti gli Alert di sicurezza</strong>. <br>Con l'adesione al Contratto di Identit&agrave; Digitale la tua linea di trading verr&agrave; disattivata.<br>Dopo l'attivazione dell'Identit&agrave; Digitale ti baster&agrave; scegliere una nuova linea di trading e sottoscrivere il Contratto dedicato per continuare ad operare.&nbsp;<br>Alcune condizioni potrebbero essere variate: scopri le <strong><i><a class="link-text" data-target="#modaleTabellaCosti" data-toggle="modal" href="#">nuove linee di trading online</a> </i></strong>.</p>
+				</div>
+				<div class="col-xs-12 col-sm-2 alignCenter box-padding"><img style="margin-top: 30px;" src="/common/fe/img/immaginePaperless.png" alt="Paperless" class="img-resp"></div>
+			</div>
+		</div>
+	</section>
+	<section>
+		<div class="disp-tab">
+			<div class="row disp-row row-gray">
+				<div class="col-xs-12 col-sm-10 box-padding">
+					<h3>COME PROCEDERE</h3>
+					<ul>
+						<li>Prendi visione dell'<strong>Informativa precontrattuale</strong></li>
+						<li>Attiva la <strong>Firma Digitale</strong></li>
+						<li>Sottoscrivi il contratto di <strong>Identit&agrave; Digitale</strong></li>
+						<li>Sottoscrivi il contratto di <strong>Trading Online</strong></li>
+					</ul>
+				</div>
+				<div class="col-xs-12 col-sm-2 alignCenter box-padding"><img style="margin-top: 30px;" src="/common/fe/img/immagineTrading.png" alt="Trading" class="img-resp"></div>
+			</div>
+		</div>
+	</section>
+</section>
