049e80af8b68a276897b9ed9d6dcd388150de2a6
davidecioetto <davide.cioetto@alten.it>
2020-11-02 10:51:40 +0100

    Perfezionato iframe questionario Aletti

--- docs_aletti/src/components/pages/Onboarding/Steps/RaccoltaProdotti/questionarioMIFID.js
+++ docs_aletti/src/components/pages/Onboarding/Steps/RaccoltaProdotti/questionarioMIFID.js
@@ -13,11 +13,11 @@ export default class extends Component {
 
     render() {
 
+        console.log(ambiente)
      
-        let iframeSrc = (ambiente.name==="locale")? "/_iframe_testing.htm" : "/promotori/dashboard/goToMifidQuest";
+        let iframeSrc = (ambiente.name==="locale" || ambiente.name==="librerie")? "/_iframe_testing.htm" : "/promotori/dashboard/goToMifidQuest";
 
         return(<>
-                <button onClick={()=>window.setMifid({idQuestionario:Math.random()})}>Termina questionario</button>
                 <iframe className="iframe-resp" name="iframe_mifid" src={iframeSrc}></iframe>
             </>
         )
