bf1859e5440c5d1d909702ae66c33a346c2c7bfb
sstrepponi <62430689+sstrepponi@users.noreply.github.com>
2020-11-06 15:23:46 +0100

    fixing ID + assicurazioni
    fixing ID + assicurazioni

---
+++ docs_convergenza/template/tpl_profilo_assicurativo_misto.php
@@ -0,0 +1,30 @@
+<!-- titolo di pagina -->
+<section>
+		<div class="titolo">
+			<h1>
+				<div class="row">
+					<div class="col-sm-8">
+						<span>Il tuo profilo assicurativo</span>
+				 	</div>
+				 </div>
+			</h1>
+		</div>
+</section>
+<!-- fine titolo di pagina -->
+<!-- intro testuale -->
+<section>
+    <p>Prima di procedere con il preventivo o l'acquisto di un prodotto assicurativo, <strong>siamo tenuti a verificare le tue esigenze in modo da offrirti un prodotto coerente con le tue necessit&agrave;.</strong>
+    <br>Ricorda che potrai modificare il tuo profilo assicurativo in qualsiasi momento.</p>
+    <p>
+        I clienti con rapporti di conto corrente presso filiali che offrono sia prodotti Vera sia prodotti Bipiemme Vita, avranno a disposizione <strong>due profili assicurativi</strong> per verificare la coerenza dei propri bisogni con l’offerta di ciascuna compagnia.
+    </p>
+</section>
+<!-- fine intro testuale -->
+<!--sezione profilo assente-->
+
+<!--fine sezione profilo assente-->
+<section>
+    
+    </div>
+</section>
+
