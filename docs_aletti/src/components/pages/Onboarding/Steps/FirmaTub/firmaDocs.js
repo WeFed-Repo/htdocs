50d8642a03cf128520190c83541a0ec99a118ae0
davidecioetto <davide.cioetto@alten.it>
2020-11-09 17:44:53 +0100

    Mappatura check

--- docs_aletti/src/components/pages/Onboarding/Steps/FirmaTub/firmaDocs.js
+++ docs_aletti/src/components/pages/Onboarding/Steps/FirmaTub/firmaDocs.js
@@ -7,64 +7,91 @@ class FirmaDocs extends Component {
     constructor(props) {
         super(props);
         this.state= {
-            checkstate : ""
+            checkstate : "",
+            checkcontrolarray: []
         };
         this.generalOnChange=this.generalOnChange.bind(this);
     }
+
+    docobj = {
+        "accordions": [
+            {
+                "title": <>Documentazione informativa <strong>Privacy</strong></>,
+                "files": [{
+                    "name": "Informativa Privacy",
+                    "url":"####",
+                    "checkgroup" : [
+                        <>Il cliente dichiara di aver preso visione della documentazione informativa precontrattuale che Banca Aletti ha consegnato.</> ,
+                        <><strong>Ricezione copia proposta del contratto unitamente a copia di tutti i relativi allegati.</strong><br />Il cliente dichiara di aver ricevuto una copia di tutti i documenti contrattuali</>
+                    ]
+                }]
+            },
+            {
+                "title": <>Documentazione informativa precontrattuale del conto corrente</>,
+                "files": [{
+                    "name": "Informativa Privacy",
+                    "url":"####",
+                    "checkgroup" : [
+                        <>Il cliente dichiara di aver preso visione della documentazione informativa precontrattuale che Banca Aletti ha consegnato.</> ,
+                        <><strong>Ricezione copia proposta del contratto unitamente a copia di tutti i relativi allegati.</strong><br />Il cliente dichiara di aver ricevuto una copia di tutti i documenti contrattuali</>
+                    ]
+                }]
+            }]
+        ,
+        "checkgroup" : [
+                <>Il cliente dichiara di aver preso visione della documentazione informativa precontrattuale che Banca Aletti ha consegnato.</> ,
+                <><strong>Ricezione copia proposta del contratto unitamente a copia di tutti i relativi allegati.</strong><br />Il cliente dichiara di aver ricevuto una copia di tutti i documenti contrattuali</>
+            ]
+        
+        }
 
     generalOnChange(e) {
         Form.change(this,e);         
     }       
 
-    
-    
 
+    componentDidMount() {
+
+        // Costruisce l'array per il confronto dei checkbox
+        let cca = [];
+        this.docobj["accordions"].forEach((v,i)=>{
+            v.files.forEach((val,i2)=>{
+                val.checkgroup.forEach((value,i3)=>{
+                    cca.push("check_"+i+"_"+i2+"_"+i3)
+                })
+            })
+        });
+        this.docobj["checkgroup"].forEach((v,i)=>{
+            cca.push("check_"+ i)
+        })
+
+        this.setState({
+            checkcontrolarray: cca
+        })
+
+    }
+
+    allChecked(checkvalue,checkarray) {
+        var checkall = (checkvalue.split(",").sort().join(",") === checkarray.sort().join(","));
+        return checkall;
+    }
+    
     render(){
 
-        let docobj = {
-            "accordions": [
-                {
-                    "title": <>Documentazione informativa <strong>Privacy</strong></>,
-                    "files": [{
-                        "name": "Informativa Privacy",
-                        "url":"####",
-                        "checkgroup" : [
-                            <>Il cliente dichiara di aver preso visione della documentazione informativa precontrattuale che Banca Aletti ha consegnato.</> ,
-                            <><strong>Ricezione copia proposta del contratto unitamente a copia di tutti i relativi allegati.</strong><br />Il cliente dichiara di aver ricevuto una copia di tutti i documenti contrattuali</>
-                        ]
-                    }]
-                },
-                {
-                    "title": <>Documentazione informativa precontrattuale del conto corrente</>,
-                    "files": [{
-                        "name": "Informativa Privacy",
-                        "url":"####",
-                        "checkgroup" : [
-                            <>Il cliente dichiara di aver preso visione della documentazione informativa precontrattuale che Banca Aletti ha consegnato.</> ,
-                            <><strong>Ricezione copia proposta del contratto unitamente a copia di tutti i relativi allegati.</strong><br />Il cliente dichiara di aver ricevuto una copia di tutti i documenti contrattuali</>
-                        ]
-                    }]
-                }]
-            ,
-            "checkgroup" : [
-                    <>Il cliente dichiara di aver preso visione della documentazione informativa precontrattuale che Banca Aletti ha consegnato.</> ,
-                    <><strong>Ricezione copia proposta del contratto unitamente a copia di tutti i relativi allegati.</strong><br />Il cliente dichiara di aver ricevuto una copia di tutti i documenti contrattuali</>
-                ]
-            
-            }
+        
 
         return(
             <section>
                 {
-                    docobj.accordions &&  docobj.accordions.map((el,i)=>{
+                    this.docobj.accordions &&  this.docobj.accordions.map((el,i)=>{
                             return(
                             <DefaultCollapse key={i}
                             label={el.title}
                             className="search-collapse">
                             {el.files.map((file,i2)=>{
                                 return (
-                                    <div key={i2}>
-                                    <Form.checkfile className="no-label"
+                                    <div key={i2} className="margin-bottom-medium">
+                                    <Form.checkfile className={"no-label " + (file.checkgroup && "no-margin-bottom")}
                                         name={"file_"+i+"_"+i2}
                                         value={this.state["file_"+i+"_"+i2]}
                                         onChange={this.generalOnChange}
@@ -81,7 +108,9 @@ class FirmaDocs extends Component {
                                             value= {this.state.checkstate}
                                             disabled={!(this.state["file_"+i+"_"+i2])}
                                             orientation="vertical"
+                                            cbchange={(val)=>{ if (this.allChecked(val,this.state.checkcontrolarray)) this.props.validFunction()} }
                                             options={file.checkgroup.map((option,index)=>{
+                                           
                                                 return ({
                                                     "text":option,
                                                     "value": "check_"+i+"_"+i2+"_"+index
@@ -96,7 +125,28 @@ class FirmaDocs extends Component {
                             </DefaultCollapse>
                            )
                     })
-                }                
+                } 
+                { this.docobj.checkgroup && 
+                    
+            
+                    <Form.checkgroup className="no-label"
+                        name="checkstate"
+                        onChange={this.generalOnChange}
+                        value= {this.state.checkstate}
+                        orientation="vertical"
+                        cbchange={(val)=>{ if (this.allChecked(val,this.state.checkcontrolarray)) this.props.validFunction()} }
+                        options={this.docobj.checkgroup.map((option,i)=>{
+                       
+                            return ({
+                                "text":option,
+                                "value": "check_"+i
+                            })
+                        })}
+                    >
+                    </Form.checkgroup>
+                }    
+            
+                                                    
                 <a onClick={this.props.validFunction}>Valida firma</a>
             </section>
         )
