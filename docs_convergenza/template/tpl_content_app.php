19bf9a770d0ba0b2dbe890a84368958b08323ec3
davidecioetto <davide.cioetto@alten.it>
2021-01-22 17:49:45 +0100

    content-app

---
+++ docs_convergenza/template/tpl_content_app.php
@@ -0,0 +1,6 @@
+<!-- Nodo che contiene l'applicativo Angular -->
+<content-app contenuto="fida|prova"></content-app>
+<!-- Fine del nodo -->
+<!-- Importazione degli assets -->
+<script src="/common/fe/content-app/content-app.js"></script>
+<!-- Fine importazione degli assets -->
