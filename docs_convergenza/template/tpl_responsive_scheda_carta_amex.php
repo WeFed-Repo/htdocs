947758134eb30b2ccfaa0031ada258914803daf9
sstrepponi <62430689+sstrepponi@users.noreply.github.com>
2021-01-25 09:35:32 +0100

    carta amex scheda prodotto webank
    carta amex scheda prodotto webank

---
+++ docs_convergenza/template/tpl_responsive_scheda_carta_amex.php
@@ -0,0 +1,194 @@
+<h2>Carta Amex</h2>
+<div class="navfrontPage outerTab">
+	<div class="innerTab">
+		<ul class="tabbig">
+			<li class="on"><a href="#">Scheda prodotto</a></li>
+			<li><a href="connect.php?page=strutt_priv.php&tpl=tpl_responsive_costi_condizioni.php&liv0=2&liv1=0&liv2=0&liv3=0&responsive=y#">Costi e condizioni</a></li>
+			<li><a href="connect.php?page=strutt_priv.php&tpl=tpl_responsive_richiesta_carta.php&liv0=2&liv1=0&liv2=0&liv3=0&responsive=y#">Richiesta carta</a></li>
+	</ul>
+</div>
+	<div class="separator"></div>
+</div>
+<div class="panel-nofill">
+    <!--CARTA VERDE -->
+    <section>
+        <div class="box-generico">
+            <div class="row">
+                <div class="col-sm-4"><br><img src="/WB/fe/img/img_carta_amex_green.jpg" alt="Carta verde american express"></div>
+                <div class="col-sm-8">
+                <h3><br>CARTA VERDE AMERICAN EXPRESS</h3>
+                <h4>BONUS DI BENVENUTO</h4>
+                <p>Effettuando una trasazione con Carta entro 90 giorni dall'emissione, otterrai un bonus di benvenuto di <strong>12500 punti Menbership Rewards&reg;</strong></p>
+                <div>
+            </div>
+            <br class="clear">
+        </div>
+    </section>
+
+    <section>
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="testoIcona">
+                    <i class="icon icon-2x flLeft icon-ico_club_membership_rewards" title="ico_rewards"></i>
+                    <div class="leftTesto">
+                        <strong>CLUB MEMBERSHIP REWARDS&reg; </strong>
+                        <p>Con l'iscrizione automatica al Club Membreship Rewards, accumuli punti con le tue spese</p>
+                    </div>	
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="testoIcona">
+                    <i class="icon icon-2x flLeft icon-ico_protezione_acquisto" title="ico_protezione_acquisto"></i>
+                    <div class="leftTesto">
+                        <strong>PROTEZIONE D'ACQUISTO </strong>
+                        <p>Protezione per 90 giorni fino a 1.300&euro; sugli acquisti con Carta (escluse alcune categorie merceologiche), in caso di furto o danneggiamento del bene.</p>
+                    </div>	
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="testoIcona">
+                    <i class="icon icon-2x flLeft icon-ico_protezione_acquisto" title="ico_protezione_acquisto"></i>
+                    <div class="leftTesto">
+                        <strong>PROTEZIONE ANTIFRODE</strong>
+                        <p>Protezione in caso di uso illecito della Carta da parte di terzi, per utilizzare la Carta in tutta tranquillit&agrave;.</p>
+                    </div>	
+                </div>
+            </div>
+        </div>
+        <div class="form-group btnWrapper">
+            <div class="btn-align-right">
+                <a href="#">Costi e condizioni</a>
+                <a title="Richiedi" href="#" type="button" class="btn btn-primary">Richiedi</a> <br class="clear">
+            </div>
+        </div>
+        <br class="clear">
+    </section>
+    <hr>
+    <!--CARTA PLATINO -->
+    <section>
+        <div class="box-generico">
+            <div class="row">
+                <div class="col-sm-4"><br><img src="/WB/fe/img/img_carta_amex_platino.jpg" alt="Carta platino american express"></div>
+                <div class="col-sm-8">
+                <h3><br>CARTA PLATINO AMERICAN EXPRESS</h3>
+                <h4>BONUS DI BENVENUTO</h4>
+                <p>Spendendo &euro; 5.000 con Carta entro 90 giorni dall'emissione, otterrai un bonus di benvenuto di <strong>80.000 punti Membership Rewards&reg;.</strong></p>
+                <div>
+            </div>
+            <br class="clear">
+        </div>
+    </section>
+
+    <section>
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="testoIcona">
+                   <i class="icon flLeft icon-2x icon-ico_priority_pass" title="priority pass"></i>
+                    <div class="leftTesto">
+                        <strong>PRIORITY PASS&trade; LIVELLO PRESTIGE </strong>
+                        <p>Iscrizione gratuita al livello Prestige del Programma Priority Pass, per accedere ad oltre 1.300 VIP Lounge aeroportuali nel mondo.</p>
+                    </div>	
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="testoIcona">
+                    <i class="icon flLeft icon-2x icon-ico_regalo_ogni_anno" title="regalo ogni anno"></i>
+                    <div class="leftTesto">
+                        <strong>UN REGALO OGNI ANNO</strong>
+                        <p>150&euro; di voucher ogni anno per i tuoi viaggi.</p>
+                    </div>	
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="testoIcona">
+                   <i class="icon flLeft icon-2x icon-ico_servizio_concierge" title="servizio concierge"></i>
+                    <div class="leftTesto">
+                        <strong>SERVIZIO CONCIERGE</strong>
+                        <p>Un servizio dedicato, 24/7 a tua disposizione per ogni esigenza, dai viaggi al lifestyle. </p>
+                    </div>	
+                </div>
+            </div>
+        </div>
+        <div class="form-group btnWrapper">
+            <div class="btn-align-right">
+                <a href="#">Costi e condizioni</a>
+                <a title="Richiedi" href="#" type="button" class="btn btn-primary">Richiedi</a> <br class="clear">
+            </div>
+        </div>
+        <br class="clear">
+    </section>
+    <hr>
+    <!--CARTA ORO -->
+    <section>
+        <div class="box-generico">
+            <div class="row">
+                <div class="col-sm-4"><br><img src="/WB/fe/img/img_carta_amex_gold.jpg" alt="Carta oro american express"></div>
+                <div class="col-sm-8">
+                <h3><br>CARTA ORO AMERICAN EXPRESS</h3>
+                <h4>BONUS DI BENVENUTO</h4>
+                <p>Spendendo &euro; 500 con Carta entro 60 giorni dall'emissione, otterrai un bonus di benvenuto di <strong>25.000 punti Membership Rewards&reg;</strong>.</p>
+                <div>
+            </div>
+            <br class="clear">
+        </div>
+    </section>
+
+    <section>
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="testoIcona">
+                    <i class="icon flLeft icon-2x icon-ico_priority_pass" title="priority pass"></i>
+                    <div class="leftTesto">
+                        <strong>PRIORITY PASS</strong>
+                        <p>Iscrizione gratuita al Programma Priority Pass, per accedere ad oltre 1.300 VIP Lounge aeroportuali nel mondo.</p>
+                    </div>	
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="testoIcona">
+                    <i class="icon flLeft icon-2x icon-ico_regalo_ogni_anno" title="regalo ogni anno"></i>
+                    <div class="leftTesto">
+                        <strong>UN REGALO OGNI ANNO</strong>
+                        <p>50&euro; di voucher ogni anno per i tuoi viaggi.</p>
+                    </div>	
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="testoIcona">
+                    <i class="icon flLeft icon-2x icon-ico_eventi_2_1" title="eventi 2x1"></i>
+                    <div class="leftTesto">
+                        <strong>EVENTI 2X1</strong>
+                        <p>Biglietti 2x1 per i migliori eventi e una Card per andare al Cinema The Space con chi vuoi. </p>
+                    </div>	
+                </div>
+            </div>
+        </div>
+        <div class="form-group btnWrapper">
+            <div class="btn-align-right">
+                <a href="#">Costi e condizioni</a>
+                <a title="Richiedi" href="#" type="button" class="btn btn-primary">Richiedi</a> <br class="clear">
+            </div>
+        </div>
+        <br class="clear">
+    </section>
+    <section>
+       <ul class="documenti">
+            <li><a target="_blank" class="btn-icon" href="#" title="Note legali carte american express"><i class="icon icon-file_pdf_fill fLeft" title="Note legali carte american express"></i> 
+                <span class="fLeft btnLink paddingLeftSmall">Note legali carte american express</span>
+            </a></li>
+		    <li><a target="_blank" class="btn-icon" href="#" title="Foglio informativo"><i class="icon icon-file_pdf_fill fLeft" title="icona posta_arrivo"></i> <span class="fLeft btnLink paddingLeftSmall">Foglio informativo</span></a></li>
+		</ul>
+    </section>
+</div>
