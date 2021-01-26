593e4ad9a85009be7d7d7dae8f9fba62501e7286
sstrepponi <62430689+sstrepponi@users.noreply.github.com>
2021-01-26 17:03:07 +0100

    carta Amex webank/youweb
    carta Amex webank/youweb  template scheda + template costi e condizioni

---
+++ docs_convergenza/template/tpl_responsive_costi_condizioni_amex.php
@@ -0,0 +1,94 @@
+<h2>Carta American Express</h2>
+<div class="navfrontPage outerTab">
+	<div class="innerTab">
+		<ul class="tabbig">
+            <?php print '<li><a href="/template/strutt_'.$site.'.php?tpl=tpl_responsive_scheda_carta_amex.php&liv0=2&liv1=0&liv2=0&liv3=0&responsive=y#">Scheda prodotto</a></li>' ?>
+            <?php print '<li class="on"><a href="/template/strutt_'.$site.'.php?tpl=tpl_responsive_costi_condizioni_amex.php&liv0=2&liv1=0&liv2=0&liv3=0&responsive=y#">Costi e condizioni</a></li>' ?>
+            <li><a href="">Richiesta carta</a></li>
+	    </ul>
+    </div>
+	<div class="separator"></div>
+</div>
+<div class="panel-nofill">
+    <section>
+        <h3>Carte American Express</h3>
+        <p>Qui trovi i costi e le condizioni della carta Verde, Oro e Platino di American Express</p>
+    </section>
+    <section>
+        <div class="headerContainerNoBootS">
+            <div class="tableContainerNoBootS">
+                <table class="table-confronta-costi" cellspacing="0" cellpadding="0" border="0" id="confrontaCarteAmex">
+                    <thead>
+                        <tr>
+                            <th  width="25%"></th>
+                            <th width="25%"><span>CARTA VERDE</span><img src="/common/fe/img/carta_small_amex_green.gif" alt="carta American Express verde"></th>
+                            <th  width="25%"><span>CARTA PLATINO</span><img src="/common/fe/img/carta_small_amex_platino.gif" alt="carta American Express platino"></th>
+                            <th  width="25%"><span>CARTA ORO</span><img src="/common/fe/img/carta_small_amex_gold.gif" alt="carta American Express oro"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><strong>Quota</strong></td>
+                            <td>Quota ridotta a 30&euro; il 1&deg; anno, rimborsata alla prima transazione entro 45 giorni dall'emissione. Dal 2&deg; anno, &euro; 75.</td>
+                            <td>Quota mensile 60&euro;</td>
+                            <td>Quota ridotta a 20&euro; il 1&deg; anno, rimborsata alla prima transazione entro 3 mesi dall'emissione. Dal 2&deg; anno, &euro; 165.</td>
+                            
+                        </tr>
+                        <tr>
+                            <td><strong>Estratti conto</strong></td>
+                            <td>Online: 0&euro;<br>
+                                Cartaceo: 0,50&euro;
+                            </td>
+                            <td>Online: 0&euro;<br>
+                                Cartaceo: 0,50&euro;
+                            </td>
+                            <td>Online: 0&euro;<br>
+                                Cartaceo: 0,50&euro;
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><strong>Commissioni per prelievi di contante a mezzo ATM</strong></td>
+                            <td>3,9% dell'importo prelevato. Commissione minima di &euro; 1,25</td>
+                            <td>3,9% dell'importo prelevato. Commissione minima di &euro; 1,25</td>
+                            <td>3,9% dell'importo prelevato. Commissione minima di &euro; 1,25</td>
+                        </tr>
+                        <tr>
+                            <td><strong>Commissione di cambio su operazioni in valuta estera</strong></td>
+                            <td>2,5% dell'importo dell'operazione</td>
+                            <td>2,5% dell'importo dell'operazione</td>
+                            <td>2,5% dell'importo dell'operazione</td>
+                        </tr>
+                        <tr>
+                            <td><strong>Imposta di bollo su Estratti Conto superiori a &euro; 77,47</strong></td>
+                            <td class="center">&euro; 2,00</td>
+                            <td class="center">&euro; 2,00</td>
+                            <td class="center">&euro; 2,00</td>
+                        </tr>
+                        <tr>
+                            <td><strong>Polizza Viaggi (infortuni viaggi, assistenza viaggi) e Protezione acquisti </strong></td>
+                            <td class="center">&euro; 0</td>
+                            <td class="center">&euro; 0</td>
+                            <td class="center">&euro; 0</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </section>
+    <section>
+    <div class="form-group btnWrapper">
+            <div class="btn-align-right">
+                <a title="Richiedi" href="#" type="button" class="btn btn-primary">Richiedi</a>
+            </div>
+            <br class="clear">
+    </div>
+    </section>
+    <section>
+       <ul class="documenti">
+            <li><a target="_blank" class="btn-icon" href="#" title="Note legali carte american express"><i class="icon icon-file_pdf_fill fLeft" title="Note legali carte american express"></i> 
+                <span class="fLeft btnLink paddingLeftSmall">Note legali carte american express</span>
+            </a></li>
+		    <li><a target="_blank" class="btn-icon" href="#" title="Foglio informativo"><i class="icon icon-file_pdf_fill fLeft" title="icona posta_arrivo"></i> <span class="fLeft btnLink paddingLeftSmall">Foglio informativo</span></a></li>
+		</ul>
+    </section>
+</div>
--- docs_convergenza/template/tpl_responsive_scheda_carta_amex.php
+++ docs_convergenza/template/tpl_responsive_scheda_carta_amex.php
@@ -1,12 +1,12 @@
-<h2>Carta Amex</h2>
+<h2>Carta American Express</h2>
 <div class="navfrontPage outerTab">
 	<div class="innerTab">
 		<ul class="tabbig">
 			<li class="on"><a href="#">Scheda prodotto</a></li>
-			<li><a href="connect.php?page=strutt_priv.php&tpl=tpl_responsive_costi_condizioni.php&liv0=2&liv1=0&liv2=0&liv3=0&responsive=y#">Costi e condizioni</a></li>
-			<li><a href="connect.php?page=strutt_priv.php&tpl=tpl_responsive_richiesta_carta.php&liv0=2&liv1=0&liv2=0&liv3=0&responsive=y#">Richiesta carta</a></li>
-	</ul>
-</div>
+			<?php print '<li><a href="/template/strutt_'.$site.'.php?tpl=tpl_responsive_costi_condizioni_amex.php&liv0=2&liv1=0&liv2=0&liv3=0&responsive=y#">Costi e condizioni</a></li>' ?>
+			<li><a href="">Richiesta carta</a></li>
+	    </ul>
+    </div>
 	<div class="separator"></div>
 </div>
 <div class="panel-nofill">
@@ -14,7 +14,7 @@
     <section>
         <div class="box-generico">
             <div class="row">
-                <div class="col-sm-4"><br><img src="/WB/fe/img/img_carta_amex_green.jpg" alt="Carta verde american express"></div>
+                <div class="col-sm-4"><br><img src="/common/fe/img/img_carta_amex_green.jpg" alt="Carta verde american express"></div>
                 <div class="col-sm-8">
                 <h3><br>CARTA VERDE AMERICAN EXPRESS</h3>
                 <h4>BONUS DI BENVENUTO</h4>
@@ -30,9 +30,9 @@
             <div class="col-sm-12">
                 <div class="testoIcona">
                     <i class="icon icon-2x flLeft icon-ico_club_membership_rewards" title="ico_rewards"></i>
-                    <div class="leftTesto">
+                    <div class="leftTesto ico-fp">
                         <strong>CLUB MEMBERSHIP REWARDS&reg; </strong>
-                        <p>Con l'iscrizione automatica al Club Membreship Rewards, accumuli punti con le tue spese</p>
+                        <p>Con l'iscrizione automatica al Club Membreship Rewards, accumuli punti con le tue spese.</p>
                     </div>	
                 </div>
             </div>
@@ -41,7 +41,7 @@
             <div class="col-sm-12">
                 <div class="testoIcona">
                     <i class="icon icon-2x flLeft icon-ico_protezione_acquisto" title="ico_protezione_acquisto"></i>
-                    <div class="leftTesto">
+                    <div class="leftTesto ico-fp">
                         <strong>PROTEZIONE D'ACQUISTO </strong>
                         <p>Protezione per 90 giorni fino a 1.300&euro; sugli acquisti con Carta (escluse alcune categorie merceologiche), in caso di furto o danneggiamento del bene.</p>
                     </div>	
@@ -52,7 +52,7 @@
             <div class="col-sm-12">
                 <div class="testoIcona">
                     <i class="icon icon-2x flLeft icon-ico_protezione_acquisto" title="ico_protezione_acquisto"></i>
-                    <div class="leftTesto">
+                    <div class="leftTesto ico-fp">
                         <strong>PROTEZIONE ANTIFRODE</strong>
                         <p>Protezione in caso di uso illecito della Carta da parte di terzi, per utilizzare la Carta in tutta tranquillit&agrave;.</p>
                     </div>	
@@ -61,7 +61,7 @@
         </div>
         <div class="form-group btnWrapper">
             <div class="btn-align-right">
-                <a href="#">Costi e condizioni</a>
+                <a href="#" class="underline">Costi e condizioni</a>
                 <a title="Richiedi" href="#" type="button" class="btn btn-primary">Richiedi</a> <br class="clear">
             </div>
         </div>
@@ -72,7 +72,7 @@
     <section>
         <div class="box-generico">
             <div class="row">
-                <div class="col-sm-4"><br><img src="/WB/fe/img/img_carta_amex_platino.jpg" alt="Carta platino american express"></div>
+                <div class="col-sm-4"><br><img src="/common/fe/img/img_carta_amex_platino.jpg" alt="Carta platino american express"></div>
                 <div class="col-sm-8">
                 <h3><br>CARTA PLATINO AMERICAN EXPRESS</h3>
                 <h4>BONUS DI BENVENUTO</h4>
@@ -88,7 +88,7 @@
             <div class="col-sm-12">
                 <div class="testoIcona">
                    <i class="icon flLeft icon-2x icon-ico_priority_pass" title="priority pass"></i>
-                    <div class="leftTesto">
+                    <div class="leftTesto ico-fp">
                         <strong>PRIORITY PASS&trade; LIVELLO PRESTIGE </strong>
                         <p>Iscrizione gratuita al livello Prestige del Programma Priority Pass, per accedere ad oltre 1.300 VIP Lounge aeroportuali nel mondo.</p>
                     </div>	
@@ -99,7 +99,7 @@
             <div class="col-sm-12">
                 <div class="testoIcona">
                     <i class="icon flLeft icon-2x icon-ico_regalo_ogni_anno" title="regalo ogni anno"></i>
-                    <div class="leftTesto">
+                    <div class="leftTesto ico-fp">
                         <strong>UN REGALO OGNI ANNO</strong>
                         <p>150&euro; di voucher ogni anno per i tuoi viaggi.</p>
                     </div>	
@@ -110,7 +110,7 @@
             <div class="col-sm-12">
                 <div class="testoIcona">
                    <i class="icon flLeft icon-2x icon-ico_servizio_concierge" title="servizio concierge"></i>
-                    <div class="leftTesto">
+                    <div class="leftTesto ico-fp">
                         <strong>SERVIZIO CONCIERGE</strong>
                         <p>Un servizio dedicato, 24/7 a tua disposizione per ogni esigenza, dai viaggi al lifestyle. </p>
                     </div>	
@@ -119,7 +119,7 @@
         </div>
         <div class="form-group btnWrapper">
             <div class="btn-align-right">
-                <a href="#">Costi e condizioni</a>
+                <a href="#" class="underline">Costi e condizioni</a>
                 <a title="Richiedi" href="#" type="button" class="btn btn-primary">Richiedi</a> <br class="clear">
             </div>
         </div>
@@ -130,7 +130,7 @@
     <section>
         <div class="box-generico">
             <div class="row">
-                <div class="col-sm-4"><br><img src="/WB/fe/img/img_carta_amex_gold.jpg" alt="Carta oro american express"></div>
+                <div class="col-sm-4"><br><img src="/common/fe/img/img_carta_amex_gold.jpg" alt="Carta oro american express"></div>
                 <div class="col-sm-8">
                 <h3><br>CARTA ORO AMERICAN EXPRESS</h3>
                 <h4>BONUS DI BENVENUTO</h4>
@@ -146,7 +146,7 @@
             <div class="col-sm-12">
                 <div class="testoIcona">
                     <i class="icon flLeft icon-2x icon-ico_priority_pass" title="priority pass"></i>
-                    <div class="leftTesto">
+                    <div class="leftTesto ico-fp">
                         <strong>PRIORITY PASS</strong>
                         <p>Iscrizione gratuita al Programma Priority Pass, per accedere ad oltre 1.300 VIP Lounge aeroportuali nel mondo.</p>
                     </div>	
@@ -157,7 +157,7 @@
             <div class="col-sm-12">
                 <div class="testoIcona">
                     <i class="icon flLeft icon-2x icon-ico_regalo_ogni_anno" title="regalo ogni anno"></i>
-                    <div class="leftTesto">
+                    <div class="leftTesto ico-fp">
                         <strong>UN REGALO OGNI ANNO</strong>
                         <p>50&euro; di voucher ogni anno per i tuoi viaggi.</p>
                     </div>	
@@ -168,7 +168,7 @@
             <div class="col-sm-12">
                 <div class="testoIcona">
                     <i class="icon flLeft icon-2x icon-ico_eventi_2_1" title="eventi 2x1"></i>
-                    <div class="leftTesto">
+                    <div class="leftTesto ico-fp">
                         <strong>EVENTI 2X1</strong>
                         <p>Biglietti 2x1 per i migliori eventi e una Card per andare al Cinema The Space con chi vuoi. </p>
                     </div>	
@@ -177,12 +177,13 @@
         </div>
         <div class="form-group btnWrapper">
             <div class="btn-align-right">
-                <a href="#">Costi e condizioni</a>
+                <a href="#" class="underline">Costi e condizioni</a>
                 <a title="Richiedi" href="#" type="button" class="btn btn-primary">Richiedi</a> <br class="clear">
             </div>
         </div>
         <br class="clear">
     </section>
+    <hr>
     <section>
        <ul class="documenti">
             <li><a target="_blank" class="btn-icon" href="#" title="Note legali carte american express"><i class="icon icon-file_pdf_fill fLeft" title="Note legali carte american express"></i> 
