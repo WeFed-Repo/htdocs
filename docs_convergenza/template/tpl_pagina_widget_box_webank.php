287fb19eddd2ab6cfec6fb4a7e4740f11e976559
davidecioetto <davide.cioetto@alten.it>
2021-03-01 15:09:22 +0100

    Correttive per problemi testuali

--- docs_convergenza/template/tpl_pagina_widget_box_webank.php
+++ docs_convergenza/template/tpl_pagina_widget_box_webank.php
@@ -28,8 +28,8 @@
     </div>
     <div class="box-description light-bg">
         <h3>Accesso facile e veloce</h3>
-        <p>Grazie al&nbsp;<strong>riconoscimento biometrico</strong>, per accedere e disporre basta la tua&nbsp;<strong>impronta digitale</strong>&nbsp;o il riconoscimento del tuo&nbsp;<strong>viso</strong>.</p>
-        <p>Dopo aver effettuato l'accesso, nella&nbsp;<em>Home</em>&nbsp;hai una visione immediata del Conto corrente, delle Carte, degli Investimenti e dei Finanziamenti attivi.</p>
+        <p>Grazie al <strong>riconoscimento biometrico</strong>, per accedere e disporre basta la tua <strong>impronta digitale</strong> o il riconoscimento del tuo <strong>viso</strong>.</p>
+        <p>Dopo aver effettuato l'accesso, nella  <i>Home </i> hai una visione immediata del Conto corrente, delle Carte, degli Investimenti e dei Finanziamenti attivi.</p>
     </div>
 </div>
 <!-- Fine box autoimpaginato -->
@@ -71,7 +71,7 @@
     <div class="box-description light-bg">
         <h3>Men&ugrave; di navigazione sempre con te</h3>
         <p><strong>Stai esplorando l'app?</strong><br><br>
-            <strong>Il menu di navigazione è sempre in primo piano</strong>.<br>Utilizzalo per accedere velocemente a:&nbsp;<i>Home, Spese, Investimenti, Filiale e Altro</i>.</p>  
+            <strong>Il menu di navigazione è sempre in primo piano</strong>.<br>Utilizzalo per accedere velocemente a: <i>Home, Spese, Investimenti, Filiale e Altro</i>.</p>  
         </p>
     </div>
 </div>
@@ -90,13 +90,13 @@
     </div>
     <div class="box-description light-bg">
         <h3>Nuovi modi di interagire con l'app</h3>
-        <p>Per utilizzare wbapp bastano pochi e semplici gesti.</p>
-        <p>Nell'<strong><em>area di benvenuto</em></strong>, senza inserire la password, puoi:</p>
+        <p>Per utilizzare <strong>App Webank</strong> bastano pochi e semplici gesti.</p>
+        <p>Nell'<strong> <i>area di benvenuto </i></strong>, senza inserire la password, puoi:</p>
         <ul>
-            <li><strong>visualizzare il saldo</strong>&nbsp;con uno&nbsp;<strong>shake</strong></li>
-            <li><strong>accedere alle&nbsp;<i>Funzioni Veloci</i></strong>. Se le hai già impostate le trovi&nbsp;<strong>trascinando verso il basso la linguetta</strong>&nbsp;e in pochi tap puoi ricaricare il cellulare o la carta prepagata</li>
+            <li><strong>visualizzare il saldo</strong> con uno <strong>shake</strong></li>
+            <li><strong>accedere alle <i>Funzioni Veloci</i></strong>. Se le hai già impostate le trovi <strong>trascinando verso il basso la linguetta</strong> e in pochi tap puoi ricaricare il cellulare o la carta prepagata</li>
         </ul>
-        <p>Una volta effettuato l'accesso, puoi operare con il&nbsp;<strong>pulsante dispositivo</strong>&nbsp;che trovi nelle diverse aree.</p>
+        <p>Una volta effettuato l'accesso, puoi operare con il <strong>pulsante dispositivo</strong> che trovi nelle diverse aree.</p>
     </div>
 </div>
 <!-- Fine box autoimpaginato -->
@@ -113,7 +113,7 @@
     </div>
     <div class="box-description light-bg">
         <h3>Come fare un bonifico</h3>
-        <p>Dall’area&nbsp;<i>Home</i>, selezionando il&nbsp;<strong>pulsante dispositivo</strong>, che trovi sempre in primo piano, puoi inserire i tuoi Bonifici: scegli fra&nbsp;<strong>Ordinario</strong>&nbsp;o&nbsp;<strong>Veloce</strong>,&nbsp;<strong>Italiano</strong>&nbsp;o&nbsp;<strong>Estero</strong>.</p>
+        <p>Dall’area <i>Home</i>, selezionando il <strong>pulsante dispositivo</strong>, che trovi sempre in primo piano, puoi inserire i tuoi Bonifici: scegli fra <strong>Ordinario</strong> o <strong>Veloce</strong>, <strong>Italiano</strong> o <strong>Estero</strong>.</p>
     </div>
 </div>
 <!-- Fine box autoimpaginato -->
@@ -130,8 +130,8 @@
     </div>
     <div class="box-description light-bg">
         <h3>Portafoglio per prodotti posseduti e asset class</h3>
-        <p>L’area&nbsp;<i>Investimenti</i>&nbsp;ti offre una visione d’insieme del tuo portafoglio in base agli strumenti che hai acquistato, suddivisa fra: azioni, obbligazioni, ETF, fondi, CW/certificate e derivati.<br>
-        Seleziona una di queste categorie per entrare nel&nbsp;<strong>dettaglio</strong>&nbsp;del tuo portafoglio ed eventualmente modificare le tue posizioni.</p>
+        <p>L’area <i>Investimenti</i> ti offre una visione d’insieme del tuo portafoglio in base agli strumenti che hai acquistato, suddivisa fra: azioni, obbligazioni, ETF, fondi, CW/certificate e derivati.<br>
+        Seleziona una di queste categorie per entrare nel <strong>dettaglio</strong> del tuo portafoglio ed eventualmente modificare le tue posizioni.</p>
     </div>
 </div>
 <!-- Fine box autoimpaginato -->
