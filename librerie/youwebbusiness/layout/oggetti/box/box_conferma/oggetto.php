6026bbef2779f6645531e5d1bda97d76af365b2d
sstrepponi <62430689+sstrepponi@users.noreply.github.com>
2021-03-05 10:35:43 +0100

    Modale con immagine + box di conferma con spunta YWB
    Modale con immagine + box di conferma con spunta YWB

---
+++ librerie/youwebbusiness/layout/oggetti/box/box_conferma/oggetto.php
@@ -0,0 +1,14 @@
+<section>
+	<div class="bordered pb-3">
+		<div class="row">
+			<div class="col-12">
+				<h4>Lorem ipsum dolor sit</h4>
+				<p>Lorem ipsum dolor sit amet, <strong>consectetur adipiscing elit</strong>, sed do eiusmod tempor incididunt </p>
+				<div class="esito-inline">
+					<img src="./fe/img/icon/autorizza.svg" class="esito-icon">
+                	<p class="esito-text"><strong>lorem ipsum</strong></p>
+                </div>
+			</div>
+		</div>
+	</div>
+</section>
---
+++ librerie/youwebbusiness/layout/oggetti/modale/modale_con_immagine/oggetto.php
@@ -0,0 +1,6 @@
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-modal-open="img">
+  Launch demo modal with image
+</button>
+
+<?php virtual('/youwebbusiness/layout/oggetti/modale/modale_con_immagine/modale.php');?>
