1994688ca29472fb19bfe951c25f424ee4a2e9e8
sstrepponi <62430689+sstrepponi@users.noreply.github.com>
2021-03-10 11:24:04 +0100

    accordion documenti + box documenti YWB
    accordion documenti + box documenti YWB

--- librerie/youwebbusiness/layout/oggetti/box/box_documenti/oggetto.php
+++ librerie/youwebbusiness/layout/oggetti/box/box_documenti/oggetto.php
@@ -1,21 +1,28 @@
-<section>
-	<div class="bordered">
-		<div class="row">
-			<div class="col-12">
-				<h4>Lorem ipsum dolor sit</h4>
-				<hr class="m-neg">
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit:</p>
-			</div>
+<div class="box-document-style">
+	<div class="card">
+		<div class="card-header">
+			<h5 class="card-header-title mb-0">Lorem ipsum dolor sit<h5>
 		</div>
-		<div class="row">
-			<div class="col-12 col-md-6">
-				<div class="list pb-3"><a href="#" aria-label="PDF"><span><img src="./fe/img/icon/pdf.svg"></span>lorem ipsum1.pdf</a></div>
-				<div class="list pb-3"><a href="#" aria-label="PDF"><span><img src="./fe/img/icon/pdf.svg"></span>lorem ipsum1.pdf</a></div>
-			</div>
-			<div class="col-12 col-md-6">
-				<div class="list pb-3"><a href="#" aria-label="PDF"><span><img src="./fe/img/icon/pdf.svg"></span>lorem ipsum1.pdf</a></div>
-				<div class="list pb-3"><a href="#" aria-label="PDF"><span><img src="./fe/img/icon/pdf.svg"></span>lorem ipsum1.pdf</a></div>
+		<div class="card-body">
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit:</p>
+			<div class="row">
+				<div class="col-12 col-md-6 pb-2">
+					<a href="#" aria-label="PDF"><span><img src="./fe/img/icon/pdf.svg"></span>lorem ipsum1.pdf</a>
+				</div>
+				<div class="col-12 col-md-6 pb-2">
+					<a href="#" aria-label="PDF"><span><img src="./fe/img/icon/pdf.svg"></span>lorem ipsum2.pdf</a>
+				</div>
+				<div class="col-12 col-md-6 pb-2">
+					<a href="#" aria-label="PDF"><span><img src="./fe/img/icon/pdf.svg"></span>lorem ipsum2.pdf</a>
+				</div>
+				<div class="col-12 col-md-6 pb-2">
+					<a href="#" aria-label="PDF"><span><img src="./fe/img/icon/pdf.svg"></span>lorem ipsum2.pdf</a>
+				</div>
 			</div>
 		</div>
 	</div>
-</section>
+</div>	
+			
+		
+	
+
