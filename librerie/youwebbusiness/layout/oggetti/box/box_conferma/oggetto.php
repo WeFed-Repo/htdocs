66c5340aa2e72c692764a0471a16fb77512c67b6
sstrepponi <62430689+sstrepponi@users.noreply.github.com>
2021-03-11 10:21:57 +0100

    oggetti YWB
    oggetti YWB

--- librerie/youwebbusiness/layout/oggetti/box/box_conferma/oggetto.php
+++ librerie/youwebbusiness/layout/oggetti/box/box_conferma/oggetto.php
@@ -2,7 +2,6 @@
 	<div class="bordered pb-3">
 		<div class="row">
 			<div class="col-12">
-				<h4>Lorem ipsum dolor sit</h4>
 				<p>Lorem ipsum dolor sit amet, <strong>consectetur adipiscing elit</strong>, sed do eiusmod tempor incididunt </p>
 				<div class="esito-inline">
 					<img src="./fe/img/icon/autorizza.svg" class="esito-icon">
--- librerie/youwebbusiness/layout/oggetti/box/box_no_promo/oggetto.php
+++ librerie/youwebbusiness/layout/oggetti/box/box_no_promo/oggetto.php
@@ -2,7 +2,7 @@
 <section>
 	<div class="box-promo no-promo">
 		<div class="row">
-			<div class="col-md col-sm-12">
+			<div class="col-md col-sm-12 pt-0">
 				<div class="h-100 box-promo-left d-flex justify-content-center align-items-center">
 					<img src="./fe/img/placeholder_box_no_promo.png" alt="immagine box mo promo"/>
 				</div>
@@ -11,7 +11,7 @@
 		<div class="row">	
 			<div class="col-md col-sm-12 text-center">
 				<h4 class="title-blue">Lorem ipsum dolor sit</h4>
-				<p class="text-box">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut e</p>
+				<p class="text-box">Lorem ipsum dolor sit amet, consectetur adipisicing elit.<br> sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut e</p>
 			</div>
 		</div>
 		
