7139b319c29289fec03c200916d490cabc296b49
sstrepponi <62430689+sstrepponi@users.noreply.github.com>
2021-03-12 17:17:14 +0100

    YWB oggetti
    YWB oggetti

---
+++ librerie/youwebbusiness/layout/oggetti/box/box_stepper/oggetto.php
@@ -0,0 +1,75 @@
+
+<section>
+  <div class="bordered">
+    <h2>Lorem ipsum</h2>
+    <p>lorem ipsum <strong>dolor</strong> sit</p>
+        <div class="step-wrapper">
+            <div class="row state-active">
+              <div class="col-12 col-md-4 col-lg-3 pr-0">
+                <div class="step-name">
+                    <div class="step-reas">Richiesta finanziamento</div>
+                    <div class="step-date">10.06.2020</div>
+                    <div class="step-number"><span>1</span></div>
+                    <div class="step-separator"></div>
+                </div>
+              </div>
+              <div class="col-12 col-md-8 col-lg-9 step-point">
+                  <div class="step-state">
+                      <span>Stato:</span>
+                      <button class="btn btn-stepper">DA COMPILARE</button>
+                      <div class="step-separator"></div>
+                      <span class="step-output">
+                        <span class="step-output-name">Compilato da <strong>Mario Rossi</strong></span>
+                        <span class="step-output-edit">Modifica</span>
+                      </span>
+                  </div>
+                  
+              </div>
+           </div>
+           <div class="row state-done">
+              <div class="col-12 col-md-4 col-lg-3 pr-0">
+                <div class="step-name">
+                    <div class="step-reas">Richiesta finanziamento</div>
+                    <div class="step-date">10.06.2020</div>
+                    <div class="step-number"><span>1</span></div>
+                    <div class="step-separator"></div>
+                </div>
+              </div>
+              <div class="col-12 col-md-8 col-lg-9 step-point">
+                  <div class="step-state">
+                      <span>Stato:</span>
+                      <button class="btn btn-stepper">DA COMPILARE</button>
+                      <div class="step-separator"></div>
+                      <span class="step-output">
+                        <span class="step-output-name">Compilato da <strong>Mario Rossi</strong></span>
+                        <span class="step-output-edit">Modifica</span>
+                      </span>
+                  </div>
+                 
+              </div>
+           </div>
+           <div class="row state-disactive">
+              <div class="col-12 col-md-4 col-lg-3 pr-0">
+                <div class="step-name">
+                    <div class="step-reas">Allegato 2bis</div>
+                    <div class="step-date">10.06.2020</div>
+                    <div class="step-number"><span>2</span></div>
+                    <div class="step-separator"></div>
+                </div>
+              </div>
+              <div class="col-12 col-md-8 col-lg-9 step-point">
+                  <div class="step-state">
+                      <span>Stato:</span>
+                      <button class="btn btn-stepper">DA COMPILARE</button>
+                      <div class="step-separator"></div>
+                      <span class="step-output">
+                        <span class="step-output-name">Compilato da <strong>Mario Rossi</strong></span>
+                        <span class="step-output-edit">Modifica</span>
+                      </span>
+                  </div>
+                
+              </div>
+           </div>
+        </div>
+  </div>
+</section>
