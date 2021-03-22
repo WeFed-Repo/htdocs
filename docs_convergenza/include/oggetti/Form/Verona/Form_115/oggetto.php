460edf6bc366c97055400dac14f14af5057ba323
sstrepponi <62430689+sstrepponi@users.noreply.github.com>
2021-03-22 09:44:18 +0100

    oggetto loading con classe top
    oggetto loading con classe top

--- docs_convergenza/include/oggetti/Form/Verona/Form_115/oggetto.php
+++ docs_convergenza/include/oggetti/Form/Verona/Form_115/oggetto.php
@@ -26,13 +26,37 @@
 			<input type="text" name="note" maxlength="50" size="55" value="Opzionale"  class="form-control placeh clear-x">
 		</div>
     </div>
+	
 </form>
 
 <script>
     //passare come parametri l'id del wrapper da coprire e l'id del testo da visualizzare
 	$('#setLoadingLink').on('click', function() {
-		setLoadingOnObject('wrapperToLoad','msgLoading');
+		setLoadingOnObject('wrapperToLoad','msgLoading','loading-top');
 		//emulo l'unset
-		setTimeout( function() {unsetLoadingOnObject('wrapperToLoad','msgLoading');}, 2000);
+		setTimeout( function() {unsetLoadingOnObject('wrapperToLoad','msgLoading','loading-top');}, 20000);
 	})
 </script>
+
+
+
+
+<div id="base"><!-- Footer (Group) -->
+<div class="ax_default" id="u5521" data-height="44" data-width="134" data-top="498" data-left="175" data-label="Footer"><!-- Unnamed (Rectangle) -->
+<div class="ax_default button_-_secondary_" id="u5522">
+<div tabindex="0" id="u5522_div">Non &egrave; possibile proseguire con la revoca della linea di trading in quanto <strong>su questa linea risultano attive delle deleghe</strong> (sei stato delegato ad operare o hai delegato qualcuno altro).<br /><br /></div>
+</div>
+</div>
+</div>
+<div class="ax_default paragraph_-_desktop" id="u5523">
+<div class="text " id="u5523_text">
+<p>Per poter proseguire dovrai prima revocare suddette deleghe: questa operazione <strong>pu&ograve; essere effettuata solo in filiale e in presenza di tutte le persone coinvolte. Accertati della loro disponibilit&agrave; prima di prendere l'appuntamento in filiale.</strong></p>
+<a class="link-text text-underline noMargin" onclick="javascript:goToPage('/serviziPerTe/richiestaAppuntamentoCompilazione.do');">Prendi appuntamento</a>
+</div>
+</div>
+<p style="text-align: right;"></p>
+<section>
+<div class="pull-left btnWrapper">
+	<input name="homepage" value="OK, ho capito" onclick="javascript:goToPage('/homepage.do');" class="btn btn-primary noMargin" type="button" /></div>
+</section>
+<section></section>
