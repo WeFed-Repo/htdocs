7bc288338b8ba39079b11b5a71b804ae6c58f899
davidecioetto <davide.cioetto@alten.it>
2021-04-21 17:13:24 +0200

    Modifiche per spacchettamento template

--- docs_convergenza/template/tpl_documenti_firma.php
+++ docs_convergenza/template/tpl_documenti_firma.php
@@ -15,24 +15,22 @@
         width: 100%;
     }
 
-    .documentIndexContainer {
+    /* Titoletto con index container */
+    .titleIndexContainer {
         text-align: left;
-    }
-
-    .documentIndexContainer p {
         font-size: 14px;
         font-weight: 600;
         padding: 0px;
+        margin-bottom: 10px;
     }
 
-    .documentIndexContainer span {
+    .titleIndexContainer span {
         font-size: 18px;
     }
 
+    /* Titolo di documento sulla destra */
     .documentTitleContainer {
         padding-bottom: 5px;
-    }
-    .documentTitleContainer h2 {
         font-size: 16px;
         font-weight: normal;
         text-align: right;
@@ -42,6 +40,7 @@
     /* Acceptance element*/
     .accept-wrapper {
         position: relative; 
+        margin: 3px 0 15px;
     }
 
     .accept-wrapper .icon {
@@ -56,15 +55,11 @@
         cursor: pointer;
     }
 
-    .accept-wrapper p {
+    .accept-wrapper .text {
         padding-left: 30px;
     }
 
-    .accept-wrapper span {
-        font-weight: bold;
-    }
-
-    .accept-wrapper.accepted p {
+    .accept-wrapper.accepted .text {
         font-weight: bold;
     }
 
@@ -115,37 +110,6 @@
         padding-left: 24px;
     }
 
-    /* Signature Area */
-    .signature-wrapper .esito-inline {
-        margin: 40px 0px;
-    }
-
-    .signature-wrapper .icon {
-        font-size: 24px;
-        margin-top: 5px;
-    }
-    .signature-wrapper .esito-inline p {
-        font-weight: bold;
-    }
-
-    .signature-wrapper .esito-inline span {
-        font-style: italic;
-        margin-left: 10px;
-        font-size: 15px;
-    }
-
-    .signature-wrapper .note {
-        padding-top: 15px;
-        padding-bottom: 10px;
-    }
-
-    .signature-wrapper .btn-inline.no-label {
-        margin-top: 8px;
-    }
-
-    .signature-wrapper .btn-inline.no-label a {
-        margin: 0px;
-    }
 
     /* Note */
     p.note.to-right{
@@ -169,44 +133,33 @@
             </div>
         </div>
     </div>
-    <div class="titolo">
-        <h1>
-            <div class="row">
-                <div class="col-sm-8">
-                    <span>NOME_PRATICA_UNIVOCO</span>
-                </div>
+    <!-- Titolo -->
+    <h1>
+        <div class="row">
+            <div class="col-sm-8">
+                <span>NOME_PRATICA_UNIVOCO</span>
             </div>
-        </h1>
-        <p>Prendi visione della documentazione e apponi le firme o le spunte dove richiesto.</p>  
-    </div>
+        </div>
+    </h1>
+    <!-- Fine titolo -->
+    <p>Prendi visione della documentazione e apponi le firme o le spunte dove richiesto.</p>
 <section> 
 
 <!-- NEW -->
 <div class="row">
 	<div class="col-sm-6">
-        <!--<section>-->
-            <div class="documentIndexContainer">
-                <p>Documento
-                    <span>1</span>
-                    di
-                    <span>4</span>
-                </p>
-            </div>
-        <!--</section>-->
-	</div>
+        <div class="titleIndexContainer">Documento <span>1</span> di <span>4</span></div>
+    </div>
     <div class="col-sm-6">
-        <!--<section>-->
-            <div class="documentTitleContainer">
-                <h2>“NOME_DOCUMENTO_1_DA_VISIONARE”</h2>
-            </div>
-        <!--</section>-->
+        <div class="documentTitleContainer">"NOME_DOCUMENTO_1_DA_VISIONARE"</div>
     </div>
 </div>
 
-
+<!-- Box wrapper pdf -->
 <section>
     <div id="pdf"></div>
 </section>
+<!-- Fine box pdf -->
 
 <!-- NEW -->
 <section>
@@ -215,11 +168,11 @@
             <!-- Confirmation -->
             <div class="accept-wrapper">
                 <i class="icon css-square" title="css-square"></i>
-                <p>Ho preso visione del documento <span>“NOME_DOCUMENTO_1_DA_VISIONARE”</span></p>
+                <span class="text">Ho preso visione del documento <strong>"NOME_DOCUMENTO_1_DA_VISIONARE"</strong></span>
             </div>
             <div class="accept-wrapper accepted">                
                 <i class="icon icon-spunta" title="icon-spunta"></i>
-                <p>Ho preso visione del documento <span>“NOME_DOCUMENTO_1_DA_VISIONARE”</span></p>
+                <span class="text">Ho preso visione del documento <strong>"NOME_DOCUMENTO_1_DA_VISIONARE"</strong></span>
             </div>           	           
         </div>
         <div class="col-sm-1">
@@ -232,57 +185,57 @@
     </div>
 </section>
 
-<!-- Signature Area -->  
+<!-- Signature 1-->  
 <section>         
-    <div class="signature-wrapper">
-        <!-- Signature Confirmation -->
-        <!-- Esito inline Box 033 -->
-        <div class="signature-notifications">
-            <div class="esito-inline">
-                <i class="icon iconBig icon-alert_ok flLeft"></i>
-                <p class="esito-text">FIRMA 1 <span>Firmato digitalmente</span></p>
-            </div>
-            <div class="esito-inline">
-                <i class="icon iconBig icon-alert_ok flLeft"></i>
-                <p class="esito-text">FIRMA 2 <span>Firmato digitalmente</span></p>
-            </div>
-        </div>
-        <!-- Signature Box-->
-        <div id="signature-area">
-            <div class="documentIndexContainer">
-                <p>Firma
-                    <span>1</span>
-                    di
-                    <span>3</span>
-                </p>
-            </div>
-            <div class="formWrapper">
-                <div class="form-group">
-                    <div class="row">
-                        <div class="col-xs-12 col-sm-12">
-                            <p class="note">Inserisci la Password che usi per accedere al sito</p>
-                        </div>
-                        <div class="form-field-input col-xs-12 col-sm-3">
-                            <!-- <label class="control-label">Inserisci la Password che usi per accedere al sito</label> -->
-                            <input type="text" id="" value="" name="" autocomplete="off" tabindex="1" class="form-control"> 
-                        </div>
-                        <div class="form-field-input col-xs-12 col-sm-9 btn-inline no-label">
-                            <a type="button" href="#" class="btn btn-primary btn-gen-pwd" title="Firma">Firma</a>
-                        </div>
+    <div class="esito-inline">
+        <i class="icon iconBig icon-alert_ok flLeft"></i>
+        <p class="esito-text"><i><strong>FIRMA 1&nbsp; &nbsp;&nbsp;Firmato digitalmente</strong></i></p>
+    </div>
+</section>
+<!-- Fine signature 1-->
+
+<!-- Signature 2 -->  
+<section>         
+    <div class="esito-inline">
+        <i class="icon iconBig icon-alert_ok flLeft"></i>
+        <p class="esito-text"><i><strong>FIRMA 2&nbsp; &nbsp;&nbsp;Firmato digitalmente</strong></i></p>
+    </div>
+</section>
+<!-- Fine signature 2-->
+
+<!-- Blocco firma -->
+<section>
+
+    <div class="titleIndexContainer">Firma <span>3</span> di <span>3</span></div>
+
+    <div class="formWrapper">
+        <div class="form-group">
+            <div class="form-field-input">
+                <label class="control-label">Inserisci la Password che usi per accedere al sito</label>
+                <div class="row">
+                    <div class="col-xs-12 col-sm-3 no-label">
+                        <input type="text" id="" value="" name="" autocomplete="off" class="form-control"> 
+                    </div>
+                    <div class="col-xs-12 col-sm-9 no-label">
+                        <a type="button" href="#" class="btn btn-primary  btn-inline btn-no-label" title="Firma">Firma</a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </section>
+<!-- Fine blocco firma -->
 
 <section>
     <!-- Testo_010 -->
     <!-- Bottone_002 --> 
     <div class="form-group btnWrapper clearfix"> 
-        <p class="note to-right">Cliccando “Prosegui” non potrai più modificare i passaggi precedenti</p>    
-  		<div class="btn-align-right">
+        <!-- Testo introduttivo per i bottoni -->
+        <p class="note to-right">Cliccando "Prosegui" non potrai più modificare i passaggi precedenti</p>    
+  		<!-- Fine testo introduttivo per i bottoni -->
+        <div class="btn-align-right">
   		 	<div>
+                <a type="button" href="#" class="btn btn-default disabled" title="prosegui">riprendi dopo</a>
                 <a type="button" href="#" class="btn btn-default">riprendi dopo</a>
   				<a type="button" href="#" class="btn btn-primary" title="prosegui">prosegui</a>
   			</div>
@@ -290,36 +243,8 @@
     </div>
 </section>
 
-<section>
-    <!-- Testo_010 -->
-    <!-- Bottone_002 --> 
-    <div class="form-group btnWrapper clearfix"> 
-        <p class="note to-right">Cliccando “Concludi” non potrai più modificare i passaggi precedenti</p>    
-  		<div class="btn-align-right">
-  		 	<div>
-                <a type="button" href="#" class="btn btn-default">riprendi dopo</a>
-  				<a type="button" href="#" class="btn btn-primary" title="concludi">concludi</a>
-  			</div>
-        </div>
-    </div>
-</section>
-
-<section>
-    <!-- Testo_010 -->
-    <!-- Bottone_002 --> 
-    <div class="form-group btnWrapper clearfix"> 
-        <p class="note to-right">Cliccando “Concludi” non potrai più modificare i passaggi precedenti</p>    
-  		<div class="btn-align-right">
-  		 	<div>
-                <a type="button" href="#" class="btn btn-default disabled" title="prosegui">riprendi dopo</a>
-  				<a type="button" href="#" class="btn btn-primary" title="concludi">concludi</a>
-  			</div>
-        </div>
-    </div>
-</section>
-
-
 <script>
+
     window.onload = function() {
         /*With IE only <IFRAME> work with Open PDF Params and Only with In-Link Params
         IMPORTANT: Use only Named Destinations, NOT anchors or bookmarks for IE
