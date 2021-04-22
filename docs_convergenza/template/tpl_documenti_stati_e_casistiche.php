793eaab60f9073bd14403afe781c0a761bb3aa2a
SG-Alten <simone.grippa@alten.it>
2021-04-22 12:36:37 +0200

    Update tpl_documenti_stati_e_casistiche.php

--- docs_convergenza/template/tpl_documenti_stati_e_casistiche.php
+++ docs_convergenza/template/tpl_documenti_stati_e_casistiche.php
@@ -60,12 +60,12 @@
     }
 
     /* Change Positions */
-    .text-with-icon.right a, .text-with-icon.right .text, .text-with-icon.right .sub-text{
+    .text-with-icon.right-icon .text, .text-with-icon.right-icon .sub-text{
         padding-left: 0px;
         padding-right: 24px;
     }
 
-    .text-with-icon.right .image, .text-with-icon.right .glyph, .text-with-icon.right .icon{
+    .text-with-icon.right-icon .image, .text-with-icon.right-icon .glyph, .text-with-icon.right-icon .icon{
         right: 0px;
         left: auto;
     }
@@ -100,59 +100,249 @@
         display: block;
     }
 
+    /* # Accordion Content # */
+    .accord-content {
+        padding: 20px 24px;
+    }
+    
+    .accord-content .text {
+        margin-bottom: 20px;
+        display: block;
+    }
+
+    .accord-content .read-more {
+        color: #0e977f;
+        text-decoration: underline;
+        font-weight: bold;
+    }
+
+    /* ## Bullet Route ## */
+    .accord-content .history-element.inactive .bullet, .accord-content .history-element.inactive .bullet-route {
+        background-color:#EBEDEE;
+    }
+
+    .accord-content .history-element.inactive a{
+        color: black;
+    }
+
+    /* .accord-content .history-element:first-of-type .bullet-route{
+        background-color:red;
+    } */
+
+    .bullet-wrapper {
+        position:relative; 
+        display:inline-block; 
+        height:30px; 
+        width:30px;
+    }
+
+    .bullet-wrapper .bullet {
+        position:absolute; 
+        width:10px; 
+        height: 10px; 
+        left:11px; top: 22px; 
+        background-color:#0e977f; 
+        border-radius:5px;
+        z-index:1;
+    }
+
+    .bullet-wrapper .bullet-route {
+        position:absolute; 
+        width:2px; 
+        height: 40px; 
+        left:15px; 
+        top: -18px; 
+        background-color:#0e977f;
+    }
+
+    /* # Linked Icon */
+    .linked-icon .icon {
+        color: #0e977f;
+    }
+
+    .linked-icon.disabled .icon {
+        color: #EBEDEE;
+        cursor: default;
+    }
+
+
 </style>
+
+<section>
+    <!-- Titolo_001 -->
+    <div class="titolo">
+        <h1>
+            <div class="row">
+                <div class="col-sm-8">
+                    <span>CONTRATTI E PROPOSTE</span>
+                </div>
+            </div>
+        </h1>  
+    </div>
+</section>
+
+<div class="section outerWrapperTab no-backgr">
+	<div class="row">
+		<div class="col-xs-12">
+			<div class="innerWrapperTab">
+				<ul>
+					<li class="on"><a href="#" title="" role="button">In corso</a></li>
+					<li><a href="#"  role="button">Archivio</a></li>
+				</ul>
+			</div>
+			<div class="separator"></div>
+		</div>
+	</div>
+</div>
 
 <!-- NEW -->
 <section>
     <!-- ROW 1 -->
-    <div class="row boxed">
-        <div class="col-sm-3">
-            <!-- Text with Icon + sub-text -->
-            <div class="text-with-icon">
-                <a class="linker" href="#">
-                    <span class="icon icon-arrow_down" title="icon-arrow_down"></span>
-                    <span class="text underline">NOME_PRATICA_UNIVOCO_1</span>
-                    <span class="sub-text">INV-0018</span>
-                </a>
-            </div>
-        </div>
+    <div class="row boxed">       
+        <div class="col-sm-12">
+            <!-- ROW 1.1 -->
+            <div class="row">
+                <div class="col-sm-3">
+                    <!-- Text with Icon + sub-text -->
+                    <div class="text-with-icon">
+                        <a class="linker" href="#">
+                            <span class="icon icon-arrow_down" title="icon-arrow_down"></span>
+                            <span class="text underline">NOME_PRATICA_UNIVOCO_1</span>
+                            <span class="sub-text">INV-0018</span>
+                        </a>
+                    </div>
+                </div>
 
-	    <div class="col-sm-2">
-            <!-- Casistica Icon -->
-            <div class="text-with-icon">
-                <span class="icon icon-numeroverde_desktop" title="icon-numeroverde_desktop"></span>
-                <a href="#">Gestore</a>
-            </div>
-        </div>
+                <div class="col-sm-2">
+                    <!-- Casistica Icon -->
+                    <div class="text-with-icon">
+                        <span class="icon icon-numeroverde_desktop" title="icon-numeroverde_desktop"></span>
+                        <a href="#">Gestore</a>
+                    </div>
+                </div>
 
-        <div class="col-sm-2">
-            <span class="text">20/03/2021</span>
-        </div>
+                <div class="col-sm-2">
+                    <span class="text">20/03/2021</span>
+                </div>
 
-        <div class="col-sm-2">
-            <h3 class="titleSection titleForm">NUOVA
-                <a href="#" class="general-help help-icon hidden-xs"><i class="icon icon-info_fill icon-2x"></i></a>
-            </h3>
-        </div>
+                <div class="col-sm-2">
+                    <div class="text-with-icon right-icon">
+                        <a class="linker" href="#">
+                            <span class="icon icon-info_fill" title="icon-info_fill"></span>
+                            <span class="text">NUOVA</span>
+                        </a>
+                    </div>
+                </div>
 
-        <div class="col-sm-2">
-            <div class="form-group btnWrapper clearfix">     
-                <div class="btn-align-right">
-                    <div>
-                        <!-- <a type="button" href="#" class="btn btn-default disabled" title="sottoscrivi">sottoscrivi</a> -->
-                        <!-- <a type="button" href="#" class="btn btn-default">sottoscrivi</a> -->
-                        <a type="button" href="#" class="btn btn-primary" title="sottoscrivii">Sottoscrivi</a>
+                <div class="col-sm-2">
+                    <div class="form-group btnWrapper clearfix">     
+                        <div class="btn-align-right">
+                            <div>
+                                <!-- <a type="button" href="#" class="btn btn-default disabled" title="sottoscrivi">sottoscrivi</a> -->
+                                <!-- <a type="button" href="#" class="btn btn-default">sottoscrivi</a> -->
+                                <a type="button" href="#" class="btn btn-primary" title="sottoscrivii">Sottoscrivi</a>
+                            </div>
+                        </div>
+                    </div>            
+                </div>
+
+                <div class="col-sm-1">
+                    <div class="linked-icon">
+                        <a href="#">
+                            <span class="icon icon-trash_filled"></span>
+                        </a>
                     </div>
                 </div>
             </div>
-        </div>
+            <!-- END ROW 1.1 -->
 
-        <div class="col-sm-1">
-            <div class="linked-icon">
-                <a href="#" type="button" data-toggle="dropdown" title="Alert">
-                    <span class="icon icon-trash_filled"></span>
-                </a>
+            <!-- ROW 1.2 -->
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="accord-content">
+                        <h3>NOME_PRATICA_UNIVOCO_1</h3>
+                        <span class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque imperdiet ex id erat pretium, et accumsan 
+                        eros pharetra. In vitae dolor condimentum, fringilla nunc vitae, molestie sem. Nulla maximus fermentum mauris congue 
+                        pellentesque. Mauris turpis sapien, vestibulum a egestas ut, luctus sed justo. Aenean felis nisl, molestie id sapien 
+                        <a class="read-more" href="#">VEDI DI PI&#218; ></a></span>
+                    
+                        <!-- ROW 1.2.1 -->
+                        <div class="row">
+                            <div class="col-sm-8">
+                                <!-- NEW ELEMENT -->
+                                <div class="history-element">
+                                    <div class="bullet-wrapper">
+                                        <div class="bullet"></div> 
+                                        <div class="bullet-route"></div>                               
+                                    </div>
+                                    <!-- Casistica Icon -->
+                                    <div class="text-with-icon" style="display:inline-block;">
+                                        <span class="glyph glyph-uread_pdf layered" title="glyph-uread_pdf layered"></span>
+                                        <a href="#">Nome_documento_1</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-2">
+                                <span class="icon icon-check_ok"></span>
+                            </div>
+                            <div class="col-sm-2">
+                                <span class="text">20/03/2021</span>
+                            </div>
+                        </div>
+                        <!-- END ROW 1.2.1 -->
+
+                        <!-- ROW 1.2.2 -->
+                        <div class="row">
+                            <div class="col-sm-8">
+                                <!-- NEW ELEMENT -->
+                                <div class="history-element">
+                                    <div class="bullet-wrapper">
+                                        <div class="bullet"></div> 
+                                        <div class="bullet-route"></div>                               
+                                    </div>
+                                    <!-- Casistica Icon -->
+                                    <div class="text-with-icon" style="display:inline-block;">
+                                        <span class="glyph glyph-uread_pdf layered" title="glyph-uread_pdf layered"></span>
+                                        <a href="#">Nome_documento_2</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-2">
+                                <span class="icon icon-check_ok"></span>
+                            </div>
+                            <div class="col-sm-2">
+                                <span class="text">20/03/2021</span>
+                            </div>
+                        </div>
+                        <!-- END ROW 1.2.2 -->
+                        <!-- ROW 1.2.3 -->
+                        <div class="row">
+                            <div class="col-sm-8">
+                                <!-- NEW ELEMENT -->
+                                <div class="history-element inactive">
+                                    <div class="bullet-wrapper">
+                                        <div class="bullet"></div> 
+                                        <div class="bullet-route"></div>                                 
+                                    </div>
+                                    <!-- Casistica Icon -->
+                                    <div class="text-with-icon" style="display:inline-block;">
+                                        <span class="glyph glyph-uread_pdf layered" title="glyph-uread_pdf layered"></span>
+                                        <a href="#">Nome_documento_3</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-2">
+                                <span class="text"> - </span>
+                            </div>
+                            <div class="col-sm-2">
+                                <span class="text"> - </span>
+                            </div>
+                        </div>
+                        <!-- END ROW 1.2.3 -->
+                    </div>
+                </div>
             </div>
+            <!-- END ROW 1.2 -->
         </div>
     </div>
     <!-- END ROW 1 -->
@@ -180,10 +370,12 @@
             <span class="text">20/03/2021</span>
         </div>
         <div class="col-sm-2">
-            <div class="text-with-icon right">
-                <span class="icon icon-info_fill" title="icon-info_fill"></span>
-                <span class="text">NUOVA</span>
-                <span class="sub-text">in attesa cointestatario</span>
+            <div class="text-with-icon right-icon">
+                <a class="linker" href="#">
+                    <span class="icon icon-info_fill" title="icon-info_fill"></span>
+                    <span class="text">NUOVA</span>
+                    <span class="sub-text">in attesa cointestatario</span>
+                </a>
             </div>
         </div>
         <div class="col-sm-2">
@@ -198,8 +390,8 @@
             </div>
         </div>
         <div class="col-sm-1">
-            <div class="linked-icon">
-                <a href="#" type="button" data-toggle="dropdown" title="Alert">
+            <div class="linked-icon disabled">
+                <a href="#">
                     <span class="icon icon-trash_filled"></span>
                 </a>
             </div>
