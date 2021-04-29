f8947489fddba4e6316ff343e319ce08dea7eca1
davidecioetto <davide.cioetto@alten.it>
2021-04-29 12:27:21 +0200

    Aggiunta funzione

--- docs_convergenza/template/tpl_documenti_conferma.php
+++ docs_convergenza/template/tpl_documenti_conferma.php
@@ -62,10 +62,14 @@
 </section>
 
 <!-- add to body overflow-y: hidden; -->
-<div style="z-index:0.8; position:fixed;top:62px; left:0px; width:100%;height:calc(100% - 62px); background-color:blue; opacity:0.8;">
+<div id="maurizio" style="z-index:0.8; position:fixed;top:62px; left:0px; width:100%;height:calc(100% - 62px); background-color:blue; opacity:0.8;">
     <div style="margin:20px; background-color:red; height:calc(100% - 100px); overflow: scroll;">
         <div style="margin:10px 0px;height:1200px; background-color:orange;"></div>
     </div>
     <div style="margin:20px; background-color:green; height: 40px;">
     </div>
 </div>
+
+<script type="text/javascript">
+    $(function(){$("body").append($("#maurizio"))})
+</script>
