dbd6cc5a33437b1e79140d2f2dbaa384e541ee23
davidecioetto <davide.cioetto@alten.it>
2021-04-30 16:20:43 +0200

    Creato oggetto di convergenza embed_pdf (no mobile)

---
+++ docs_convergenza/include/oggetti/Box/commons/embed_pdf/oggetto.php
@@ -0,0 +1,7 @@
+<!-- Soluzione desktop -->
+<div id="idDelContenitoreDelPdf" class="pdf-container loading"></div>
+<script>
+// Inizializzazione del PDF
+var pdfdoc = $("#idDelContenitoreDelPdf").pdf({url:"/template/statici/documento_prova.pdf"});
+</script>
+<!-- Fine soluzione desktop -->
