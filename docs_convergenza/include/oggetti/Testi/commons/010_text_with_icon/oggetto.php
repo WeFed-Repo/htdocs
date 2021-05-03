1bef6b7ffff8878b86859e16d99929dce23b7d9c
SG-Alten <simone.grippa@alten.it>
2021-05-03 16:16:00 +0200

    Convergenza Oggetto Text-With-Icon WB Less Styling

--- docs_convergenza/include/oggetti/Testi/commons/010_text_with_icon/oggetto.php
+++ docs_convergenza/include/oggetti/Testi/commons/010_text_with_icon/oggetto.php
@@ -2,75 +2,73 @@
 <section> 
 
     <!-- Casistica Image -->
-    <div class="text-with-icon ellips">
+    <div class="text-with-icon">
         <img src="/common/fe/img/pdf_icon.png" alt="pdf">
-        <a href="#">Nome_Documento_1_Da_Visionare</a>
+        <a href="#">Lorem ipsum</a>
     </div>
-
-    <hr>
-
-    <!-- Casistica Glifo -->
-    <div class="text-with-icon ellips">
-        <span class="glyph glyph-uread_pdf" title="glyph-uread_pdf"></span>
-        <a href="#">Nome_Documento_2_Da_Firmare</a>
-    </div>
-    <hr> 
 
     <!-- Text with Icon + sub-text -->                    
     <div class="text-with-icon ">
-        <a class="linker" data-toggle="collapse" href="#collapse1" role="button" aria-expanded="false" aria-controls="collapse1">
-            <span class="icon icon-arrow_down" title="icon-arrow_down"></span>
-            <span class="icon icon-arrow_up" title="icon-arrow_up"></span>
-            <span class="text underline">NOME_PRATICA_UNIVOCO_1</span>
-            <span class="sub-text">a682002b-db58-4325-ba7d-51166868a68f</span>
-        </a>
-    </div>
-
-    <hr> 
-
-    <!-- Casistica All link -->
-    <div class="text-with-icon ">
-        <a data-toggle="modal" data-target="#modaleContatti" href="javascript:;" class="no-underline">
-            <span class="icon icon-assic_assistenza_telefono" title="icon-numeroverde_desktop"></span>
-            <span>Gestore</span>
-        </a>
-    </div>
-
-    <hr> 
-
-    <!-- Casistica Tooltip -->
-    <div class="text-with-icon right-icon with-tooltip">
         <a class="linker">
-            <span class="text transform-uppercase">Nuova</span>
-            <i class="icon icon-ico_help_filled_tab" data-toggle="tooltip" title="Potrai iniziare la sottoscrizione della pratica una volta che il cointestatario avrà completato la sua parte."></i>
+            <span class="icon icon-ico_help_filled_tab" title="icon-ico_help_filled_tab"></span>
+            <span class="text underline">Lorem ipsum</span>
+            <span class="sub-text">Lorem ipsum dolor sit</span>
         </a>
-    </div>
-
-    <hr> 
+    </div> 
 
     <!-- Casistica Tooltip con Sub-->
     <div class="text-with-icon right-icon with-tooltip">
         <a class="linker">
-            <span class="text transform-uppercase">Nuova</span>
-            <i class="icon icon-ico_help_filled_tab" data-toggle="tooltip" title="Potrai iniziare la sottoscrizione della pratica una volta che il cointestatario avrà completato la sua parte."></i>
-            <span class="sub-text">In attesa di cointestatario</span>
+            <span class="text transform-uppercase">Lorem ipsum</span>
+            <i class="icon icon-ico_help_filled_tab" data-toggle="tooltip" title="Lorem ipsum dolor sit"></i>
+            <span class="sub-text">Lorem ipsum dolor sit</span>
         </a>
     </div>
+</section>
 
-    <hr>
-
+<section>
     <!-- Casistica Lista -->
     <div class="list-icon-wrapper to-right">
         <div class="text-with-icon">
             <a href="#">
                 <span class="icon icon-trash_filled" title="icon-trash_filled"></span>
-                <span class="no-mobile-label">Rifiuta pratica</span>
+                <span class="no-mobile-label">Lorem ipsum</span>
             </a>
         </div>
         <div class="text-with-icon">
             <a href="#">
                 <span class="icon icon-numeroverde_desktop" title="icon-numeroverde_desktop"></span>
-                <span class="no-mobile-label">Contatti</span>
+                <span class="no-mobile-label">Lorem ipsum</span>
+            </a>
+        </div>
+        <div class="text-with-icon">
+            <a href="#">
+                <span class="icon icon-ico_help_filled_tab" title="icon-ico_help_filled_tab"></span>
+                <span class="no-mobile-label">Lorem ipsum</span>
+            </a>
+        </div>
+    </div>
+</section>
+
+<section>
+    <!-- Casistica Lista -->
+    <div class="list-icon-wrapper to-left">
+        <div class="text-with-icon">
+            <a href="#">
+                <span class="icon icon-trash_filled" title="icon-trash_filled"></span>
+                <span class="no-mobile-label">Lorem ipsum</span>
+            </a>
+        </div>
+        <div class="text-with-icon">
+            <a href="#">
+                <span class="icon icon-numeroverde_desktop" title="icon-numeroverde_desktop"></span>
+                <span class="no-mobile-label">Lorem ipsum</span>
+            </a>
+        </div>
+        <div class="text-with-icon">
+            <a href="#">
+                <span class="icon icon-ico_help_filled_tab" title="icon-ico_help_filled_tab"></span>
+                <span class="no-mobile-label">Lorem ipsum</span>
             </a>
         </div>
     </div>
