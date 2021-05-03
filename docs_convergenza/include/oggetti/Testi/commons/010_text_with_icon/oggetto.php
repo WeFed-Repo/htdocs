5a61ec440ec4ca93a3c9cbf8a3623a8d03124a65
SG-Alten <simone.grippa@alten.it>
2021-05-03 13:14:30 +0200

    Creazione Oggetto text-with-icon

---
+++ docs_convergenza/include/oggetti/Testi/commons/010_text_with_icon/oggetto.php
@@ -0,0 +1,77 @@
+<!-- TIPOLOGIE DI UTILIZZO DI TEXT-WITH-ICON -->
+<section> 
+
+    <!-- Casistica Image -->
+    <div class="text-with-icon ellips">
+        <img src="/common/fe/img/pdf_icon.png" alt="pdf">
+        <a href="#">Nome_Documento_1_Da_Visionare</a>
+    </div>
+
+    <hr>
+
+    <!-- Casistica Glifo -->
+    <div class="text-with-icon ellips">
+        <span class="glyph glyph-uread_pdf" title="glyph-uread_pdf"></span>
+        <a href="#">Nome_Documento_2_Da_Firmare</a>
+    </div>
+    <hr> 
+
+    <!-- Text with Icon + sub-text -->                    
+    <div class="text-with-icon ">
+        <a class="linker" data-toggle="collapse" href="#collapse1" role="button" aria-expanded="false" aria-controls="collapse1">
+            <span class="icon icon-arrow_down" title="icon-arrow_down"></span>
+            <span class="icon icon-arrow_up" title="icon-arrow_up"></span>
+            <span class="text underline">NOME_PRATICA_UNIVOCO_1</span>
+            <span class="sub-text">a682002b-db58-4325-ba7d-51166868a68f</span>
+        </a>
+    </div>
+
+    <hr> 
+
+    <!-- Casistica All link -->
+    <div class="text-with-icon ">
+        <a data-toggle="modal" data-target="#modaleContatti" href="javascript:;" class="no-underline">
+            <span class="icon icon-assic_assistenza_telefono" title="icon-numeroverde_desktop"></span>
+            <span>Gestore</span>
+        </a>
+    </div>
+
+    <hr> 
+
+    <!-- Casistica Tooltip -->
+    <div class="text-with-icon right-icon with-tooltip">
+        <a class="linker">
+            <span class="text transform-uppercase">Nuova</span>
+            <i class="icon icon-ico_help_filled_tab" data-toggle="tooltip" title="Potrai iniziare la sottoscrizione della pratica una volta che il cointestatario avrà completato la sua parte."></i>
+        </a>
+    </div>
+
+    <hr> 
+
+    <!-- Casistica Tooltip con Sub-->
+    <div class="text-with-icon right-icon with-tooltip">
+        <a class="linker">
+            <span class="text transform-uppercase">Nuova</span>
+            <i class="icon icon-ico_help_filled_tab" data-toggle="tooltip" title="Potrai iniziare la sottoscrizione della pratica una volta che il cointestatario avrà completato la sua parte."></i>
+            <span class="sub-text">In attesa di cointestatario</span>
+        </a>
+    </div>
+
+    <hr>
+
+    <!-- Casistica Lista -->
+    <div class="list-icon-wrapper to-right">
+        <div class="text-with-icon">
+            <a href="#">
+                <span class="icon icon-trash_filled" title="icon-trash_filled"></span>
+                <span class="no-mobile-label">Rifiuta pratica</span>
+            </a>
+        </div>
+        <div class="text-with-icon">
+            <a href="#">
+                <span class="icon icon-numeroverde_desktop" title="icon-numeroverde_desktop"></span>
+                <span class="no-mobile-label">Contatti</span>
+            </a>
+        </div>
+    </div>
+</section>
