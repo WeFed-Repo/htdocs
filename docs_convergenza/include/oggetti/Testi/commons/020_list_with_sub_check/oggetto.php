fdd2d0764be15c3f220f55730d1a6485a48664ca
sstrepponi <62430689+sstrepponi@users.noreply.github.com>
2021-05-06 12:29:09 +0200

    scheda prodotto cdb
    scheda prodotto cdb

--- docs_convergenza/include/oggetti/Testi/commons/020_list_with_sub_check/oggetto.php
+++ docs_convergenza/include/oggetti/Testi/commons/020_list_with_sub_check/oggetto.php
@@ -2,9 +2,9 @@
 <ul class="ul-align-left">
     <li><strong>Lorem ipsum</strong>
         <ul class="sub-ul-check">
-            <li><i class="icon icon-checkbox_spunta" title="icon-checkbox_spunta"></i><span>Lorem ipsum dolor sit amet, consectetur adipiscing elit</span></li>
-            <li><i class="icon icon-checkbox_spunta" title="icon-checkbox_spunta"></i><span>Lorem ipsum dolor sit amet</span></li>
-            <li><i class="icon icon-checkbox_spunta" title="icon-checkbox_spunta"></i><span>Lorem ipsum dolor sit amet, consectetur adipiscing elit</span></li>
+            <li><i class="icon icon-check-ok_table" title="icon-checkbox_spunta"></i><span>Lorem ipsum dolor sit amet, consectetur adipiscing elit</span></li>
+            <li><i class="icon icon-check-ok_table" title="icon-checkbox_spunta"></i><span>Lorem ipsum dolor sit amet</span></li>
+            <li><i class="icon icon-check-ok_table" title="icon-checkbox_spunta"></i><span>Lorem ipsum dolor sit amet, consectetur adipiscing elit</span></li>
         </ul>
     </li>
     <li><strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit</strong></li>
