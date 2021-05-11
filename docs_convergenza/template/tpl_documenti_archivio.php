0abc05a9e205920217233cbae179bff5e8f8f84f
davidecioetto <davide.cioetto@alten.it>
2021-05-11 13:06:45 +0200

    Gestione filtri form

--- docs_convergenza/template/tpl_documenti_archivio.php
+++ docs_convergenza/template/tpl_documenti_archivio.php
@@ -43,18 +43,34 @@
 </script>
 
 <style>
+
+/* Classi generiche css per determinare comportamenti di aperture/chiusure */
+.opened-visible {display:none}
+.opened-hidden {display:block}
+.opened .opened-hidden {display:none;}
+.opened .opened-visible {display:block;}
+
 .mobile-switchable-console {display:none;} 
-@media (max-width: 1023px) {
+
+@media (max-width: 767px) {
+    .mobile-switchable {margin-bottom:15px}
+
+    .mobile-switchable-toggler {margin-top:-10px;}
     .mobile-switchable .closer {display:none;}
     .mobile-switchable-console {display:block;}
     .mobile-switchable-form {display:none}
     
     .mobile-switchable.opened .closer {display: inline-block}
     .mobile-switchable.opened .opener {display: none;}
-    .mobile-switchable.opened .mobile-switchable-form {display:block; padding-right:60px;}
+    .mobile-switchable.opened .mobile-switchable-form {display:block; padding-right:45px;}
 }
 
 /* Bottoni con glifo per VR */
+@media (min-width:768px) {
+    .btn.btn-inline {margin-top:7px;}
+    .btn.btn-no-label.btn-inline {margin-top:35px;}  
+}
+
 .btn .icon {font-size:20px; font-weight:normal;line-height: 13px; margin-right:5px}
 .btn span.icon-text {    font-family: "bankFont" !important;
     font-size: 14px;
@@ -68,14 +84,27 @@
 /* Bottone icona grande */
 .btn-icon-big {background: #0e977f; border-radius:50%; width:34px;height:34px; color:#fff; text-align:center; display:inline-block; margin:0;}
 .btn-icon-big i.icon {line-height:32px;}
+@media (max-width:768px) {
+    .col-xs-12 .btnWrapper .btn-align-left:last-child:first-child {float:none}
+    .formWrapper .form-group.btnWrapper {margin: 15px 0}
+    .btnWrapper .btn-align-left {float:none;}
+    .btnWrapper .btn {width:100%;margin:0 15px;}
+}
 </style>
 
 <div class="formWrapper mobile-switchable">
-
+    <h3 class="titleSection titleForm hidden-xs">Cerca pratica</h3>
     <div class="mobile-switchable-console">
         <div class="row">
             <div class="col-xs-8">
-                <h4>Tutte le pratiche</h4>
+                <!-- label dinamica -->
+                <div class="opened-hidden">
+                    <h3 class="titleSection titleForm" id="tutteLePratiche">Tutte le pratiche</h3>
+                    <div class="text-with-icon" id="rimuoviFiltri" style="display:none"><a><i class="icon icon-close_inverted_fill"></i>Rimuovi filtri selezionati</a></div>
+                </div>
+                <h3 class="titleSection titleForm opened-visible">Cerca pratica</h3>
+                
+                <!-- fine label dinamica -->
             </div>
             <div class="col-xs-4">
                 <div class="btn-align-right mobile-switchable-toggler">
@@ -87,10 +116,9 @@
     </div>
     
     <div class="mobile-switchable-form">
-        
         <div class="form-group">
             <div class="row">
-                <div class="form-field-input col-sm-3">
+                <div class="form-field-input col-sm-4 col-xs-12">
                     <label class="control-label">Stato</label>
                     <div class="form-field">
                         <select class="form-control">
@@ -101,7 +129,7 @@
                         </select>
                     </div>
                 </div>
-                <div class="form-field-input col-sm-3">
+                <div class="form-field-input col-sm-4 col-xs-12">
                     <label class="control-label">Categoria</label>
                     <select class="form-control">
                         <option selected>Tutte</option>
@@ -110,7 +138,7 @@
                         <option>Amet consectetur</option>
                     </select>
                 </div>
-                <div class="form-field-input col-sm-3">
+                <div class="form-field-input col-sm-4 col-xs-12">
                     <label class="control-label">Avviata da</label>
                     <select class="form-control">
                         <option selected>Tutti</option>
@@ -122,7 +150,7 @@
             </div>
 
             <div class="row">
-                <div class="form-field-input col-sm-3">
+                <div class="form-field-input col-sm-4 col-xs-12">
                     <label class="control-label">Periodo</label>
                     <select class="form-control" onchange="$('#dateRange').toggle($(this).val()=='scegli')">
                         <option selected>Ultimi 12 mesi</option>
@@ -141,7 +169,9 @@
                     });
                 </script>
                 <div id="dateRange" style="display:none">
-                    <div class="form-field-input col-sm-2">
+                    <div class="col-xs-12 col-sm-6  col-md-4">
+                        <div class="row">
+                        <div class="form-field-input col-sm-6 col-xs-6">
                         <label class="control-label">Dal</label>
                         <div class="form-field">
                             <div class="input-group">
@@ -150,7 +180,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="form-field-input col-sm-2">
+                    <div class="form-field-input col-sm-6 col-xs-6">
                         <label class="control-label">Al</label>
                         <div class="form-field">
                             <div class="input-group">
@@ -159,12 +189,19 @@
                             </div>
                         </div>
                     </div>
+                        </div>
+                    </div>
+                        
                 </div>
                 <!-- Fine blocco selezione date -->
-                <div class="col-sm-1">
-                    <div class="btn-align-left">
-                        <a type="button" href="#" class="btn btn-primary" title="Cerca" id="cerca">Cerca</a>
-                    </div>
+                <div class="col-sm-2 col-xs-12">
+
+                    <div class="form-group btnWrapper ">
+				        <div class="btn-align-left">
+				            <a type="button" class="btn btn-primary btn-inline btn-no-label" title="Cerca" id="cerca">Cerca</a>
+				        </div>
+				    </div>
+
                 </div>
             </div>
 
@@ -345,8 +382,18 @@ var costruisciTabella = function() {
 // Inizializzazione tabella 
 $(function(){
     
-    // Emulazione "cerca"
+    // Emulazione gestione filtri su Mobile
+    $("#rimuoviFiltri").click(function(){
+        $("#tutteLePratiche").show();
+        $(this).hide();
+        costruisciTabella();
+    })
+
+    // Emulazione "cerca" (con comportamenti mobile per gestione filtri)
     $("#cerca").click(function(){
+        $("#tutteLePratiche").hide();
+        $("#rimuoviFiltri").show();
+        $(this).parents(".mobile-switchable").removeClass("opened");
         costruisciTabella();
     });
     costruisciTabella();
@@ -386,5 +433,41 @@ $(function(){
     
 
 </style>
+<!-- Informazioni sullo stato delle pratiche -->
+<div id="infoPratiche" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+	<div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">×</button>
+        <h1 class="modal-title">Informazioni sugli stati</h1>
+      </div>
+      <div class="modal-body">
+        <p>
+            <strong>SOTTOSCRITTA</strong><br>
+            Lorem ipsum lorem ipsum lorem ipsum ipsum lorem ipsum lorem ipsum ipsum lorem ipsum lorem ipsum.<br>
+            <br>
+            <strong>LOREM IPSUM</strong><br>
+            Lorem ipsum lorem ipsum lorem ipsum ipsum lorem ipsum lorem ipsum ipsum lorem ipsum lorem ipsum.<br>
+            <br>
+            <strong>LOREM IPSUM</strong><br>
+            Lorem ipsum lorem ipsum lorem ipsum ipsum lorem ipsum lorem ipsum ipsum lorem ipsum lorem ipsum.<br>
+            <br>
+            <strong>LOREM IPSUM</strong><br>
+            Lorem ipsum lorem ipsum lorem ipsum ipsum lorem ipsum lorem ipsum ipsum lorem ipsum lorem ipsum.<br>
+            
+        </p>
+      </div>
+      <div class="modal-footer">
+       	<div class="align-right">
+			<input type="button" name="chiudi" value="Chiudi" data-dismiss="modal" class="btn  btn-primary" alt="Chiudi">
+		</div>
+		</div>
+    </div>
+	</div>
+</div>
+<section class="visible-xs">
+    <div class="text-with-icon right-icon"><a data-toggle="modal" data-target="#infoPratiche">Informazioni sugli stati delle pratiche<i class="icon icon-ico_help_filled_tab"></i></a></div>
+</section>
+<!-- Fine informazioni sullo stato delle pratiche -->
 <table class="loading" id="tableArchivio"></table>
 <!-- Fine framework tabella -->
