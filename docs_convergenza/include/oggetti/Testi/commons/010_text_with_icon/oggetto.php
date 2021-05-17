f0179bb0d78a6af6fe5dc923978a61c73d8d4e80
davidecioetto <davide.cioetto@alten.it>
2021-05-17 10:50:07 +0200

    Prima revisione .text-with-icon

--- docs_convergenza/include/oggetti/Testi/commons/010_text_with_icon/oggetto.php
+++ docs_convergenza/include/oggetti/Testi/commons/010_text_with_icon/oggetto.php
@@ -1,49 +1,66 @@
 <!-- TIPOLOGIE DI UTILIZZO DI TEXT-WITH-ICON -->
-<div class="list-icon-wrapper not-inline">
-    <div class="text-with-icon"><a href="#"><span class="image pdf"></span>Lorem ipsum</a></div>
-    <div class="text-with-icon"><a href="#"><span class="image pdf"></span>Lorem ipsum</a></div>
-    <div class="text-with-icon"><a href="#"><span class="image pdf"></span>Lorem ipsum</a></div>
-    <div class="text-with-icon"><a href="#"><span class="image pdf"></span>Lorem ipsum</a></div>
-</div>
 <section>
-    <!-- Casistica Lista -->
+<p><b>Esempio di lista</b></p>
+<div class="list-icon-wrapper not-inline">
+    <div class="text-with-icon"><a href="#"><span class="image-icon pdf"></span>Lorem ipsum</a></div>
+    <div class="text-with-icon"><a href="#"><span class="image-icon pdf"></span>Link di prova con testo<br>su piu' righe.</a></div>
+    <div class="text-with-icon"><span class="image-icon pdf"></span>Testo senza link</div>
+    <div class="text-with-icon"><a href="#"><span class="image-icon pdf"></span>Lorem ipsum</a></div>
+    <div class="text-with-icon"><a href="#"><i class="icon icon-trash_filled" title="icon-trash_filled"></i><span class="dark">Link tipo scuro</span></a></div>
+    <div class="text-with-icon"><a href="#"><span class="image-icon pdf v-centered"></span>Link su piu' righe con<br>icona centrata verticalmente</a></div>
+</div>
+</section>
+
+<section>
+    <p><b>Lista allineata a destra con testi che spariscono su mobile (opzionali)</b></p>
+    <!-- Casistica Lista allineata a destra-->
     <div class="list-icon-wrapper to-right">
         <div class="text-with-icon">
             <a href="#">
-                <span class="icon icon-trash_filled" title="icon-trash_filled"></span>
-                <span class="no-mobile-label">Lorem ipsum</span>
+                <i class="icon icon-trash_filled" title="icon-trash_filled"></i>
+                <span class="hidden-xs">Lorem ipsum</span>
             </a>
         </div>
         <div class="text-with-icon">
             <a href="#">
-                <span class="icon icon-numeroverde_desktop" title="icon-numeroverde_desktop"></span>
-                <span class="no-mobile-label">Lorem ipsum</span>
+                <i class="icon icon-numeroverde_desktop" title="icon-numeroverde_desktop"></i>
+                <span class="hidden-xs">Lorem ipsum testo<br>su piu' righe</span>
             </a>
         </div>
-        <div class="text-with-icon"><a href="#"><span class="image pdf"></span>Lorem ipsum</a></div>
+        <div class="text-with-icon"><a href="#"><span class="image-icon pdf"></span><span class="hidden-xs">Lorem ipsum</span></a></div>
     </div>
 </section>
 
 <section>
+    <p><b>Lista allineata a sinistra</b></p>
     <!-- Casistica Lista -->
-    <div class="list-icon-wrapper to-left">
+    <div class="list-icon-wrapper">
         <div class="text-with-icon">
             <a href="#">
-                <span class="icon icon-trash_filled" title="icon-trash_filled"></span>
-                <span class="no-mobile-label">Lorem ipsum</span>
+                <i class="icon icon-trash_filled" title="icon-trash_filled"></i>
+                <span class="hidden-xs">Lorem ipsum</span>
             </a>
         </div>
         <div class="text-with-icon">
             <a href="#">
-                <span class="icon icon-numeroverde_desktop" title="icon-numeroverde_desktop"></span>
-                <span class="no-mobile-label">Lorem ipsum</span>
+                <i class="icon icon-numeroverde_desktop" title="icon-numeroverde_desktop"></i>
+                <span class="hidden-xs">Lorem ipsum</span>
             </a>
         </div>
         <div class="text-with-icon">
             <a href="#">
-                <span class="icon icon-ico_help_filled_tab" title="icon-ico_help_filled_tab"></span>
-                <span class="no-mobile-label">Lorem ipsum</span>
+                <i class="icon icon-ico_help_filled_tab" title="icon-ico_help_filled_tab"></i>
+                <span class="hidden-xs">Lorem ipsum</span>
             </a>
         </div>
     </div>
+</section>
+
+<section>
+    <p><b>Testo con icona a destra</b></p>
+    <div  class="list-icon-wrapper">
+        <div class="text-with-icon right-icon"><a href="#"><span class="image-icon pdf"></span>Lorem ipsum</a></div>
+        <div class="text-with-icon right-icon"><a href="#"><i class="icon icon-ico_help_filled_tab"></i>Testo su piu' righe<br>su piu' righe lorem dolor ipsum</a></div>
+        <div class="text-with-icon right-icon"><a href="#"><span class="image-icon pdf v-centered"></span>Testo su piu' righe<br>con icona centrata<br>verticalmente</a></div>
+    <div>
 </section>
