dfd9518762340ee93a36a1bc15524f43eb7a4961
sstrepponi <62430689+sstrepponi@users.noreply.github.com>
2021-05-19 15:17:38 +0200

    allineamento di stili link stanza di firma + tpl pannello controllo
    allineamento di stili link stanza di firma + tpl pannello controllo

---
+++ docs_convergenza/template/tpl_responsive_pannello_controllo_cdb.php
@@ -0,0 +1,155 @@
+<!---INIZIO CONTENUTI PER IL CONTENT --->
+
+<!--area utilizzo carta -->
+<section>
+    <h3 class="titleSection titleForm">Utilizzo carta</h3>
+	<div class="border-output">
+        <div class="row first-row">
+            <div class="col-sm-6 col-xs-12 boxDefault">
+                <div class="formWrapper">
+					<div class="row">
+						<div class="col-xs-8">
+							<label class="control-label">Residuo complessivo mensile</label>
+						</div>
+						<div class="col-xs-4">
+							<strong class="flRight">XXX,XX &euro;</strong>
+						</div>
+					</div>
+                    <div class="row">
+						<div class="col-xs-8">
+							<label class="control-label">Residuo giornaliero prelievi</label>
+						</div>
+						<div class="col-xs-4">
+                            <strong class="flRight">XXX,XX &euro;</strong>
+						</div>
+					</div>
+                    <div class="row">
+						<div class="col-xs-8">
+							<label class="control-label">Residuo giornaliero pagamenti</label>
+						</div>
+						<div class="col-xs-4">
+                            <strong class="flRight">XXX,XX &euro;</strong>
+						</div>
+					</div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-xs-12 boxDefault">
+                <div class="formWrapper">
+					<div class="row">
+						<div class="col-xs-8">
+							<label class="control-label">Residuo complessivo mensile</label>
+						</div>
+						<div class="col-xs-4">
+                            <strong class="flRight">XXX,XX &euro;</strong>
+						</div>
+					</div>
+                    <div class="row">
+						<div class="col-xs-8">
+							<label class="control-label">Residuo giornaliero prelievi</label>
+						</div>
+						<div class="col-xs-4">
+                            <strong class="flRight">XXX,XX &euro;</strong>
+						</div>
+					</div>
+                    <div class="row">
+						<div class="col-xs-8">
+							<label class="control-label">Residuo giornaliero pagamenti</label>
+						</div>
+						<div class="col-xs-4">
+                            <strong class="flRight">XXX,XX &euro;</strong>
+						</div>
+					</div>
+                </div>
+            </div>
+        </div> 
+</div>
+</section>
+<!--fine area utilizzo carta -->
+<hr>
+<!--limiti di  utilizzo carta -->
+<section>
+    <h3 class="titleSection titleForm">Limiti di utilizzo</h3>
+    <div class="border-output">
+        <div class="row first-row">
+            <div class="col-sm-12 boxDefault">
+                <div class="row">
+                    <div class="col-sm-6 col-xs-12">
+                        <div class="formWrapper">
+                            <div class="row">
+                                <div class="col-xs-8">
+                                    <label class="control-label">Limite complessivo mensile</label>
+                                </div>
+                                <div class="col-xs-4">
+                                    <strong class="flRight">XXX,XX &euro;</strong>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-xs-8">
+                                    <label class="control-label">Limite giornaliero prelievi</label>
+                                </div>
+                                <div class="col-xs-4">
+                                    <strong class="flRight">XXX,XX &euro;</strong>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-xs-8">
+                                    <label class="control-label">Limite giornaliero pagamenti</label>
+                                </div>
+                                <div class="col-xs-4">
+                                    <strong class="flRight">XXX,XX &euro;</strong>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--fine limiti di utilizzo carta -->
+
+<!--testo modifica-->
+<section>
+<p class="noPadding">Per richiedere la <strong>modifica dei massimali</strong> <a href="#" class="btnLink">PRENDI APPUNTAMENTO</a> in Filiale.<br>
+(Verifica con il titolare del conto di appoggio di essere abilitato alla modifica dei massimali)
+</p>
+</section>
+<!--fine testo modifica-->
+<hr>
+<!--box elenco-->
+<div class="box-feature">
+    <h3 class="titleSection titleForm">Spending control</h3>
+    <p class="paddingBottomSmall">Con la funzionalit&agrave; <strong>Spending control</strong> puoi limitare le soglie di spesa e decidere dove e come utilizzare la tua carta.
+    Puoi scegliere infatti le categorie merceologiche autorizzate, i canali di utilizzo della carta ( ATM, pos, web ), la periodicit&agrave; di utilizzo e le aree geografiche ammesse. Puoi inoltre effettuare il blocco/sblocco totale carta, manuale o per periodi predefiniti</p>
+    <p class="noPadding">Per gestire il servizio <a href="#" class="btnLink">VAI AL PORTALE NEXI</a></p>
+</div>
+<hr>
+<div class="box-feature">
+    <h3 class="titleSection titleForm">3D Secure</h3>
+    <p class="paddingBottomSmall">3D Secure &egrave; il servizio che ti permette di eseguire operazioni di pagamento online in sicurezza, grazie all’impiego di specifici fattori di autenticazione.</p>
+    <p class="noPadding">Per gestire il servizio <a href="#" class="btnLink">VAI AL PORTALE NEXI</a></p>
+</div>
+<hr>
+<div class="box-feature">
+    <h3 class="titleSection titleForm">Alert SMS</h3>
+    <p class="paddingBottomSmall">Alert SMS &egrave; il servizio che ti permette di ricevere un messaggio per le operazioni più rilevanti eseguite con la carta.</p>
+    <p class="noPadding">Per gestire il servizio <a href="#" class="btnLink">VAI AL PORTALE NEXI</a></p>
+</div>
+<hr>
+<div class="box-feature">
+    <h3 class="titleSection titleForm">Notifiche app</h3>
+    <p class="paddingBottomSmall">Push app notification è il servizio che ti permette di tenere tutte le spese sotto controllo tramite il tuo dispositivo</p>
+    <p class="noPadding">Per gestire il servizio utilizza l'app NEXI Pay.</p>
+</div>
+<hr>
+<div class="box-feature">
+    <h3 class="titleSection titleForm">Pin carta</h3>
+    <p class="noPadding">Non ricordi il pin associato alla carta o vuoi modificarlo? <a href="#" class="btnLink">VAI AL PORTALE NEXI</a></p>
+</div>
+<hr>
+<div class="box-feature">
+    <h3 class="titleSection titleForm">Blocco carta</h3>
+    <p>In caso di furto o smarrimento della carta contatta il servizio Nexi (il servizio è attivo h24 /365)</p>
+</div>
+<!--fine box elenco-->
+<!---FINE CONTENUTI PER IL CONTENT --->
