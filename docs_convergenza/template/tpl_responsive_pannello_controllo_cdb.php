65f56032acc04feb0a53542f7d207af78ffded24
sstrepponi <62430689+sstrepponi@users.noreply.github.com>
2021-05-19 16:06:43 +0200

    tpl pannello controllo cdb
    tpl pannello controllo cdb

--- docs_convergenza/template/tpl_responsive_pannello_controllo_cdb.php
+++ docs_convergenza/template/tpl_responsive_pannello_controllo_cdb.php
@@ -1,7 +1,8 @@
 <!---INIZIO CONTENUTI PER IL CONTENT --->
 
-<!--area utilizzo carta -->
+<!-- prima section-->
 <section>
+    <!--area utilizzo carta -->
     <h3 class="titleSection titleForm">Utilizzo carta</h3>
 	<div class="border-output">
         <div class="row first-row">
@@ -37,7 +38,7 @@
                 <div class="formWrapper">
 					<div class="row">
 						<div class="col-xs-8">
-							<label class="control-label">Residuo complessivo mensile</label>
+							<label class="control-label">Utilizzato complessivo mensile</label>
 						</div>
 						<div class="col-xs-4">
                             <strong class="flRight">XXX,XX &euro;</strong>
@@ -45,7 +46,7 @@
 					</div>
                     <div class="row">
 						<div class="col-xs-8">
-							<label class="control-label">Residuo giornaliero prelievi</label>
+							<label class="control-label">Utilizzato giornaliero prelievi</label>
 						</div>
 						<div class="col-xs-4">
                             <strong class="flRight">XXX,XX &euro;</strong>
@@ -53,7 +54,7 @@
 					</div>
                     <div class="row">
 						<div class="col-xs-8">
-							<label class="control-label">Residuo giornaliero pagamenti</label>
+							<label class="control-label">Utilizzato giornaliero pagamenti</label>
 						</div>
 						<div class="col-xs-4">
                             <strong class="flRight">XXX,XX &euro;</strong>
@@ -62,12 +63,10 @@
                 </div>
             </div>
         </div> 
-</div>
-</section>
-<!--fine area utilizzo carta -->
-<hr>
-<!--limiti di  utilizzo carta -->
-<section>
+    </div>
+    <!--fine area utilizzo carta -->
+    <hr class="hidden-xs">
+    <!--limiti di  utilizzo carta -->
     <h3 class="titleSection titleForm">Limiti di utilizzo</h3>
     <div class="border-output">
         <div class="row first-row">
@@ -105,51 +104,92 @@
             </div>
         </div>
     </div>
+    <!--fine limiti di utilizzo carta -->
+    <!--testo modifica-->
+    <p class="noPadding">Per richiedere la <strong>modifica dei massimali</strong> <a href="#" class="btnLink">PRENDI APPUNTAMENTO</a> in Filiale.<br>
+    (Verifica con il titolare del conto di appoggio di essere abilitato alla modifica dei massimali)
+    </p>
+    <!--fine testo modifica-->
 </section>
-<!--fine limiti di utilizzo carta -->
-
-<!--testo modifica-->
+<!-- fine prima section-->
+<hr>
+<!-- seconda section-->
 <section>
-<p class="noPadding">Per richiedere la <strong>modifica dei massimali</strong> <a href="#" class="btnLink">PRENDI APPUNTAMENTO</a> in Filiale.<br>
-(Verifica con il titolare del conto di appoggio di essere abilitato alla modifica dei massimali)
-</p>
+    <!--box elenco-->
+    <div class="box-feature">
+        <h3 class="titleSection titleForm">Spending control</h3>
+        <p class="paddingBottomSmall">Con la funzionalit&agrave; <strong>Spending control</strong> puoi limitare le soglie di spesa e decidere dove e come utilizzare la tua carta.
+        Puoi scegliere infatti le categorie merceologiche autorizzate, i canali di utilizzo della carta ( ATM, pos, web ), la periodicit&agrave; di utilizzo e le aree geografiche ammesse. Puoi inoltre effettuare il blocco/sblocco totale carta, manuale o per periodi predefiniti</p>
+        <p class="noPadding">Per gestire il servizio <a href="#" class="btnLink">VAI AL PORTALE NEXI</a></p>
+    </div>
+    <hr>
+    <div class="box-feature">
+        <h3 class="titleSection titleForm">3D Secure</h3>
+        <p class="paddingBottomSmall">3D Secure &egrave; il servizio che ti permette di eseguire operazioni di pagamento online in sicurezza, grazie all’impiego di specifici fattori di autenticazione.</p>
+        <p class="noPadding">Per gestire il servizio <a href="#" class="btnLink">VAI AL PORTALE NEXI</a></p>
+    </div>
+    <hr>
+    <div class="box-feature">
+        <h3 class="titleSection titleForm">Alert SMS</h3>
+        <p class="paddingBottomSmall">Alert SMS &egrave; il servizio che ti permette di ricevere un messaggio per le operazioni più rilevanti eseguite con la carta.</p>
+        <p class="noPadding">Per gestire il servizio <a href="#" class="btnLink">VAI AL PORTALE NEXI</a></p>
+    </div>
+    <hr>
+    <div class="box-feature">
+        <h3 class="titleSection titleForm">Notifiche app</h3>
+        <p class="paddingBottomSmall">Push app notification è il servizio che ti permette di tenere tutte le spese sotto controllo tramite il tuo dispositivo</p>
+        <p class="noPadding">Per gestire il servizio utilizza l'app NEXI Pay.</p>
+    </div>
+    <hr>
+    <div class="box-feature">
+        <h3 class="titleSection titleForm">Pin carta</h3>
+        <p class="noPadding">Non ricordi il pin associato alla carta o vuoi modificarlo? <a href="#" class="btnLink">VAI AL PORTALE NEXI</a></p>
+    </div>
+    <hr>
+    <div class="box-feature">
+        <h3 class="titleSection titleForm">Blocco carta</h3>
+        <p>In caso di furto o smarrimento della carta contatta il servizio Nexi (il servizio è attivo h24 /365)</p>
+        <div class="border-output">
+            <div class="row first-row">
+                <div class="col-sm-6 col-xs-12 boxDefault noMargin">
+                    <div class="formWrapper">
+                        <div class="row">
+                            <div class="col-xs-12">
+                                <strong><a class="no-underline" href="tel:800151616">800.15.16.16</a></strong> <span>Per chi chiama dall'Italia</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-xs-12 boxDefault noMargin">
+                    <div class="formWrapper">
+                        <div class="row">
+                            <div class="col-xs-12">
+                                <strong><a class="no-underline" href="tel:+390234980020">+39 02 34.980.020 *</a></strong> <span>Per chi chiama dall'estero</span>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-xs-12">
+                                <strong><a class="no-underline" href="tel:+18004736896">+1 800 473.6896 **</a> </strong> <span>Per chi chiama dagli USA</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+        <br>
+        <p class="noPadding">* &Egrave; possibile effettuare chiamate a carico di Nexi.
+            <br>
+            ** Numero verde accessibile soltanto da una linea telefonica USA, fissa o mobile.
+        <p>
+    </div>
+    <hr>
+    <!--fine box elenco-->
 </section>
-<!--fine testo modifica-->
-<hr>
-<!--box elenco-->
-<div class="box-feature">
-    <h3 class="titleSection titleForm">Spending control</h3>
-    <p class="paddingBottomSmall">Con la funzionalit&agrave; <strong>Spending control</strong> puoi limitare le soglie di spesa e decidere dove e come utilizzare la tua carta.
-    Puoi scegliere infatti le categorie merceologiche autorizzate, i canali di utilizzo della carta ( ATM, pos, web ), la periodicit&agrave; di utilizzo e le aree geografiche ammesse. Puoi inoltre effettuare il blocco/sblocco totale carta, manuale o per periodi predefiniti</p>
-    <p class="noPadding">Per gestire il servizio <a href="#" class="btnLink">VAI AL PORTALE NEXI</a></p>
+<!--fine  seconda section-->
+<!-- bottone-->
+<div class="btn-align-right">
+    <a title="Movimenti" href="#" type="button" class="btn btn-primary">Movimenti</a> <br class="clear">
 </div>
-<hr>
-<div class="box-feature">
-    <h3 class="titleSection titleForm">3D Secure</h3>
-    <p class="paddingBottomSmall">3D Secure &egrave; il servizio che ti permette di eseguire operazioni di pagamento online in sicurezza, grazie all’impiego di specifici fattori di autenticazione.</p>
-    <p class="noPadding">Per gestire il servizio <a href="#" class="btnLink">VAI AL PORTALE NEXI</a></p>
-</div>
-<hr>
-<div class="box-feature">
-    <h3 class="titleSection titleForm">Alert SMS</h3>
-    <p class="paddingBottomSmall">Alert SMS &egrave; il servizio che ti permette di ricevere un messaggio per le operazioni più rilevanti eseguite con la carta.</p>
-    <p class="noPadding">Per gestire il servizio <a href="#" class="btnLink">VAI AL PORTALE NEXI</a></p>
-</div>
-<hr>
-<div class="box-feature">
-    <h3 class="titleSection titleForm">Notifiche app</h3>
-    <p class="paddingBottomSmall">Push app notification è il servizio che ti permette di tenere tutte le spese sotto controllo tramite il tuo dispositivo</p>
-    <p class="noPadding">Per gestire il servizio utilizza l'app NEXI Pay.</p>
-</div>
-<hr>
-<div class="box-feature">
-    <h3 class="titleSection titleForm">Pin carta</h3>
-    <p class="noPadding">Non ricordi il pin associato alla carta o vuoi modificarlo? <a href="#" class="btnLink">VAI AL PORTALE NEXI</a></p>
-</div>
-<hr>
-<div class="box-feature">
-    <h3 class="titleSection titleForm">Blocco carta</h3>
-    <p>In caso di furto o smarrimento della carta contatta il servizio Nexi (il servizio è attivo h24 /365)</p>
-</div>
-<!--fine box elenco-->
+<!-- fine bottone-->
 <!---FINE CONTENUTI PER IL CONTENT --->
