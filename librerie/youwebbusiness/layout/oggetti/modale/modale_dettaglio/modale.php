f97b7a7d930c699b5fcce726b65c08920fb5772d
SG-Alten <simone.grippa@alten.it>
2021-06-03 11:30:07 +0200

    YouBusiness El.21

---
+++ librerie/youwebbusiness/layout/oggetti/modale/modale_dettaglio/modale.php
@@ -0,0 +1,119 @@
+<!-- Modal -->
+<div class="modal fade" data-modal="simpledetail">
+  <div class="modal-dialog modal-dialog-centered modal-xl" role="document">
+    <div class="modal-content">
+      <div class="modal-header d-flex justify-content-between pb-0">
+        <div class="d-flex justify-content-between w-100">
+          <div>
+            <h5 class="modal-title mt-2">
+              Dettaglio movimento
+            </h5>
+          </div>
+          <div>
+            <p class="text-right mt-3">
+              Creato il : 20/02/2021 da Mario Rosside
+            </p>
+          </div>
+        </div>        
+        <div class="modal-toolbar">
+          <a href="#" aria-label="Print" >
+            <img src="./fe/img/icon/print.svg">
+          </a>
+          <a href="#" data-dismiss="modal" aria-label="Close" >
+            <img src="./fe/img/icon/close.svg">
+          </a>
+        </div>
+      </div>
+      <div class="modal-body">
+        <div class="row pb-3">
+          <div class="col-12 col-md-12">
+            <div class="detail-panel p-3 md-0">
+              <h2>Rapporto</h2>
+
+              <div class="row">
+                <div class="col-6 col-md-6">
+                  <div class="list">
+                    <dt class="list-key">Ragione sociale</dt>
+                    <dd class="list-value">Automobile 24</dd>
+                  </div>
+                  <div class="list">
+                    <dt class="list-key">Rapporto</dt>
+                    <dd class="list-value">Conto cassa</dd>
+                  </div>
+                </div>
+                <div class="col-6 col-md-6">
+                  <div class="list">
+                    <dt class="list-key">ABI - Banca</dt>
+                    <dd class="list-value">0534 - Banco BPM SPA</dd>
+                  </div>
+                  <div class="list">
+                    <dt class="list-key">IBAN</dt>
+                    <dd class="list-value">-</dd>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12 col-md-12">
+            <div class="detail-panel p-3 md-0">
+              <h2>Movimento</h2>
+              <div class="row">
+                <div class="col-6 col-md-6">
+                  <div class="list">
+                    <dt class="list-key">Data contabile</dt>
+                    <dd class="list-value">18/02/2021</dd>
+                  </div>
+                  <div class="list">
+                    <dt class="list-key">Importo</dt>
+                    <dd class="list-value">-102,86</dd>
+                  </div>
+                  <div class="list">
+                    <dt class="list-key">Divisa</dt>
+                    <dd class="list-value">EUR</dd>
+                  </div>
+                  <div class="list">
+                    <dt class="list-key">Causale</dt>
+                    <dd class="list-value">110</dd>
+                  </div>
+                </div>
+                <div class="col-6 col-md-6">
+                  <div class="list">
+                    <dt class="list-key">Descrizione</dt>
+                    <dd class="list-value">pag. utenze varie</dd>
+                  </div>
+                  <div class="list">
+                    <dt class="list-key">Categoria/Sottocategoria</dt>
+                    <dd class="list-value">Risorse Umane/Bonus</dd>
+                  </div>
+                  <div class="list">
+                    <dt class="list-key">Hashtag</dt>
+                    <dd class="list-value">-</dd>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Variante 1 -->
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary">associa fattura</button>
+        <button type="button" class="btn btn-primary">rendi ricorrente</button>
+      </div>
+      <!-- Variante 2 -->
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary" disabled>associa fattura</button>
+        <button type="button" class="btn btn-primary">rendi ricorrente</button>
+      </div>
+      <!-- Alert -->
+      <div class="m-3">
+        <div class="alert alert-danger">
+          Ci dispiace, al momento non sei abilitato alla funzione di Associa Fattura. Rivolgiti al master per attivare le abilitazioni sul tuo profilo.
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
---
+++ librerie/youwebbusiness/layout/oggetti/modale/modale_dettaglio/oggetto.php
@@ -0,0 +1,5 @@
+<button type="button" class="btn btn-primary" data-modal-open="simpledetail">
+  Launch demo modal
+</button>
+
+<?php virtual('/youwebbusiness/layout/oggetti/modale/modale_dettaglio/modale.php');?>
