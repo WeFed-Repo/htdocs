bf759dcd985cf87b250d1fb6ad7333f8768a3b65
SG-Alten <simone.grippa@alten.it>
2021-06-04 10:53:03 +0200

    Update Modal and Accordion

--- librerie/youwebbusiness/layout/oggetti/accordion/accordion_multi_value_head/accordion.php
+++ librerie/youwebbusiness/layout/oggetti/accordion/accordion_multi_value_head/accordion.php
@@ -9,20 +9,35 @@
       </p>
     </div>
     <div class="d-flex justify-content-between align-items-top">
-      <p class="pr-4 pl-4 mb-0"><strong>-102,86</strong></p>
+      <p class="pr-4 pl-4 mb-0 full-contain"><strong>-102,86</strong></p>
       <span class="card-header-icon mt-0 mb-0"></span>
     </div>
   </div>
   <div class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+    <hr class="up-body-separator">
     <div class="card-body">
-      Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
-      richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor
-      brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor,
-      sunt aliqua put a bird on it squid single-origin coffee nulla assumenda
-      shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson
-      cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo.
-      Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth
-      nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      <div class="row">
+        <div class="col-6 col-md-6">
+          <div class="list">
+            <dt class="list-key">Lorem Ipsum</dt>
+            <dd class="list-value">Lorem Ipsum</dd>
+          </div>
+          <div class="list">
+            <dt class="list-key">Lorem Ipsum</dt>
+            <dd class="list-value">Lorem Ipsum</dd>
+          </div>
+        </div>
+        <div class="col-6 col-md-6">
+          <div class="list">
+            <dt class="list-key">Lorem Ipsum</dt>
+            <dd class="list-value">Lorem Ipsum</dd>
+          </div>
+          <div class="list">
+            <dt class="list-key">Lorem Ipsum</dt>
+            <dd class="list-value">Lorem Ipsum</dd>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </div>
@@ -38,11 +53,12 @@
       </p>
     </div>
     <div class="d-flex justify-content-between align-items-top">
-      <p class="pr-4 pl-4 mb-0"><strong>-102,86</strong></p>
+      <p class="pr-4 pl-4 mb-0 full-contain"><strong>-102,86</strong></p>
       <span class="card-header-icon mt-0 mb-0"></span>
     </div>
   </div>
   <div class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+    <hr class="up-body-separator">
     <div class="card-body">
       Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
       richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor
@@ -64,11 +80,12 @@
       </h5>
     </div>
     <div class="d-flex justify-content-between align-items-top">
-      <p class="pr-4 pl-4 mb-0"><strong>-102,86</strong></p>
+      <p class="pr-4 pl-4 mb-0 full-contain"><strong>-102,86</strong></p>
       <span class="card-header-icon mt-0 mb-0"></span>
     </div>
   </div>
   <div class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+    <hr class="up-body-separator">
     <div class="card-body">
       Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
       richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor
--- librerie/youwebbusiness/layout/oggetti/modale/modale_dettaglio/modale.php
+++ librerie/youwebbusiness/layout/oggetti/modale/modale_dettaglio/modale.php
@@ -103,27 +103,69 @@
             <div class="card-header collapsed d-flex justify-content-between align-items-top" data-toggle="collapse">
               <div class="row d-flex justify-content-between align-items-center w-100">
                 <h5 class="col-sm-8 card-header-title mb-0">
-                  Collapsible Group Item #1
+                  Fattura associata
                 </h5>
                 <p class="col-sm-4 pr-4 text-sm-left text-md-right mb-0">
                   1629420831
                 </p>
               </div>
               <div class="d-flex justify-content-between align-items-top">
-                <p class="pr-4 pl-4 mb-0"><strong>-102,86</strong></p>
+                <p class="pr-4 pl-4 mb-0 full-contain"><strong>1.280,86 €</strong></p>
                 <span class="card-header-icon mt-0 mb-0"></span>
               </div>
             </div>
+            
             <div class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+              <hr class="up-body-separator">
               <div class="card-body">
-                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
-                richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor
-                brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor,
-                sunt aliqua put a bird on it squid single-origin coffee nulla assumenda
-                shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson
-                cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo.
-                Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth
-                nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                
+                <div class="row">
+                  <div class="col-6 col-md-6">
+                    <div class="list">
+                      <dt class="list-key">Lorem Ipsum</dt>
+                      <dd class="list-value">Lorem Ipsum</dd>
+                    </div>
+                    <div class="list">
+                      <dt class="list-key">Lorem Ipsum</dt>
+                      <dd class="list-value">Lorem Ipsum</dd>
+                    </div>
+                    <div class="list">
+                      <dt class="list-key">Lorem Ipsum</dt>
+                      <dd class="list-value">Lorem Ipsum</dd>
+                    </div>
+                    <div class="list">
+                      <dt class="list-key">Lorem Ipsum</dt>
+                      <dd class="list-value">Lorem Ipsum</dd>
+                    </div>
+                    <div class="list">
+                      <dt class="list-key">Lorem Ipsum</dt>
+                      <dd class="list-value">Lorem Ipsum</dd>
+                    </div>
+                    <div class="list">
+                      <dt class="list-key">Lorem Ipsum</dt>
+                      <dd class="list-value">Lorem Ipsum</dd>
+                    </div>
+                  </div>
+                  <div class="col-6 col-md-6">
+                    <div class="list">
+                      <dt class="list-key">Lorem Ipsum</dt>
+                      <dd class="list-value">Lorem Ipsum</dd>
+                    </div>
+                    <div class="list">
+                      <dt class="list-key">Lorem Ipsum</dt>
+                      <dd class="list-value">Lorem Ipsum</dd>
+                    </div>
+                    <div class="list">
+                      <dt class="list-key">Lorem Ipsum</dt>
+                      <dd class="list-value">Lorem Ipsum</dd>
+                    </div>
+                    <div class="list">
+                      <dt class="list-key">Lorem Ipsum</dt>
+                      <dd class="list-value">Lorem Ipsum</dd>
+                    </div>
+                  </div>
+                </div>
+
               </div>
             </div>
           </div>
@@ -140,27 +182,65 @@
             <div class="card-header collapsed d-flex justify-content-between align-items-top" data-toggle="collapse">
               <div class="row d-flex justify-content-between align-items-center w-100">
                 <h5 class="col-sm-8 card-header-title mb-0">
-                  Collapsible Group Item #1
+                  Movimento 1
                 </h5>
-                <p class="col-sm-4 pr-4 text-sm-left text-md-right mb-0">
-                  1629420831
-                </p>
               </div>
               <div class="d-flex justify-content-between align-items-top">
-                <p class="pr-4 pl-4 mb-0"><strong>-102,86</strong></p>
+                <p class="pr-4 pl-4 mb-0 full-contain"><strong>-50,00 €</strong></p>
                 <span class="card-header-icon mt-0 mb-0"></span>
               </div>
             </div>
             <div class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+            <hr class="up-body-separator">
               <div class="card-body">
-                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
-                richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor
-                brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor,
-                sunt aliqua put a bird on it squid single-origin coffee nulla assumenda
-                shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson
-                cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo.
-                Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth
-                nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                
+                <div class="row">
+                  <div class="col-6 col-md-6">
+                    <div class="list">
+                      <dt class="list-key">Lorem Ipsum</dt>
+                      <dd class="list-value">Lorem Ipsum</dd>
+                    </div>
+                    <div class="list">
+                      <dt class="list-key">Lorem Ipsum</dt>
+                      <dd class="list-value">Lorem Ipsum</dd>
+                    </div>
+                    <div class="list">
+                      <dt class="list-key">Lorem Ipsum</dt>
+                      <dd class="list-value">Lorem Ipsum</dd>
+                    </div>
+                    <div class="list">
+                      <dt class="list-key">Lorem Ipsum</dt>
+                      <dd class="list-value">Lorem Ipsum</dd>
+                    </div>
+                    <div class="list">
+                      <dt class="list-key">Lorem Ipsum</dt>
+                      <dd class="list-value">Lorem Ipsum</dd>
+                    </div>
+                    <div class="list">
+                      <dt class="list-key">Lorem Ipsum</dt>
+                      <dd class="list-value">Lorem Ipsum</dd>
+                    </div>
+                  </div>
+                  <div class="col-6 col-md-6">
+                    <div class="list">
+                      <dt class="list-key">Lorem Ipsum</dt>
+                      <dd class="list-value">Lorem Ipsum</dd>
+                    </div>
+                    <div class="list">
+                      <dt class="list-key">Lorem Ipsum</dt>
+                      <dd class="list-value">Lorem Ipsum</dd>
+                    </div>
+                    <div class="list">
+                      <dt class="list-key">Lorem Ipsum</dt>
+                      <dd class="list-value">Lorem Ipsum</dd>
+                    </div>
+                    <div class="list">
+                      <dt class="list-key">Lorem Ipsum</dt>
+                      <dd class="list-value">Lorem Ipsum</dd>
+                    </div>
+                  </div>
+                </div>
+
               </div>
             </div>
           </div>
@@ -169,27 +249,65 @@
             <div class="card-header collapsed d-flex justify-content-between align-items-top" data-toggle="collapse">
               <div class="row d-flex justify-content-between align-items-center w-100">
                 <h5 class="col-sm-8 card-header-title mb-0">
-                  Collapsible Group Item #2
-                </h5>
-                <p class="col-sm-4 pr-4 text-sm-left text-md-right mb-0">
-                  1629420831
-                </p>
+                  Movimento 2
+                </h5>                
               </div>
               <div class="d-flex justify-content-between align-items-top">
-                <p class="pr-4 pl-4 mb-0"><strong>-102,86</strong></p>
+                <p class="pr-4 pl-4 mb-0 full-contain"><strong>-52,86 €</strong></p>
                 <span class="card-header-icon mt-0 mb-0"></span>
               </div>
             </div>
             <div class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+            <hr class="up-body-separator">
               <div class="card-body">
-                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
-                richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor
-                brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor,
-                sunt aliqua put a bird on it squid single-origin coffee nulla assumenda
-                shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson
-                cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo.
-                Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth
-                nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                
+                <div class="row">
+                  <div class="col-6 col-md-6">
+                    <div class="list">
+                      <dt class="list-key">Lorem Ipsum</dt>
+                      <dd class="list-value">Lorem Ipsum</dd>
+                    </div>
+                    <div class="list">
+                      <dt class="list-key">Lorem Ipsum</dt>
+                      <dd class="list-value">Lorem Ipsum</dd>
+                    </div>
+                    <div class="list">
+                      <dt class="list-key">Lorem Ipsum</dt>
+                      <dd class="list-value">Lorem Ipsum</dd>
+                    </div>
+                    <div class="list">
+                      <dt class="list-key">Lorem Ipsum</dt>
+                      <dd class="list-value">Lorem Ipsum</dd>
+                    </div>
+                    <div class="list">
+                      <dt class="list-key">Lorem Ipsum</dt>
+                      <dd class="list-value">Lorem Ipsum</dd>
+                    </div>
+                    <div class="list">
+                      <dt class="list-key">Lorem Ipsum</dt>
+                      <dd class="list-value">Lorem Ipsum</dd>
+                    </div>
+                  </div>
+                  <div class="col-6 col-md-6">
+                    <div class="list">
+                      <dt class="list-key">Lorem Ipsum</dt>
+                      <dd class="list-value">Lorem Ipsum</dd>
+                    </div>
+                    <div class="list">
+                      <dt class="list-key">Lorem Ipsum</dt>
+                      <dd class="list-value">Lorem Ipsum</dd>
+                    </div>
+                    <div class="list">
+                      <dt class="list-key">Lorem Ipsum</dt>
+                      <dd class="list-value">Lorem Ipsum</dd>
+                    </div>
+                    <div class="list">
+                      <dt class="list-key">Lorem Ipsum</dt>
+                      <dd class="list-value">Lorem Ipsum</dd>
+                    </div>
+                  </div>
+                </div>
+
               </div>
             </div>
           </div>
