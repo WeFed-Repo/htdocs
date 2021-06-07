47426193ba982a01fee1ca712bf1ccd25a126b7b
SG-Alten <simone.grippa@alten.it>
2021-06-07 17:33:01 +0200

    Update Element Text-With-Icon and add Icon Help

--- librerie/youwebbusiness/layout/oggetti/text/text_with_icon/oggetto.php
+++ librerie/youwebbusiness/layout/oggetti/text/text_with_icon/oggetto.php
@@ -1,12 +1,12 @@
 <div class="text-with-icon right-icon color-greenLink">
-    <i class="icon icon-ico_divieto"></i>
+    <i class="icon icon-ico_help"></i>
     Lorem ipsum dolor sit amet, consectetur adipisicing elit.
 </div>
 <div class="text-with-icon right-icon">
-    <i class="icon icon-ico_divieto"></i>
+    <i class="icon icon-ico_help"></i>
     Lorem ipsum dolor sit amet, consectetur adipisicing elit.
 </div>
 <div class="text-with-icon left-icon">
-    <i class="icon icon-ico_divieto"></i> 
+    <i class="icon icon-ico_help"></i> 
     Lorem ipsum dolor sit amet, consectetur adipisicing elit.
 </div>
