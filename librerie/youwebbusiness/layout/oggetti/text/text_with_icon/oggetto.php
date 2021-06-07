70fb907824771007a2bb475432c10ef34e2151de
SG-Alten <simone.grippa@alten.it>
2021-06-07 17:11:19 +0200

    Create Element Text-With-Icon

---
+++ librerie/youwebbusiness/layout/oggetti/text/text_with_icon/oggetto.php
@@ -0,0 +1,12 @@
+<div class="text-with-icon right-icon color-greenLink">
+    <i class="icon icon-ico_divieto"></i>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+</div>
+<div class="text-with-icon right-icon">
+    <i class="icon icon-ico_divieto"></i>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+</div>
+<div class="text-with-icon left-icon">
+    <i class="icon icon-ico_divieto"></i> 
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+</div>
