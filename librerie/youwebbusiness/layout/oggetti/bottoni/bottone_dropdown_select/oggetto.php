a501cc91072e6e9f64a4db317a55c29dd5e66921
sstrepponi <62430689+sstrepponi@users.noreply.github.com>
2021-06-08 17:05:06 +0200

    oggetto bottone dropdown operazioni massive (in lavorazione)
     oggetto bottone dropdown operazioni massive (in lavorazione)

---
+++ librerie/youwebbusiness/layout/oggetti/bottoni/bottone_dropdown_select/oggetto.php
@@ -0,0 +1,27 @@
+<div class="dropdown dropdown-select-wrapper">
+    <button class="btn btn-white dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+        OPERAZIONI MASSIVE
+        <img src="fe/img/icon/more.svg">
+     </button>
+      <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+          <li><a href="javascript:;" data-value="">Associa progetto</a></li>
+          <li><a href="javascript:;" data-value="">Modifica Categoria/Sottoprodotto</a></li>
+      </ul>
+     </div>
+</div>
+<script>
+
+//emulazione funzione di popolamento del dropdown
+$(function(){
+var dropDownSelect = $(".dropdown-select-wrapper");
+    dropDownSelect.each(function(index) {
+        var dropDownMenu = dropDownSelect.find(".dropdown-menu"),
+            dropDownItems = dropDownMenu.find("a"),
+            dropDownBtn = dropDownSelect.find(".btn");
+            dropDownItems.click(function(){
+              dropDownBtn.html($(this).text() + ' <span class="caret"></span>');
+                dropDownBtn.val($(this).data('value'));
+            });
+    })
+})
+</script>
--- librerie/youwebbusiness/layout/oggetti/tabelle/_tabella_da_sviluppo/oggetto.php
+++ librerie/youwebbusiness/layout/oggetti/tabelle/_tabella_da_sviluppo/oggetto.php
@@ -60,34 +60,9 @@
                </div>
                <div class="col-auto">
                <!--bottone operazioni massive-->
-                <div class="dropdown dropdown-select-wrapper">
-                        <button class="btn btn-white dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                        OPERAZIONI MASSIVE
-                            <img src="fe/img/icon/more.svg">
-                        </button>
-                        <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-                            <li><a href="javascript:;" data-value="">Associa progetto</a></li>
-                            <li><a href="javascript:;" data-value="">Modifica Categoria/Sottoprodotto</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <style>
-
-                </style>
-                <script>
-
-                    //emulazione funzione di popolamento del dropdown
-                    $(function(){
-                    var dropDownSelect = $(".dropdown-select-wrapper");
-                        dropDownSelect.each(function(index) {
-                            var dropDownItems = dropDownSelect.find(".dropdown-menu a");
-                            dropDownItems.click(function(){
-                                $(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
-                                $(this).parents(".dropdown").find('.btn').val($(this).data('value'));
-                            });
-                        })
-                    })
-                </script>
+               <!-- BOX ASSOCIA PROGETTO -->
+                <?php virtual('/youwebbusiness/layout/oggetti/bottoni/bottone_dropdown_select/oggetto.php'); ?>
+                <!-- ./ BOX ASSOCIA PROGETTO-->
                <!-- ko if: Control.enableAutorizzazioneMultipla() && Control.isCheckAllMobileVisible() --><!-- /ko -->
             </div>
          </div>
