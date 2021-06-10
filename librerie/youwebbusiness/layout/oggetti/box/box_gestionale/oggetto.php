d56be2f404891e83d4e9c276bc30b05379001536
SG-Alten <simone.grippa@alten.it>
2021-06-10 18:25:45 +0200

    Nuovi elementi Selettore Titolo Verde Box Gestionale

---
+++ librerie/youwebbusiness/layout/oggetti/box/box_gestionale/oggetto.php
@@ -0,0 +1,62 @@
+<!-- BOX GESTIONALE-->
+<style>
+	.box-gestionale .sftw-image {
+		height: 40px;
+	}
+	.box-gestionale .note {
+		font-size: 13px;
+	}
+</style>	
+
+<section>
+	<div class="box-gestionale bordered mt-3 mb-3 p-3 pb-4 pt-4">
+		
+		<div class="row row-eq-height align-items-top">
+			<div class="col-md-8 col-12 pb-3 left-text">
+				<h2>Collega il tuo gestionale di fatturazione elettronica</h2>
+				<p>1. Le fatture della tua azienda verranno mostrate direttamente sul portale e riconciliate automaticamente</p>
+				<p>2. Potrai caricare anche le tue fatture in formato xml che hai precendentemente scaricato
+					<span class="note d-block">Limite caricamento: file xml 1mb</span>
+				</p>
+				<p>GESTIONALI DISPONIBILI:</p>
+				<img class="sftw-image" src="./fe/img/placeholder-gestionale-sw-1.png" alt="immagine azienda box-gestionale">
+				<img class="sftw-image" src="./fe/img/placeholder-gestionale-sw-2.png" alt="immagine azienda box-gestionale">
+				<img class="sftw-image" src="./fe/img/placeholder-gestionale-sw-3.png" alt="immagine azienda box-gestionale">
+			</div>
+			<div class="col-md-4 col-12 pr-3 pl-3">
+				<div class="row align-items-top justify-content-center">			
+					<img class="d-flex img-fluid w-75" src="./fe/img/placeholder_box_gestionale.png" alt="immagine box box-gestionale">				
+				</div>
+				<!-- Bottoni Opzione 1-->
+				<div class="row justify-content-end mt-4">
+					<div class="col-sm-12">
+						<div class="form-row d-flex justify-content-end">
+							<div class="form-group col-6 col-lg-6">
+								<a href="#" class="btn btn-primary btn-block " >MODIFICA</a>
+							</div>
+							<div class="form-group col-6 col-lg-6">
+								<a href="#" class="btn btn-secondary btn-block ">SCOLLEGA</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- End Bottoni Opzione 1 -->	
+			</div>
+		</div>
+		<!-- Bottoni Opzione 2-->
+		<div class="row justify-content-end mt-3">
+			<div class="col-sm-6">
+				<div class="form-row d-flex justify-content-end">
+					<div class="form-group col-6 col-lg-4">
+						<a href="#" class="btn btn-primary btn-block " >MODIFICA</a>
+					</div>
+					<div class="form-group col-6 col-lg-4">
+						<a href="#" class="btn btn-secondary btn-block ">SCOLLEGA</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- End Bottoni Opzione 2 -->			
+	</div>
+</section>
+<!-- FINE BOX GESTIONALE-->
---
+++ librerie/youwebbusiness/layout/oggetti/selettore/selettore_azienda/oggetto.php
@@ -0,0 +1,46 @@
+ <!-- SELETTORE -->
+  <section class="selettore selettore-azienda">
+  
+    <!-- testata filtri desktop-->
+    <div class="d-none d-lg-block">
+      <section class="bordered p-3 m-0">
+        <div class="row d-flex justify-content-between align-items-center">
+            <div class="col col-md-10">
+              <h3 class="title mb-0">Clicca sul pulsante “Cambia” per selezionare l’azienda</h1>
+            </div>
+            <div class="col col-lg-2">
+                <a href="javascript:void(0)" class="btn btn-white fl-right mb-0" data-filter-button="filter-azienda">
+                    <i class="icon icon-arrow_down" title="icon-aggiorna"></i> Cambia
+                </a>
+            </div>
+        </div>
+      </section>
+    </div>
+    <!-- testata filtri mobile-->
+    <div class="d-block d-lg-none no-margin-section">
+      <section class="bordered m-0">
+      <div class="row" data-filter-button="filter-azienda">
+          <div class="col-10">
+          <div class="pb-0"><span class="dati-value d-inline">Nessuno</span></div>
+          <div class="pb-3"><span class="dati-value d-inline rapporti">Nessuno</span></div>
+          </div>
+          <div class="col-2 align-self-stretch">
+          <span class="btn-mobile-arrow d-flex align-items-center justify-content-end pb-3">
+              <i class="icon-arrow_right"></i>
+          </span>
+          </div>
+      </div>
+      </section>
+    </div>
+    
+    
+    <!-- parte con la select e i checkbox -->
+    <div class="no-margin-section">
+      <section data-filter-form="filter-azienda" style="display:none;">
+          <div class="bordered no-b-t">            
+  
+          </div>
+      </section>
+    </div>
+</section>
+
