11faf397ff97fa296fda83e93b1f73c2c7bbd7d5
sstrepponi <62430689+sstrepponi@users.noreply.github.com>
2021-06-11 08:45:43 +0200

    creato oggetto nuovo box alert contestuale
    creato oggetto nuovo box alert contestuale

---
+++ docs_convergenza/include/oggetti/Box/commons/alert_contestuale/oggetto.php
@@ -0,0 +1,10 @@
+<!--nuovo box di alert -->
+<div class="box-alert-inline warning">
+    <div class="box-alert-inline-esito">
+    <i class="icon icon-alert_caution" title="icon-alert_caution"></i>
+        <p>SI È VERIFICATO UN PROBLEMA NEL CARICAMENTO DELLE INFORMAZIONI</p>
+    </div>
+    <div class="text-with-icon"><a href="#"><i class="icon icon-reload" title="icon-trash_filled"></i>
+    <span class="text">Ricarica la pagina</span></a>
+</div>
+</div>
