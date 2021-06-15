bbcf560802b723f2390c76ac088aba92d566736d
sstrepponi <62430689+sstrepponi@users.noreply.github.com>
2021-06-15 16:57:35 +0200

    oggetti YWB
    oggetti YWB

--- librerie/youwebbusiness/layout/oggetti/accordion/accordion_with_squared/accordion.php
+++ librerie/youwebbusiness/layout/oggetti/accordion/accordion_with_squared/accordion.php
@@ -3,19 +3,18 @@
     <div class="row d-flex justify-content-between align-items-center w-100">
       <div class="col-md-6 col-lg-7 col-xl-8">
           <h5 class="card-header-title mb-0">
-            <span class="text-with-squared">
-              <span class="squared bkg-bfm_status_confermare"></span>
-              <span class="text color-gray1">Da confermare</span>
-            </span>
+            <div class="text-with-squared">
+              <div class="squared bkg-bfm_status_confermare"></div>
+              <div class="text color-gray1"><span>Da confermare</span></div>
+            </div>
           </h5>
       </div>
-      <hr class="up-body-separator d-block d-md-none w-100 mt-3 mb-3">
+      <hr class="separator d-block d-md-none w-100 mt-3 mb-3">
       <div class="col-md-6 col-lg-5 col-xl-4">
           <div class="row justify-content-md-end justify-content-between">
           <div class="col-5 col-md-5 col-lg-5 col-xl-5 d-flex justify-content-md-end justify-content-between align-items-center">
             <span class="color-gray2">10 transazioni</span>
           </div>
-          <!--<div class="col-2 col-md-2 col-lg-2 col-xl-2 d-flex justify-content-center align-items-center">18%</div>-->
           <div class="col-5 col-md-5 col-lg-5 col-xl-5 full-contain d-flex justify-content-end align-items-center">
             <strong class="text color-gray1">150.000,86 &euro;</strong>
           </div>
@@ -35,19 +34,24 @@
     <div class="row d-flex justify-content-between align-items-center w-100">
       <div class="col-md-6 col-lg-7 col-xl-8">
           <h5 class="card-header-title mb-0">
-            <span class="text-with-squared">
-              <span class="squared bkg-bfm_status_attive"></span>
-              <span class="text color-gray1">Attive</span>
-            </span>
+            <div class="text-with-squared">
+              <div class="squared bkg-bfm_status_attive"></div>
+              <div class="text color-gray1 with-progress">
+                  <span>Attive</span>
+                 <div class="progress">
+                    <div class="progress-bar bkg-bfm_status_attive" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                  </div>
+                </div>
+            </div>
           </h5>
       </div>
-      <hr class="up-body-separator d-block d-md-none w-100 mt-3 mb-3">
+      <hr class="separator d-block d-md-none w-100 mt-3 mb-3">
       <div class="col-md-6 col-lg-5 col-xl-4">
           <div class="row justify-content-md-end justify-content-between">
           <div class="col-5 col-md-5 col-lg-5 col-xl-5 d-flex justify-content-md-end justify-content-between align-items-center">
             <span class="color-gray2">10 transazioni</span>
           </div>
-          <!--<div class="col-2 col-md-2 col-lg-2 col-xl-2 d-flex justify-content-center align-items-center">18%</div>-->
+          <div class="col-2 col-md-2 col-lg-2 col-xl-2 d-flex justify-content-center align-items-center"><strong class="font-size-16 color-greenLink">25%</strong></div>
           <div class="col-5 col-md-5 col-lg-5 col-xl-5 full-contain d-flex justify-content-end align-items-center">
             <strong class="text color-gray1">150.000,86 &euro;</strong>
           </div>
@@ -67,19 +71,25 @@
     <div class="row d-flex justify-content-between align-items-center w-100">
       <div class="col-md-6 col-lg-7 col-xl-8">
           <h5 class="card-header-title mb-0">
-            <span class="text-with-squared">
-              <span class="squared bkg-bfm_status_categorizzare"></span>
-              <span class="text color-gray1">Da categorizzare</span>
-            </span>
+            <div class="text-with-squared">
+              <div class="squared bkg-bfm_status_categorizzare"></div>
+              <div class="text color-gray1 with-progress">
+                  <span>Da categorizzare</span>
+                  <div class="progress">
+                    <div class="progress-bar bkg-bfm_status_categorizzare" role="progressbar" style="width: 10%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
+                  </div>
+              </div>
+            </div>
           </h5>
+         
       </div>
-      <hr class="up-body-separator d-block d-md-none w-100 mt-3 mb-3">
+      <hr class="separator d-block d-md-none w-100 mt-3 mb-3">
       <div class="col-md-6 col-lg-5 col-xl-4">
           <div class="row justify-content-md-end justify-content-between">
           <div class="col-5 col-md-5 col-lg-5 col-xl-5 d-flex justify-content-md-end justify-content-between align-items-center">
             <span class="color-gray2">10 transazioni</span>
           </div>
-          <!--<div class="col-2 col-md-2 col-lg-2 col-xl-2 d-flex justify-content-center align-items-center">18%</div>-->
+          <div class="col-2 col-md-2 col-lg-2 col-xl-2 d-flex justify-content-center align-items-center"><strong class="font-size-16 color-greenLink">10%</strong></div>
           <div class="col-5 col-md-5 col-lg-5 col-xl-5 full-contain d-flex justify-content-end align-items-center">
             <strong class="text color-gray1">150.000,86 &euro;</strong>
           </div>
