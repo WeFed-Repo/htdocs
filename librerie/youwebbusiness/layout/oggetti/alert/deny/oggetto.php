734ae058be2db9c44149f66456cc9b43f7f83915
SG-Alten <simone.grippa@alten.it>
2021-06-15 10:12:53 +0200

    Update oggetto.php

--- librerie/youwebbusiness/layout/oggetti/alert/deny/oggetto.php
+++ librerie/youwebbusiness/layout/oggetti/alert/deny/oggetto.php
@@ -1,6 +1,6 @@
 <div class="alert alert-deny">
     <div class="alert-icon ">
-        <i class="icon icon-ico_divieto"></i>
+        <i class="icon icon-divieto"></i>
     </div> 
     Lorem ipsum dolor sit amet, consectetur adipisicing elit.
 </div>
