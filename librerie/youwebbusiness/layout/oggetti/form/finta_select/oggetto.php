54c84b3fab3338af3e7a57bd959b71294e544e17
sstrepponi <62430689+sstrepponi@users.noreply.github.com>
2021-06-17 17:20:26 +0200

    select custom (in lavorazione)
    select custom (in lavorazione)

--- librerie/youwebbusiness/layout/oggetti/form/finta_select/oggetto.php
+++ librerie/youwebbusiness/layout/oggetti/form/finta_select/oggetto.php
@@ -5,11 +5,11 @@
     <div class="form-row">
       <div class="form-group col-sm-4">
         <label class="control-label">Select normal</label>
-        <div class="dropdown select-custom" name="sc" id="sc" data-placeholder="Seleziona"></div>
+        <div class="dropdown select-custom" name="sc" id="sc"></div>
       </div>
       <div class="form-group col-sm-4">
         <label class="control-label">Select con checkbox</label>
-        <div class=" dropdown select-custom" name="scCheckbox" id="scCheckbox" data-checkbox=true data-placeholder="Seleziona"></div>
+        <div class=" dropdown select-custom" name="scCheckbox" id="scCheckbox" data-checkbox=true data-placeholder="Seleziona.."></div>
       </div>
       <div class="form-group col-sm-4">
         <label class="control-label">Select bottone funzionale</label>
@@ -24,32 +24,74 @@
 <script type="text/javascript">
     $(function () {
       //esempio di array con i valori per la select
-      var dataOptions =  [
+      var dataOptionsSimpleSelect =  [
               {
                 "id": 0,
-                "text": "Option 1",
-                "value":"1"
-              },
-              {
-                "id": 1,
-                "text": "Option 2",
-                "value": "2",
+                "text": "Seleziona",
+                "value":"0",
                 "selected": true
               },
               {
+                "id": 1,
+                "text": "Option 1",
+                "value": "1",
+                "selected": false
+              },
+              {
                 "id": 2,
+                "text": "Option 2",
+                "value": "2",
+                "disabled": true
+              },
+              {
+                "id": 3,
                 "text": "Option 3",
                 "value": "3",
                 "disabled": true
               }
         ]
+        var dataOptionsCheckSelect =  [
+              {
+                "id": 1,
+                "text": "Option 1",
+                "value": "1",
+                "selected": false
+              },
+              {
+                "id": 2,
+                "text": "Option 2",
+                "value": "2",
+                "disabled": true
+              }
+        ]
+        var dataOptionsBtn =  [
+              {
+                "id": 1,
+                "text": "Option 1",
+                "value": "1",
+                "selected": false
+              },
+              {
+                "id": 2,
+                "text": "Option 2",
+                "value": "2",
+                "disabled": true
+              }
+        ]
       
-      
-      $(".select-custom").each(function(){
-        $(this).initSelectCustom(dataOptions, function(){
+      //esegue la finta select normale
+      $("#sc").initSelectCustom(dataOptionsSimpleSelect, function(){
             console.log("funzione di cmb");
-        })
-      }) 
+      });
+       //esegue la finta select con i check
+      $("#scCheckbox").initSelectCustom(dataOptionsCheckSelect, function(){
+            console.log("funzione di cmb");
+      })
+      //esegue la finta select con i check
+      $("#scBtn").initSelectCustom(dataOptionsBtn, function(){
+            console.log("funzione di cmb");
+      })
+     
     })
     
     
