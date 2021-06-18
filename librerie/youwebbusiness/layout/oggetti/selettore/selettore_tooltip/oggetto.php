3ae5e244af8ccee543006a853414556d6afe6e45
sstrepponi <62430689+sstrepponi@users.noreply.github.com>
2021-06-18 17:45:19 +0200

    box dinamico + materiale ywb
    box dinamico + materiale ywb

---
+++ librerie/youwebbusiness/layout/oggetti/selettore/selettore_tooltip/oggetto.php
@@ -0,0 +1,50 @@
+ <!-- SELETTORE -->
+  <section class="selettore">
+  <!-- testata filtri desktop-->
+    <div class="d-lg-block">
+      <section class="bordered p-3 m-0">
+        <div class="row d-flex justify-content-between align-items-center">
+            <div class="col col-md-10">
+              <div class="row">
+                <div class="col col-md-5 col-lg-5">
+                  <span class="dati-key">Ragione sociale:</span> <span class="dati-value">Tutti</span>
+                </div>
+                <div class="col col-md-5 col-lg-5">
+                  <span class="dati-key">Gestionale:</span> 
+                    <span class="dati-value">
+                      <span class="text-with-icon-inline right-icon">Team System digital <a class="icon" href="javascript:;"  title="<span class='font-size-small'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor <a href='#'>incididunt ut labore</a> et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</span>" data-trigger="click" data-toggle="tooltip"><i class="icon icon-help"></a></i></span>
+                    </span> 
+                </div>
+                <div class="col col-md-2 col-lg-2">
+                  <span class="dati-key">Ragione sociale:</span> <span class="dati-value">Tutti</span>
+                </div>
+              </div>
+            </div>
+            <div class="col col-lg-2 d-none d-lg-block">
+              <a href="javascript:void(0)" class="btn btn-white fl-right mb-0" data-filter-button="filter-selector-tool">
+                  <i class="icon icon-arrow_down" title="icon-aggiorna"></i> Cambia
+              </a>
+            </div>
+        </div>
+      </section>
+    </div>
+    <!-- testata filtri mobile-->
+    <div class="d-block d-lg-none no-margin-section">
+      <section class="bordered m-0">
+        <div class="row" data-toggle="modal" data-target="#filter-selector-tool">
+          
+        </div>
+      </section>
+    </div>
+    
+    
+    <!-- contenuti desktop per il mobile solita logica dell'overlayer-->
+    <div class="no-margin-section">
+      <section data-filter-form="filter-selector-tool" style="display:none;">
+          <div class="bordered no-b-t">            
+  
+          </div>
+      </section>
+    </div>
+</section>
+
--- librerie/youwebbusiness/layout/template/tpl_bfm_oggetti.php
+++ librerie/youwebbusiness/layout/template/tpl_bfm_oggetti.php
@@ -1,27 +1,27 @@
 <!-- SELETTORE RAPPORTO BFM -->
 <?php virtual('/youwebbusiness/layout/oggetti/selettore/selettore_bfm/oggetto.php'); ?>
 <!-- ./ SELETTORE RAPPORTO BFM -->
-
+<hr>
 <!-- BOX DIVIDI SPESA -->
 <?php virtual('/youwebbusiness/layout/oggetti/form/numbered_block/oggetto.php'); ?>
 <!-- ./ BOX DIVIDI SPESA -->
-
+<hr>
 <!-- MODALE DETTAGLIO MOVIMENTO -->
 <?php virtual('/youwebbusiness/layout/oggetti/modale/modale_dettaglio/oggetto.php'); ?>
 <!-- ./ MODALE DETTAGLIO MOVIMENTO -->
-
+<hr>
 <!-- BOX ASSOCIA PROGETTO -->
 <?php virtual('/youwebbusiness/layout/oggetti/box/box_block_arrow/oggetto.php'); ?>
 <!-- ./ BOX ASSOCIA PROGETTO-->
-
+<hr>
 <!-- BOX GESTIONALE -->
 <?php virtual('/youwebbusiness/layout/oggetti/box/box_gestionale/oggetto.php'); ?>
 <!-- ./ BOX GESTIONALE-->
-
+<hr>
 <!-- SELETTORE AZIENDA -->
 <?php virtual('/youwebbusiness/layout/oggetti/selettore/selettore_azienda/oggetto.php'); ?>
 <!-- ./ SELETTORE AZIENDA -->
-
+<hr>
 <!-- TEXT WITH ICON -->
 <div class="row">
 	<div class="col-sm-12">
@@ -32,21 +32,30 @@
     </div>
 </div>
 <!-- ./ TEXT WITH ICON -->
-
+<hr>
 <!-- MODALE IMMAGINE VERTICALE -->
 <?php virtual('/youwebbusiness/layout/oggetti/modale/modale_con_immagine_verticale/oggetto.php');?>
 <!-- ./ MODALE IMMAGINE VERTICALE -->
-
+<hr>
 <!-- MODALE CON CHECKBOX -->
 <?php virtual('/youwebbusiness/layout/oggetti/modale/modale_checkbox/oggetto.php');?>
 <!-- ./ MODALE CON CHECKBOX -->
-
+<hr>
 <!-- BOX CON BOTTONI; ACCORDION; TABELLA -->
 <!-- MODALE CON CHECKBOX -->
 <?php virtual('/youwebbusiness/layout/oggetti/box/box_accordion_with_table/oggetto.php');?>
 <!-- ./ MODALE CON CHECKBOX -->
-
+<hr>
 <!-- BOX CON INFO E BOTTONE -->
 <?php virtual('/youwebbusiness/layout/oggetti/box/box_no_promo_info/oggetto.php');?>
 <!-- ./ BOX CON INFO E BOTTONE-->
+<hr>
 
+<!-- TABELLA CON TOOLTIP E STATI -->
+<?php virtual('/youwebbusiness/layout/oggetti/tabelle/_tabella_da_sviluppo/oggetto.php');?>
+<!-- ./ TABELLA CON TOOLTIP E STATI-->
+<hr>
+
+<!-- SELETTORE CON TOOLTIP + ICONA -->
+<?php virtual('/youwebbusiness/layout/oggetti/selettore/selettore_tooltip/oggetto.php'); ?>
+<!-- ./ SELETTORE AZIENDA -->
