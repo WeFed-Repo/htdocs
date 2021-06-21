6a245e24049f7aacc225b349af5661bd29cdf815
sstrepponi <62430689+sstrepponi@users.noreply.github.com>
2021-06-21 09:38:20 +0200

    oggetto YWB selettore con tooltip
    oggetto YWB selettore con tooltip

--- librerie/youwebbusiness/layout/oggetti/selettore/selettore_tooltip/oggetto.php
+++ librerie/youwebbusiness/layout/oggetti/selettore/selettore_tooltip/oggetto.php
@@ -1,22 +1,27 @@
  <!-- SELETTORE -->
   <section class="selettore">
   <!-- testata filtri desktop-->
-    <div class="d-lg-block">
+    <div class="d-none d-lg-block">
       <section class="bordered p-3 m-0">
         <div class="row d-flex justify-content-between align-items-center">
             <div class="col col-md-10">
               <div class="row">
-                <div class="col col-md-5 col-lg-5">
-                  <span class="dati-key">Ragione sociale:</span> <span class="dati-value">Tutti</span>
+                <div class="col col-md-4 col-lg-4">
+                  <span class="dati-key">Ragione sociale:</span> <span class="dati-value">Fiat S.p.a</span>
                 </div>
-                <div class="col col-md-5 col-lg-5">
+                <div class="col col-md-4 col-lg-4">
                   <span class="dati-key">Gestionale:</span> 
                     <span class="dati-value">
-                      <span class="text-with-icon-inline right-icon">Team System digital <a class="icon" href="javascript:;"  title="<span class='font-size-small'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor <a href='#'>incididunt ut labore</a> et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</span>" data-trigger="click" data-toggle="tooltip"><i class="icon icon-help"></a></i></span>
+                      <span class="text-with-icon-inline right-icon">Team System digital 
+                      <a class="icon-inline" href="javascript:;"  title="<span class='font-size-small'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor <a href='#'>incididunt ut labore</a> et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</span>" data-trigger="focus" data-toggle="tooltip"><i class="icon icon-help mr-0"></i></a></span>
                     </span> 
                 </div>
-                <div class="col col-md-2 col-lg-2">
-                  <span class="dati-key">Ragione sociale:</span> <span class="dati-value">Tutti</span>
+                <div class="col col-md-4 col-lg-4">
+                  <span class="dati-value"> 
+                      <span class="text-with-icon-inline right-icon"><a href="#"><i class="icon icon-upload"></i>Carica xml</a>  
+                      <a class="icon-inline" href="javascript:;"  title="<span class='font-size-small'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor <a href='#'>incididunt ut labore</a> et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</span>" data-trigger="focus" data-toggle="tooltip"><i class="icon icon-help mr-0"></i></a>
+                    </span>
+                  </span>
                 </div>
               </div>
             </div>
@@ -30,9 +35,29 @@
     </div>
     <!-- testata filtri mobile-->
     <div class="d-block d-lg-none no-margin-section">
-      <section class="bordered m-0">
-        <div class="row" data-toggle="modal" data-target="#filter-selector-tool">
-          
+      <section class="bordered pb-3">
+        <div class="row align-items-center" data-toggle="modal" data-target="#filter-selector-tool">
+          <div class="col-11">
+            <div class="row align-items-center">
+              <div class="col-6 col-lg-6">
+                <div ><span class="dati-value d-inline"><strong>Fiat spa</strong></span></div>
+                <div><span class="dati-value d-inline rapporti"> <span class="text-with-icon-inline right-icon">Team System digital 
+                <a class="icon-inline" href="javascript:;"  title="<span class='font-size-small'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor <a href='#'>incididunt ut labore</a> et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</span>" data-trigger="focus" data-toggle="tooltip"><i class="icon icon-help mr-0"></i></a></span></span></div>
+              </div>
+              <div class="col-6 col-lg-6">
+                  <span class="dati-value"> 
+                      <span class="text-with-icon-inline right-icon"><a href="#"><i class="icon icon-upload mr-1"></i>Carica xml</a>  
+                      <a class="icon-inline" href="javascript:;"  title="<span class='font-size-small'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor <a href='#'>incididunt ut labore</a> et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</span>" data-trigger="focus" data-toggle="tooltip"><i class="icon icon-help mr-0"></i></a>
+                    </span>
+                  </span>
+                </div>
+            </div>
+          </div>
+          <div class="col-1">
+            <span class="btn-mobile-arrow d-flex align-items-center justify-content-end">
+                <i class="icon-arrow_right"></i>
+            </span>
+          </div>
         </div>
       </section>
     </div>
