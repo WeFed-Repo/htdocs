4616d6e286955518296a549be7a9914180f9dc47
sstrepponi <62430689+sstrepponi@users.noreply.github.com>
2021-06-21 17:04:24 +0200

    integrazione oggetti YWB
    integrazione oggetti YWB

--- librerie/youwebbusiness/layout/oggetti/accordion/accordion_with_squared/accordion.php
+++ librerie/youwebbusiness/layout/oggetti/accordion/accordion_with_squared/accordion.php
@@ -1,7 +1,7 @@
-<div class="card collapser">
+<div class="card collapser b-r-3">
   <div class="card-header collapsed d-flex justify-content-between align-items-top" data-toggle="collapse">
     <div class="row d-flex justify-content-between align-items-center w-100">
-      <div class="col-md-6 col-lg-7 col-xl-8">
+      <div class="col-md-7 col-lg-7 col-xl-8">
           <h5 class="card-header-title mb-0">
             <div class="text-with-squared">
               <div class="squared bkg-bfm_status_confermare"></div>
@@ -10,7 +10,7 @@
           </h5>
       </div>
       <hr class="separator d-block d-md-none w-100 mt-3 mb-3">
-      <div class="col-md-6 col-lg-5 col-xl-4">
+      <div class="col-md-5 col-lg-5 col-xl-4">
           <div class="row justify-content-md-end justify-content-between">
           <div class="col-5 col-md-5 col-lg-4 col-xl-5 d-flex justify-content-md-end justify-content-between align-items-center">
             <span class="color-gray2">10 transazioni</span>
@@ -29,15 +29,16 @@
     </div>
   </div>
 </div>
-<div class="card collapser">
+<div class="card collapser b-r-3 border-error">
   <div class="card-header collapsed d-flex justify-content-between align-items-top" data-toggle="collapse">
     <div class="row d-flex justify-content-between align-items-center w-100">
-      <div class="col-md-6 col-lg-7 col-xl-8">
+      <div class="col-md-7 col-lg-7 col-xl-8">
           <h5 class="card-header-title mb-0">
             <div class="text-with-squared">
               <div class="squared bkg-bfm_status_attive"></div>
               <div class="text color-gray1 with-progress">
-                  <span>Attive</span>
+                 <span>Attive</span>
+                 <span class="d-none d-md-block text-alert"><i class="icon icon-warning color-error"></i> Soglia superata</span>
                  <div class="progress">
                     <div class="progress-bar bkg-bfm_status_attive" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
                   </div>
@@ -46,7 +47,7 @@
           </h5>
       </div>
       <hr class="separator d-block d-md-none w-100 mt-3 mb-3">
-      <div class="col-md-6 col-lg-5 col-xl-4">
+      <div class="col-md-5 col-lg-5 col-xl-4">
           <div class="row justify-content-md-end justify-content-between">
           <div class="col-5 col-md-5 col-lg-4 col-xl-5 d-flex justify-content-md-end justify-content-between align-items-center">
             <span class="color-gray2">10 transazioni</span>
@@ -66,15 +67,16 @@
     </div>
   </div>
 </div>
-<div class="card collapser">
+<div class="card collapser b-r-3 border-warning">
   <div class="card-header collapsed d-flex justify-content-between align-items-top" data-toggle="collapse">
     <div class="row d-flex justify-content-between align-items-center w-100">
-      <div class="col-md-6 col-lg-7 col-xl-8">
+      <div class="col-md-7 col-lg-7 col-xl-8">
           <h5 class="card-header-title mb-0">
             <div class="text-with-squared">
               <div class="squared bkg-bfm_status_categorizzare"></div>
               <div class="text color-gray1 with-progress">
                   <span>Da categorizzare</span>
+                  <span class="d-none d-md-block text-alert"><i class="icon icon-attenzione color-warning"></i> Soglia quasi raggiunta</span>
                   <div class="progress">
                     <div class="progress-bar bkg-bfm_status_categorizzare" role="progressbar" style="width: 10%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
                   </div>
@@ -84,7 +86,7 @@
          
       </div>
       <hr class="separator d-block d-md-none w-100 mt-3 mb-3">
-      <div class="col-md-6 col-lg-5 col-xl-4">
+      <div class="col-md-5 col-lg-5 col-xl-4">
           <div class="row justify-content-md-end justify-content-between">
           <div class="col-5 col-md-5 col-lg-4 col-xl-5 d-flex justify-content-md-end justify-content-between align-items-center">
             <span class="color-gray2">10 transazioni</span>
