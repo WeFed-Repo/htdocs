2e32cef9149cbc7485ec06f79bf6a5c872b516a3
SG-Alten <simone.grippa@alten.it>
2021-06-30 13:03:16 +0200

    Nuovo oggetto carousel-multicard [Versione Solo Desktop!]

---
+++ librerie/youwebbusiness/layout/oggetti/widget/carousel_multicards/oggetto.php
@@ -0,0 +1,68 @@
+<div id="carouselMulticard" class="carousel carousel-multicard slide carousel-arrows-outside pb-3">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselMulticard" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselMulticard" data-slide-to="1"></li>
+    <li data-target="#carouselMulticard" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <div class="carousel-item-centerer">
+        <div class="row row-eq-height">
+          <div class="col-4">
+            <div class="carousel-card"></div>
+          </div>
+          <div class="col-4">
+            <div class="carousel-card"></div>
+          </div>
+          <div class="col-4">
+            <div class="carousel-card"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <div class="carousel-item-centerer">
+        <div class="row row-eq-height">
+          <div class="col-4">
+            <div class="carousel-card"></div>
+          </div>
+          <div class="col-4">
+            <div class="carousel-card"></div>
+          </div>
+          <div class="col-4">
+            <div class="carousel-card"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <div class="carousel-item-centerer">
+        <div class="row row-eq-height">
+          <div class="col-4">
+            <div class="carousel-card"></div>
+          </div>
+          <div class="col-4">
+            <div class="carousel-card"></div>
+          </div>
+          <div class="col-4">
+            <div class="carousel-card"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <a class="carousel-control-prev" href="#carouselMulticard" role="button" data-slide="prev">
+    <img src="./fe/img/icon/left-arrow.svg">
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselMulticard" role="button" data-slide="next">
+    <img src="./fe/img/icon/right-arrow.svg">
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+
+
+
+
