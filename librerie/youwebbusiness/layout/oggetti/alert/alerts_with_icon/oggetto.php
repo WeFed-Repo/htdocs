702cd336fd09681e7123acfe5853505173b9f52a
SG-Alten <simone.grippa@alten.it>
2021-06-30 11:37:01 +0200

    Nuovi elementi Alert con bottoni (bottoni vecchio metodo)

--- librerie/youwebbusiness/layout/oggetti/alert/alerts_with_icon/oggetto.php
+++ librerie/youwebbusiness/layout/oggetti/alert/alerts_with_icon/oggetto.php
@@ -35,3 +35,63 @@
     </div> 
     <span class="alert-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
 </div>
+
+<div class="alert alert-icomoon danger-style-o medium-spaced d-block">
+    <div class="alert-icon">
+        <i class="icon icon-warning"></i>
+    </div> 
+    <span class="alert-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
+
+    <div class="row alert-button-area justify-content-end mt-3">
+        <div class="col-sm-6">
+            <div class="form-row d-flex justify-content-end">
+                <div class="form-group col-12 col-sm-6 col-lg-4">
+                    <a href="#" class="btn btn-primary btn-block m-0">Bottone 1</a>
+                </div>
+                <div class="form-group col-12 col-sm-6 col-lg-4">
+                    <a href="#" class="btn btn-secondary btn-block m-0">Bottone 2</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="alert alert-icomoon warning-style-o medium-spaced d-block">
+    <div class="alert-icon">
+        <i class="icon icon-attenzione"></i>
+    </div> 
+    <span class="alert-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
+
+    <div class="row alert-button-area justify-content-end mt-3">
+        <div class="col-sm-6">
+            <div class="form-row d-flex justify-content-end">
+                <div class="form-group col-12 col-sm-6 col-lg-4">
+                    <a href="#" class="btn btn-primary btn-block m-0">Bottone 1</a>
+                </div>
+                <div class="form-group col-12 col-sm-6 col-lg-4">
+                    <a href="#" class="btn btn-secondary btn-block m-0">Bottone 2</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="alert alert-icomoon success-style-o medium-spaced d-block">
+    <div class="alert-icon">
+        <i class="icon icon-round_check"></i>
+    </div> 
+    <span class="alert-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
+
+    <div class="row alert-button-area justify-content-end mt-3">
+        <div class="col-sm-6">
+            <div class="form-row d-flex justify-content-end">
+                <div class="form-group col-12 col-sm-6 col-lg-4">
+                    <a href="#" class="btn btn-primary btn-block m-0">Bottone 1</a>
+                </div>
+                <div class="form-group col-12 col-sm-6 col-lg-4">
+                    <a href="#" class="btn btn-secondary btn-block m-0">Bottone 2</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
