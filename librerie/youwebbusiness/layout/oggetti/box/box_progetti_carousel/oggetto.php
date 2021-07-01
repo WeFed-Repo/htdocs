c1a6a29d17e94210b39f4e31c7d47df4c33c9aee
sstrepponi <62430689+sstrepponi@users.noreply.github.com>
2021-07-01 09:47:10 +0200

    box progetti carousel
    box progetti carousel (tutte le casistiche)

--- librerie/youwebbusiness/layout/oggetti/box/box_progetti_carousel/oggetto.php
+++ librerie/youwebbusiness/layout/oggetti/box/box_progetti_carousel/oggetto.php
@@ -1,12 +1,14 @@
 <!-- BOX DA USARE NEL CAROUSEL-->
 
 <section>
-	<!--in corso -->
+	<!--casistiche in corso -->
 		<div class="row">
 			<div class="col-12 col-md-4">
 				<div class="box-progetti bordered border-top-error light b-r-t box-with-shadow">
-					<a href="#" class="link-p-a"><i class="icon icon-circle_arrow_right"></i></a>
-					<div class="text-center pl-md-2 pr-md-2 pl-xl-5 pr-xl-5">
+					<div href="#" class="el-p-a">
+					   <a href="#"><i class="icon icon-circle_arrow_right"></i></a>
+					</div>
+					<div class="text-center pl-md-3 pr-md-3 pl-xl-5 pr-xl-5">
 						<span class="text color-gray1 font-size-16"><strong>Ristrutturazione immobile</strong></span>
 						<div class="arrow-box-wrapper"><p class="color-greenLink font-size-small">02 marzo   <i class="icon icon-arrow_right_text"></i> 30 novembre</p></div>
 						<hr>
@@ -29,8 +31,12 @@
 			</div>
 			<div class="col-12 col-md-4">
 				<div class="box-progetti bordered border-top-warning light b-r-t box-with-shadow">
-					<a href="#" class="link-p-a"><i class="icon icon-circle_arrow_right"></i></a>
-					<div class="text-center pl-md-2 pr-md-2 pl-xl-5 pr-xl-5">
+					
+					<div href="#" class="el-p-a">
+					   <a href="#"><i class="icon icon-circle_arrow_right"></i></a>
+					</div>
+					
+					<div class="text-center pl-md-3 pr-md-3 pl-xl-5 pr-xl-5">
 						<span class="text color-gray1 font-size-16"><strong>Nuovi impianti Parma</strong></span>
 						<div class="arrow-box-wrapper"><p class="color-greenLink font-size-small">02 marzo   <i class="icon icon-arrow_right_text"></i> 30 novembre</p></div>
 						<hr>
@@ -53,8 +59,10 @@
 			</div>
 			<div class="col-12 col-md-4">
 				<div class=" box-progetti bordered border-top-success light b-r-t box-with-shadow">
-					<a href="#" class="link-p-a"><i class="icon icon-circle_arrow_right"></i></a>
-					<div class="text-center pl-md-2 pr-md-2 pl-xl-5 pr-xl-5">
+					<div href="#" class="el-p-a">
+					   <a href="#"><i class="icon icon-circle_arrow_right"></i></a>
+					</div>
+					<div class="text-center pl-md-3 pr-md-3 pl-xl-5 pr-xl-5">
 						<span class="text color-gray1 font-size-16"><strong>Uffici Brescia</strong></span>
 						<div class="arrow-box-wrapper"><p class="color-greenLink font-size-small">02 marzo   <i class="icon icon-arrow_right_text"></i> 30 novembre</p></div>
 						<hr>
@@ -77,8 +85,67 @@
 			</div>
 		</div>
 </section>
-<!--scaduti -->
-
+<!--casistica scaduto + casistica nuovo -->
+<section>
+	 <div class="row">
+			<div class="col-12 col-md-4">
+				<!--scaduto -->
+				<div class="box-progetti bordered border-top-$gray8 light b-r-t box-with-shadow">
+					<div href="#" class="el-p-a">
+					   <a href="#"><i class="icon icon-circle_arrow_right"></i></a>
+					</div>
+					<div class="text-center pl-md-3 pr-md-3 pl-xl-5 pr-xl-5">
+						<span class="text color-gray1 font-size-16"><strong>Ristrutturazione immobile</strong></span>
+						<div class="arrow-box-wrapper"><p class="color-$gray8 font-size-small"><i class="icon icon-calendario pr-1"></i>Scaduto il: 30 Nov 2020</p></div>
+						<hr>
+						<div class="font-size-small">
+							<p>
+								<span class="text-alert">Totale spese effettuate</span>
+								<span class="text"><strong>8.000,00 &euro;</strong></span>
+							</p>
+							<p>
+								<span class="text-alert">Budget prefissato</span>
+								<span class="text font-size-20 color-gray1"><strong>100.000,00 &euro;</strong></span>
+							</p>
+						</div>
+					</div>
+					<div class="d-flex justify-content-between align-items-center cta-wrapper">
+						<a href="#" class="cta-gray">Elimina</a>
+						<a href="#">Gestisci</a>
+					</div>
+				</div>
+			</div>
+			<!--nuovo -->
+			<div class="col-12 col-md-4">
+				<div class="box-progetti bordered box-with-shadow">
+					<div href="#" class="el-p-a with-label">
+					    <span class="label-new">NUOVO</span>
+						<a href="#"><i class="icon icon-circle_arrow_right"></i></a>
+					</div>
+					<div class="text-center pl-md-3 pr-md-3 pl-xl-5 pr-xl-5">
+						<span class="text color-gray1 font-size-16"><strong>Nuovi impianti Parma</strong></span>
+						<div class="arrow-box-wrapper"><p class="color-greenLink font-size-small">02 marzo   <i class="icon icon-arrow_right_text"></i> 30 novembre</p></div>
+						<hr>
+						<div class="font-size-small">
+							<p>
+								<span class="text-alert">Totale spese effettuate</span>
+								<span class="text"><strong>0,00 &euro;</strong></span>
+							</p>
+							<p>
+								<span class="text-alert">Budget prefissato</span>
+								<span class="text font-size-20 color-gray1"><strong>100.000,00 &euro;</strong></span>
+							</p>
+						</div>
+					</div>
+					<div class="d-flex justify-content-between align-items-center cta-wrapper">
+						<a href="#" class="cta-gray">Elimina</a>
+						<a href="#"><i class="icon icon-piu mr-0"></i>Aggiungi transazione</a>
+					</div>
+				</div>
+			</div>
+			
+		</div>
+</section>
 
 
 <!-- BOX DA USARE NEL CAROUSEL-->
