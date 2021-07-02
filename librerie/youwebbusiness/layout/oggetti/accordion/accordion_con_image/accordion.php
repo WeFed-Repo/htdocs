ce8a91c2083fbdd9d73f72617811a68ab9200c16
SG-Alten <simone.grippa@alten.it>
2021-07-02 17:56:10 +0200

    Impostazione nuovo oggetto YWB Accordion with image

---
+++ librerie/youwebbusiness/layout/oggetti/accordion/accordion_con_image/accordion.php
@@ -0,0 +1,100 @@
+<div class="card collapser">
+  <div class="card-header collapsed d-flex justify-content-between align-items-center" data-toggle="collapse">
+    <h5 class="card-header-title mb-0">
+      Collapsible Group Item #1
+    </h5>
+    <span class="card-header-icon"></span>
+  </div>
+  <div class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+    <div class="card-body">
+      <hr class="up-body-separator">
+      <p><span class="card-body-text">Visualizza di seguito le categorie di spesa sulle quali puoi impostare le tue soglie</span><p>
+      <!-- Square Box Soglie -->
+      <div class="row">
+        <!-- Group -->
+        <div class="col-md-4">
+          <div class="box-with-icon clean-style">
+            <span class="text-with-squared">
+              <span class="squared ico-wrapper bkg-bfm_fornitori"><i class="icon icon-category_fornitori"></i></span>
+              <a href="#" class="text">Fornitori</a>
+            </span>    
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="box-with-icon clean-style">
+            <span class="text-with-squared">
+              <span class="squared ico-wrapper bkg-bfm_operazionifinanziarie"><i class="icon icon-category_operazionifinanziarie"></i></span>
+              <a href="#" class="text">Operazioni <br> finanziarie</a>
+            </span>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="box-with-icon clean-style">
+            <span class="text-with-squared">
+              <span class="squared ico-wrapper bkg-bfm_bollette"><i class="icon icon-category_bollette"></i></span>
+              <a href="#" class="text">Bollette</a>
+            </span>
+          </div>
+        </div>
+        <!-- End Group -->
+        <!-- Group -->
+        <div class="col-md-4">
+          <div class="box-with-icon clean-style">
+            <span class="text-with-squared">
+              <span class="squared ico-wrapper bkg-bfm_ricavi"><i class="icon icon-category_ricavi"></i></span>
+              <a href="#" class="text">Ricavi</a>
+            </span>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="box-with-icon clean-style">
+            <span class="text-with-squared">
+              <span class="squared ico-wrapper bkg-bfm_assicurazione"><i class="icon icon-category_assicurazione"></i></span>
+              <a href="#" class="text">Assicurazione</a>
+            </span>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="box-with-icon clean-style">
+            <span class="text-with-squared">
+              <span class="squared ico-wrapper bkg-bfm_altrespese"><i class="icon icon-category_altrespese"></i></span>
+              <a href="#" class="text">Altre <br>Spese</a>
+            </span>
+          </div>
+        </div>
+        <!-- End Group -->
+        <!-- Group -->
+        <div class="col-md-4">
+          <div class="box-with-icon clean-style">
+            <span class="text-with-squared">
+              <span class="squared ico-wrapper bkg-bfm_servizi"><i class="icon icon-category_servizi"></i></span>
+              <a href="#" class="text">Servizi</a>
+            </span>
+          </div>
+        </div>
+        <div class="col-md-4">
+
+        </div>
+        <div class="col-md-4">
+
+        </div>
+        <!-- End Group -->
+        <!-- Group -->
+        <div class="col-md-4">
+
+        </div>
+        <div class="col-md-4">
+
+        </div>
+        <div class="col-md-4">
+
+        </div>
+        <!-- End Group -->
+        <!-- Group -->
+        <div class="col-md-4">
+
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
---
+++ librerie/youwebbusiness/layout/oggetti/accordion/accordion_con_image/oggetto.php
@@ -0,0 +1,3 @@
+<div class="accordion accordion-with-image bordered-style">
+  <?php virtual('/youwebbusiness/layout/oggetti/accordion/accordion_con_image/accordion.php');?>
+</div>
