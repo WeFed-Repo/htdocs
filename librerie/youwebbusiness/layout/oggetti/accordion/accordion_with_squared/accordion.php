22b92869f37f1e54726023641de2b2d7cd17ca93
sstrepponi <62430689+sstrepponi@users.noreply.github.com>
2021-07-05 11:42:35 +0200

    fix colonne box di loading
    fix colonne box di loading

--- librerie/youwebbusiness/layout/oggetti/accordion/accordion_with_squared/accordion.php
+++ librerie/youwebbusiness/layout/oggetti/accordion/accordion_with_squared/accordion.php
@@ -1,7 +1,7 @@
 <div class="card collapser b-r-3">
   <div class="card-header collapsed d-flex justify-content-between align-items-top" data-toggle="collapse">
     <div class="row d-flex justify-content-between align-items-center w-100">
-      <div class="col-md-7 col-lg-7 col-xl-8">
+      <div class="col-md-6 col-lg-7 col-xl-7">
           <h5 class="card-header-title mb-0">
             <div class="text-with-squared">
               <div class="squared bkg-bfm_status_confermare"></div>
@@ -10,7 +10,7 @@
           </h5>
       </div>
       <hr class="separator d-block d-md-none w-100 mt-3 mb-3">
-      <div class="col-md-5 col-lg-5 col-xl-4">
+      <div class="col-md-6 col-lg-5 col-xl-5 pr-0">
           <div class="row justify-content-md-end justify-content-between">
           <div class="col-5 col-md-5 col-lg-4 col-xl-5 d-flex justify-content-md-end justify-content-between align-items-center">
             <span class="color-gray2">10 transazioni</span>
@@ -34,7 +34,7 @@
 <div class="card collapser b-r-3 border-error">
   <div class="card-header collapsed d-flex justify-content-between align-items-top" data-toggle="collapse">
     <div class="row d-flex justify-content-between align-items-center w-100">
-      <div class="col-md-7 col-lg-7 col-xl-8">
+      <div class="col-md-6 col-lg-7 col-xl-7 pr-0">
           <h5 class="card-header-title mb-0">
             <div class="text-with-squared">
               <div class="squared bkg-bfm_status_attive"></div>
@@ -49,12 +49,12 @@
           </h5>
       </div>
       <hr class="separator d-block d-md-none w-100 mt-3 mb-3">
-      <div class="col-md-5 col-lg-5 col-xl-4">
+      <div class="col-md-6 col-lg-5 col-xl-5 pr-0">
           <div class="row justify-content-md-end justify-content-between">
-          <div class="col-5 col-md-5 col-lg-4 col-xl-5 d-flex justify-content-md-end justify-content-between align-items-center">
+          <div class="col-4 col-md-4 col-lg-4 col-xl-5 d-flex justify-content-md-end justify-content-between align-items-center">
             <span class="color-gray2">10 transazioni</span>
           </div>
-          <div class="col-2 col-md-2 col-lg-2 col-xl-2 d-flex justify-content-center justify-content-md-end align-items-center"><strong class="font-size-16 color-greenLink">25%</strong></div>
+          <div class="col-3 col-md-3 col-lg-2 col-xl-2 d-flex justify-content-center justify-content-md-end align-items-center"><strong class="font-size-16 color-greenLink">25,00%</strong></div>
           <div class="col-5 col-md-5 col-lg-6 col-xl-5 pl-0 d-flex justify-content-end align-items-center">
             <strong class="text color-gray1">150.000,86 &euro;</strong>
           </div>
@@ -72,7 +72,7 @@
 <div class="card collapser b-r-3 border-warning">
   <div class="card-header collapsed d-flex justify-content-between align-items-top" data-toggle="collapse">
     <div class="row d-flex justify-content-between align-items-center w-100">
-      <div class="col-md-7 col-lg-7 col-xl-8">
+      <div class="col-md-6 col-lg-7 col-xl-7 pr-0">
           <h5 class="card-header-title mb-0">
             <div class="text-with-squared">
               <div class="squared bkg-bfm_status_categorizzare"></div>
@@ -88,12 +88,12 @@
          
       </div>
       <hr class="separator d-block d-md-none w-100 mt-3 mb-3">
-      <div class="col-md-5 col-lg-5 col-xl-4">
+      <div class="col-md-6 col-lg-5 col-xl-5 pr-0">
           <div class="row justify-content-md-end justify-content-between">
-          <div class="col-5 col-md-5 col-lg-4 col-xl-5 d-flex justify-content-md-end justify-content-between align-items-center">
+          <div class="col-5 col-md-4 col-lg-4 col-xl-5 d-flex justify-content-md-end justify-content-between align-items-center">
             <span class="color-gray2">10 transazioni</span>
           </div>
-          <div class="col-2 col-md-2 col-lg-2 col-xl-2 d-flex justify-content-center justify-content-md-end align-items-center"><strong class="font-size-16 color-greenLink">10%</strong></div>
+          <div class="col-2 col-md-3 col-lg-2 col-xl-2 d-flex justify-content-center justify-content-md-end align-items-center"><strong class="font-size-16 color-greenLink">10,00%</strong></div>
           <div class="col-5 col-md-5 col-lg-6 col-xl-5 pl-0 d-flex justify-content-end align-items-center">
             <strong class="text color-gray1">150.000,86 &euro;</strong>
           </div>
--- librerie/youwebbusiness/layout/oggetti/box/box_with_squared/oggetto.php
+++ librerie/youwebbusiness/layout/oggetti/box/box_with_squared/oggetto.php
@@ -3,7 +3,7 @@
   <div class="card b-r-3 with-squared border-error">
     <div class="card-header  d-flex justify-content-between align-items-top">
       <div class="row d-flex justify-content-between align-items-center w-100">
-        <div class="col-md-7 col-lg-7 col-xl-8">
+        <div class="col-md-6 col-lg-7 col-xl-7">
             <h5 class="card-header-title mb-0">
               <div class="text-with-squared">
                 <div class="squared bkg-bfm_status_attive"></div>
@@ -18,12 +18,12 @@
             </h5>
         </div>
         <hr class="separator d-block d-md-none w-100 mt-3 mb-3">
-        <div class="col-md-5 col-lg-5 col-xl-4">
+        <div class="col-md-6 col-lg-5 col-xl-5">
             <div class="row justify-content-md-end justify-content-between">
-            <div class="col-5 col-md-5 col-lg-4 col-xl-5 d-flex justify-content-md-end justify-content-between align-items-center">
+            <div class="col-5 col-md-4 col-lg-4 col-xl-5 d-flex justify-content-md-end justify-content-between align-items-center">
               <span class="color-gray2">10 transazioni</span>
             </div>
-            <div class="col-2 col-md-2 col-lg-2 col-xl-2 d-flex justify-content-center justify-content-md-end align-items-center"><strong class="font-size-16 color-greenLink">25%</strong></div>
+            <div class="col-2 col-md-3 col-lg-2 col-xl-2 d-flex justify-content-center justify-content-md-end align-items-center"><strong class="font-size-16 color-greenLink">25,00%</strong></div>
             <div class="col-5 col-md-5 col-lg-6 col-xl-5 pl-0 d-flex justify-content-end align-items-center">
               <strong class="text color-gray1">150.000,86 &euro;</strong>
             </div>
