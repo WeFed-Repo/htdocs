dac82216667decc58652144279f3ab245641bd46
sstrepponi <62430689+sstrepponi@users.noreply.github.com>
2021-07-05 15:56:30 +0200

    fix selettore YWB check + oggetto box soglie
    fix selettore YWB check + oggetto box soglie (in lavorazione)

---
+++ librerie/youwebbusiness/layout/oggetti/box/box_elenco_soglie/oggetto.php
@@ -0,0 +1,28 @@
+<!-- BOX ELENCO SOGLIE-->
+<section class="no-margin-section">
+	<div class="bordered with-squared border-error box-fixed-card">
+		<div class="d-flex justify-content-between align-items-top">
+			<div class="box-fixed-card-left">
+				<span class="text-with-squared justify-content-center mb-1">
+					<span class="squared ico-wrapper bkg-bfm_bollette"><i class="icon icon-category_bollette"></i></span>
+				</span>
+				<div><span class="color-gray1 font-size-16"><strong>3.000,00 &euro;</strong></span></div>
+				<div><span>Mensile</span></div>
+			</div>
+			<div class="box-fixed-card-center">
+			</div>
+			<div class="box-fixed-card-right">
+				<div class="btn-group">
+					<a href="javascript:void(0);" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						<img src="fe/img/icon/more.svg">
+					</a>
+					<div class="dropdown-menu" x-placement="left-start">
+						<a role="button" class="dropdown-item">Dettaglio</a>
+						<a role="button" class="dropdown-item">Modifica</a>
+						<a role="button" class="dropdown-item">Elimina</a>
+					</div>
+                </div>	
+			</div>
+		</div>
+	</div>
+</section> 
