96b0b97d7c50d2de97e63bcc774a8493b44622ef
SG-Alten <simone.grippa@alten.it>
2021-07-08 14:10:25 +0200

    Htag YWB versione componente

--- librerie/youwebbusiness/layout/oggetti/form/hashtag_selection/oggetto.php
+++ librerie/youwebbusiness/layout/oggetti/form/hashtag_selection/oggetto.php
@@ -1,8 +1,8 @@
 <section>
-  <!-- Utilizzare la classe "mono-htag" se si vuole limitare l'inserimento a un SINGOLO-HTAG 
-      - Ricordarsi di caricare un solo HTAG come attivo nel caso ci si trovi in questa modalità!!!
+  <!--
+      - Ricordarsi di caricare un solo HTAG come attivo nel caso ci si trovi in modalità monotag!!!
   -->
-  <div class="bordered htag-module mono-htag">
+  <div class="bordered htag-module mono-htag" id="myhtagcomponent">
     <div class="row">
       <div class="col-12">
         <h2>Crea qui il tuo hashtag</h4>
@@ -15,32 +15,13 @@
           <p>Selezionalo tra gli hashtag già creati</p>
         </div>      
         <div class="htag-wall">
-          <div id="tg-1" class="htag-tag">
-            <i class="icon icon-round_check"></i>
-            <span class="htag-text">#progetto1</span>
-          </div>
-          <div id="tg-2" class="htag-tag selected">
-            <i class="icon icon-round_check"></i>
-            <span class="htag-text">#progetto2</span>
-          </div>
-          <div id="tg-3" class="htag-tag">
-            <i class="icon icon-round_check"></i>
-            <span class="htag-text">#progetto_molto_molto_molto_molto_lungo3</span>
-          </div>
-          <div id="tg-4" class="htag-tag htag-hide">
-            <i class="icon icon-round_check"></i>
-            <span class="htag-text">#progetto4</span>
-          </div> 
-          <div id="tg-5" class="htag-tag htag-hide">
-            <i class="icon icon-round_check"></i>
-            <span class="htag-text">#progetto_molto_molto_molto_molto_lungo5</span>
-          </div>        
+          <!-- Area Caricamento Htag -->
         </div>
-        <a id="more-htag" class="text-with-icon right-icon float-right">
+        <a id="more-htag" class="more-minus-htag text-with-icon right-icon float-right">
           <span class="icon icon-circle_plus"></span>
           Vedi tutti
         </a>
-        <a id="minus-htag" class="text-with-icon right-icon float-right d-none">
+        <a id="minus-htag" class="more-minus-htag text-with-icon right-icon float-right d-none">
           <span class="icon icon-circle_minus"></span>
           Vedi meno
         </a>
@@ -72,13 +53,10 @@
         <div class="htag-subtitle">
           <p><strong>L’hashtag che hai scelto:</strong></p>
         </div> 
-        <!-- Nel caso la modalità sia "mono-htag" ricordarsi di caricare un solo HTAG come attivo!!!
+        <!-- Nel caso la modalità sia "monotag" ricordarsi di caricare un solo HTAG come attivo!!!
         -->
         <div class="htag-added-wall">
-          <div id="tg-2-add" class="htag-tag selected">
-            <i class="icon icon-close"></i>
-            <span class="htag-text">#progetto2</span>
-          </div>
+          <!-- Area Caricamento Htag -->
         </div>
       </div>
     </div>
@@ -86,111 +64,196 @@
 </section>
 
 <script>
-  $(function(){
+            // Esempio Configurazione Oggetto Htag
+            var defHtag = {
+                "Htag": {
+                    "ShowNumber": 3,
+                    // monotag | multitag
+                    "Type": "multitag",
+                    "TagExisting": [
+                        {
+                            "Name": "Progetto1",
+                            "Value": "Progetto1",
+                            "State": "selected",
+                            "Origin": "old"
+                        },
+                        {
+                            "Name": "Progetto2",
+                            "Value": "Progetto2",
+                            "State": "unselected",
+                            "Origin": "old"
+                        },
+                        {
+                            "Name": "Progetto3",
+                            "Value": "Progetto3",
+                            "State": "unselected",
+                            "Origin": "old"
+                        },
+                        {
+                            "Name": "Progetto4",
+                            "Value": "Progetto4",
+                            "State": "unselected",
+                            "Origin": "old"
+                        },
+                        {
+                            "Name": "Progetto5",
+                            "Value": "Progetto5",
+                            "State": "unselected",
+                            "Origin": "old"
+                        }
+                    ]
+                }
+            }
 
-    // Verifica della modalità di inserimento e avvio della funzione di pulizia
-    function htagaddedwallclear() {
-      if($(".htag-module").is('.mono-htag')) {
-        // Pulizia dell'elenco degli Htag selezionati
-        $('.htag-added-wall .htag-tag').each(function () {
-          $(".htag-wall").find('#' + $(this).attr('id').slice(0,-4)).toggleClass("selected");
-          $(this).remove();
-        });
-      }
-    }   
 
-    // Funzione per gestire l'Append
-    function htagAppendOn() { 
-      if($(this).attr('id').slice(-4)=="-add") {
-        $(".htag-wall").find('#' + $(this).attr('id').slice(0,-4)).toggleClass("selected");
-      }      
-      $(this).remove();
-    }
-    
-    $(document).on('click', ('.htag-added-wall .htag-tag'), htagAppendOn);
+            /* ########## START componente Htag[SG] ######### */
+            /* Oggetti relativi all htg */
+            var htg;
+            var myid;
+            /* Avvio dell htg con parametri */
+            var startHtag = function (params) {
 
-    $(".htag-wall .htag-tag").click(function () {
-      if($(this).hasClass("selected")) {
-        $(this).toggleClass("selected");
-        $(".htag-added-wall").find('#' + $(this).attr('id') + '-add').remove();        
-      } else {
-        htagaddedwallclear();
-        $(this).toggleClass("selected");
-        let $newtag = '<div id="'+ $(this).attr('id') + '-add" class="htag-tag selected"><i class="icon icon-close"></i><span class="htag-text">' + $(this).find(".htag-text").text() + '</span></div>';
-        $(".htag-added-wall").append($newtag);      
-      }        
-    });
+                // Crea l'oggetto con i valori di default
+                htg = new Object({
+                    type: "multitag",
+                    shownumber: 3,
+                    tagexisting: []
+                })
 
-    $(".htag-added-wall .htag-tag").click(function () {
-      $(".htag-wall").find('#' + $(this).attr('id').slice(0,-4)).toggleClass("selected");
-      $(this).remove(); 
-    });
+                myid = '';
 
-    // Gestione inserimento htag con Autocomplete
-    /* var availableTags = [];
-    var newTagsCount = 0;
+                // Assegna i valori leggendo l'oggetto di configurazione in ingresso (se ricevuto)
+                if (params.objConf) {
+                    htg.tagexisting = params.objConf.Htag.TagExisting;
+                    htg.type = params.objConf.Htag.Type;
+                    htg.shownumber = params.objConf.Htag.ShowNumber;
+                } 
+                
+                if (params.id) {
+                  myid = params.id;
+                } 
+                                                
 
-    $( "input[name=htag-input]" ).autocomplete({
-      source: availableTags,
-      select: function() {
-        newTagsCount++;
-        let newtagtext = "" ;
-        if($('input[name=htag-input]').val().trim().charAt(0)=="#") {
-          newtagtext = $('input[name=htag-input]').val().trim();
-        }else{
-          newtagtext = '#' + $('input[name=htag-input]').val().trim();
-        }
-        let newtag = '<div id="tag-'+ newTagsCount + '-add-new" class="htag-tag selected"><i class="icon icon-close"></i><span class="htag-text">' + newtagtext + '</span></div>';
-        $(".htag-added-wall").append($newtag);
-        availableTags[0]= "";
-        $(this).val(''); 
-        return false;
-      },
-      open: function(event, ui) {
-        let widthctl = ($(this).width() + 18);
-        $(this).autocomplete("widget").attr('style', function(i,s) { return (s || '') + 'width: '+ widthctl + 'px !important;' });
-      }
-    });  
+                // #### Funzioni di Appoggio ####
+                // Generazione dei contenuti in base al nuovo oggetto
+                function htgupdate(){
+                  // Pulisco tutti i tag
+                  $(myid + ' .htag-tag').remove();
+                  $(myid + ' input[name=htag-input]').prop('disabled', false);
+                  $(myid + ' #btnCreaTag').removeClass('disabled');
+                  $(myid + ' input[name=htag-input]').val('');
+                  // Rigenero i Tag in base all'oggetto
+                  let counter = 1
+                  $.each(htg.tagexisting,function(){                                    
+                    let newtag = '<div id="tg-' + counter + '" class="htag-tag ' + ($(this)[0].State=="selected"? 'selected' : '') + ' ' + (counter>htg.shownumber? 'htag-hide' : '') + '" value="' + $(this)[0].Value + '" title="' + $(this)[0].Name + '"><i class="icon icon-round_check"></i><span class="htag-text">#' + $(this)[0].Name + '</span></div>';
+                    // Verifico se sia un tag di configurazione o nuovo 
+                    if($(this)[0].Origin=="old"){
+                      $(myid + " .htag-wall").append(newtag);
+                    }
+                    // Generazione Tag nel selected Wall  
+                    if($(this)[0].State == "selected") {
+                      let newtag = '<div id="tg-' + counter + '-add" class="htag-tag selected" value="' + $(this)[0].Value + '" title="' + $(this)[0].Name + '"><i class="icon icon-close"></i><span class="htag-text">#' + $(this)[0].Name + '</span></div>';
+                      $(myid + " .htag-added-wall").append(newtag);
+                      if(htg.type == "monotag"){
+                        $(myid + ' input[name=htag-input]').prop('disabled', true);
+                        $(myid + ' #btnCreaTag').addClass('disabled');
+                      }
+                    }
+                    counter++;
+                  });
+                  console.log(htg);
+                }  
 
-    $('input[name=htag-input]').keyup(function() {
-      let newautocompletetag = "";
-      if($(this).val().trim().charAt(0)=="#"){
-        newautocompletetag = $(this).val().trim();
-      }else{
-        newautocompletetag = '#' + $(this).val().trim();
-      }
-      availableTags[0]=(newautocompletetag);        
-    }); */
+                // Funzione per aggiornare l'oggetto al click sul tag esistente
+                function htagChangeObj() { 
+                  let tagpos = $(this).attr('id').replace('tg-','').replace('-add','') - 1;                  
+                  if(htg.type == "monotag"){
+                    // Nel caso siamo nella gestione monotag
+                    $.each(htg.tagexisting,function(index){ 
+                      if(tagpos == index) {
+                        if(htg.tagexisting[tagpos].Origin == "old"){
+                          if(htg.tagexisting[tagpos].State == "selected"){
+                            htg.tagexisting[tagpos].State = "unselected";
+                          } else {
+                            htg.tagexisting[tagpos].State = "selected";
+                          }
+                        } else {
+                          htg.tagexisting.splice(tagpos,1);
+                        }
+                      } else {
+                        if(htg.tagexisting[index].Origin == "old"){
+                          htg.tagexisting[index].State = "unselected";
+                        } else {                          
+                          htg.tagexisting.splice(index,1);
+                        }
+                      }                     
+                    });                 
+                  } else {
+                    // Nel caso siamo nella gestione multitag
+                    if(htg.tagexisting[tagpos].Origin == "old"){
+                      if(htg.tagexisting[tagpos].State == "selected"){
+                        htg.tagexisting[tagpos].State = "unselected";
+                      } else {
+                        htg.tagexisting[tagpos].State = "selected";
+                      }
+                    } else {
+                      htg.tagexisting.splice(tagpos,1);
+                    }
+                  } 
+                  htgupdate();
+                }
 
-    // Gestione inserimento htag con Bottone CREA
-    var newTagsCount = 0;
-    $("#btnCreaTag").click(function () {
-      htagaddedwallclear();
-      newTagsCount++;
-      let newtagtext = "" ;
-      if($('input[name=htag-input]').val().trim().charAt(0)=="#") {
-        newtagtext = $('input[name=htag-input]').val().trim();
-      }else{
-        newtagtext = '#' + $('input[name=htag-input]').val().trim();
-      }
-      let newtag = '<div id="tag-'+ newTagsCount + '-add-new" class="htag-tag selected"><i class="icon icon-close"></i><span class="htag-text">' + newtagtext + '</span></div>';
-      $(".htag-added-wall").append(newtag);
-      $('input[name=htag-input]').val('');
-    });
+                // Funzione per aggiornare l'oggetto al click sul crea
+                function htagPushObj() {                
+                  if(htg.type == "monotag"){
+                    // Nel caso siamo nella gestione monotag pulisco tutto
+                    $.each(htg.tagexisting,function(index){ 
+                      htg.tagexisting[index].State = "unselected";                  
+                    }); 
+                  }
+                  // Push del nuovo tag
+                  let newtagtext = '';
+                  if($(myid + ' input[name=htag-input]').val().trim().charAt(0)=="#") {
+                    newtagtext = $(myid + ' input[name=htag-input]').val().trim().substring(1);
+                  }else{
+                    newtagtext = $(myid + ' input[name=htag-input]').val().trim();
+                  }
+                  let data = {
+                              "Name": newtagtext,
+                              "Value": newtagtext,
+                              "State": "selected",
+                              "Origin": "new"
+                            }
+                  htg.tagexisting.push(data);           
+                  htgupdate();
+                }
 
-    // Show-Hide Default Tags
-    $("#more-htag").click(function () {
-      $("#more-htag").toggleClass("d-none");
-      $("#minus-htag").toggleClass("d-none");
-      $(".htag-wall .htag-tag.htag-hide").addClass("htag-show");
-      $(".htag-wall .htag-tag.htag-hide").removeClass("htag-hide");      
-    });
-    $("#minus-htag").click(function () {
-      $("#more-htag").toggleClass("d-none");
-      $("#minus-htag").toggleClass("d-none");
-      $(".htag-wall .htag-tag.htag-show").addClass("htag-hide");
-      $(".htag-wall .htag-tag.htag-show").removeClass("htag-show");      
-    });
-  });
-</script>
+                // Attribuzione               
+                $(document).on('click', (myid + ' .htag-tag'), htagChangeObj);
+                $(myid + ' #btnCreaTag').click(htagPushObj);
+                
+                // Funziona mostra tutti
+                $(myid + ' .more-minus-htag').click(function(){
+                  if(htg.tagexisting.lenght != htg.shownumber){
+                    htg.shownumber = htg.tagexisting.lenght;
+                  } else {
+                    htg.shownumber = params.objConf.Htag.ShowNumber;
+                  }
+                  $(myid + ' .more-minus-htag').toggleClass("d-none");
+                  htgupdate();
+                });
 
+                // Init
+                htgupdate();
+            } 
+
+
+            /* Chiamata del componente Htag */
+            startHtag({
+                id: "#myhtagcomponent",
+                objConf: defHtag,
+                
+                // Handler Htag attivi [rivedere]
+            
+            })
+        </script>
